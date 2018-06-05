b0e9504c1bc77d541bcabaff9ff38833543ec3ed
Pascal Kaufmann <kaufmae@me.com>
2018-06-06 00:07:21 +0200

    crossbase breakthrough

--- core/api/resolvers/mutations/createAssortment.js
+++ core/api/resolvers/mutations/createAssortment.js
@@ -1,7 +1,10 @@
 import { log } from 'meteor/unchained:core-logger';
 import { Assortments } from 'meteor/unchained:core-assortments';
 
-export default (root, { assortment }, { userId }) => {
+export default (root, { assortment }, { userId, localeContext }) => {
   log('mutation createAssortment', { userId });
-  return Assortments.createAssortment({ ...assortment });
+  return Assortments.createAssortment({
+    locale: localeContext.language,
+    ...assortment,
+  });
 };
