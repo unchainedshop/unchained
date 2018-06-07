5392120f32e899b1ee052775666b1dadbcfded98
Pascal Kaufmann <kaufmae@me.com>
2018-06-07 21:34:10 +0200

    fix avatar

--- core/api/resolvers/mutations/updateUserAvatar.js
+++ core/api/resolvers/mutations/updateUserAvatar.js
@@ -2,7 +2,6 @@ import { Promise } from 'meteor/promise';
 import { log } from 'meteor/unchained:core-logger';
 import { Avatars } from 'meteor/unchained:core-avatars';
 import { Users } from 'meteor/unchained:core-users';
-import { Promise } from 'meteor/promise';
 
 export default function (root, { avatar, userId: foreignUserId }, { userId }) {
   const normalizedUserId = foreignUserId || userId;
