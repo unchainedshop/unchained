2f3e707b4628f73a0a3ec4bf759e63b80c9ddac8
Pascal Kaufmann <kaufmae@me.com>
2018-06-20 10:01:05 +0200

    fix assortment graph render eeeeverything

--- controlpanel/components/assortments/AssortmentGraph.js
+++ controlpanel/components/assortments/AssortmentGraph.js
@@ -1,32 +1,51 @@
-import { compose, withProps } from 'recompose';
+import { compose, withProps, withHandlers, withState } from 'recompose';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import React from 'react';
 import { InteractiveForceGraph, ForceGraphNode, ForceGraphLink } from 'react-vis-force';
 
-const AssortmentList = ({ nodes, links }) => (nodes.length > 0 ? (
-  <InteractiveForceGraph
-    zoom
-    simulationOptions={{ height: 600, width: 600 }}
-    labelAttr="label"
-    onSelectNode={node => console.log(node)}
-    highlightDependencies
-  >
-    {nodes.map(node => (
-      <ForceGraphNode key={node.id} node={node} fill={node.fill} />
-    ))}
-    {links.map(link => (
-      <ForceGraphLink key={link.id} link={link} />
-    ))}
-  </InteractiveForceGraph>
+const AssortmentList = ({
+  nodes, links, isShowLeafNodes, toggleShowLeafNodes, data: { loading },
+}) => (nodes.length > 0 ? (
+  <div>
+    <div>
+      Show non-root nodes? <input type="checkbox" checked={isShowLeafNodes} onClick={toggleShowLeafNodes} />
+    </div>
+    {loading ? (
+      <div>
+        <h1>Loading... this can take minutes...</h1>
+      </div>
+    ) : (
+      <InteractiveForceGraph
+        zoom
+        simulationOptions={{ height: 600, width: 600 }}
+        labelAttr="label"
+        onSelectNode={node => console.log(node)}
+      >
+        {nodes.map(node => <ForceGraphNode key={node.id} node={node} fill={node.fill} />)}
+        {links.map(link => <ForceGraphLink key={link.id} link={link} />)}
+      </InteractiveForceGraph>
+    )}
+
+  </div>
 ) : null);
 
+const assortmentToNode = assortment => ({
+  label: assortment.texts.title,
+  fill: assortment.isRoot ? 'blue' : 'grey',
+  radius: assortment.isRoot ? 7 : 4,
+  group: assortment.sequence,
+  id: assortment._id,
+});
+
 export default compose(
+  withState('isShowLeafNodes', 'setShowLeafNodes', false),
   graphql(gql`
-    query assortmentLinks {
-      assortments(limit: 0, offset: 0, includeLeaves: true) {
+    query assortmentLinks($isShowLeafNodes: Boolean) {
+      assortments(limit: 0, offset: 0, includeLeaves: $isShowLeafNodes) {
         _id
         isRoot
+        sequence
         texts {
           title
         }
@@ -34,30 +53,47 @@ export default compose(
           _id
           parent {
             _id
+            sequence
+            texts {
+              title
+            }
           }
           child {
             _id
+            sequence
+            texts {
+              title
+            }
           }
         }
       }
     }
   `),
+  withHandlers({
+    toggleShowLeafNodes: ({ isShowLeafNodes, setShowLeafNodes }) => () =>
+      setShowLeafNodes(!isShowLeafNodes),
+  }),
   withProps(({ data: { assortments = [] } = {} }) => {
     const links = [];
-    const nodes = assortments.map((assortment) => {
-      if (assortment.linkedAssortments) {
-        assortment.linkedAssortments.forEach((link) => {
-          links.push(`${link.parent._id}.${link.child._id}`);
-        });
-      }
-      return {
-        label: assortment.texts.title,
-        fill: assortment.isRoot ? 'blue' : 'grey',
-        radius: assortment.isRoot ? 7 : 4,
-        group: assortment.sequence,
-        id: assortment._id,
-      };
-    });
+    const nodes = Object.values(assortments
+      .reduce((assortmentAccumulator, assortment) => {
+        const assortmentsResult = assortmentAccumulator;
+        if (!assortmentsResult[assortment._id]) {
+          assortmentsResult[assortment._id] = assortmentToNode(assortment);
+        }
+        if (assortment.linkedAssortments) {
+          assortment.linkedAssortments.forEach((link) => {
+            if (!assortmentsResult[link.parent._id]) {
+              assortmentsResult[link.parent._id] = assortmentToNode(link.parent);
+            }
+            if (!assortmentsResult[link.child._id]) {
+              assortmentsResult[link.child._id] = assortmentToNode(link.child);
+            }
+            links.push(`${link.parent._id}.${link.child._id}`);
+          });
+        }
+        return assortmentsResult;
+      }, {}));
 
     return {
       nodes,
@@ -65,7 +101,6 @@ export default compose(
         id: keypath,
         source: keypath.split('.')[0],
         target: keypath.split('.')[1],
-        value: 2,
       })),
     };
   }),
