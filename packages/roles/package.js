2c11c1d1ad32732a54f5d0aefd806ed868a5d907
Pascal Kaufmann <kaufmae@me.com>
2018-12-30 14:03:50 +0100

    bump unchained to 0.19.0

--- packages/roles/package.js
+++ packages/roles/package.js
@@ -1,7 +1,7 @@
 /* globals Package */
 Package.describe({
   name: 'unchained:roles',
-  version: '0.18.0',
+  version: '0.19.0',
   summary: 'Unchained Engine: Roles',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
