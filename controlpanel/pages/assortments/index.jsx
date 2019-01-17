9fb7db0c39d173a71fbe033afdb889cf8a9c170e
Pascal Kaufmann <kaufmae@me.com>
2019-01-17 10:11:25 +0100

    refactors dependency graphs and code structure of the control panel and minimal example

--- controlpanel/pages/assortments/index.jsx
+++ controlpanel/pages/assortments/index.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { Container, Tab } from 'semantic-ui-react';
 import App from '../../components/App';
 import AssortmentList from '../../components/assortments/AssortmentList';
-import AssortmentGraph from '../../components/assortments/AssortmentGraph';
-
 import connectApollo from '../../lib/connectApollo';
-
 
 const panes = [
   {
@@ -16,21 +13,13 @@ const panes = [
       </Tab.Pane>
     ),
   },
-  {
-    menuItem: 'Graph',
-    render: () => (
-      <Tab.Pane>
-        <AssortmentGraph />
-      </Tab.Pane>
-    ),
-  },
 ];
 
 export default connectApollo(props => (
   <App {...props}>
     <Container>
       <h2>
-Assortments
+        Assortments
       </h2>
       <Tab panes={panes} />
     </Container>
