abd5c2bcf095f34644855dce7b301b81f8d3662e
Marco <macrozone@gmail.com>
2019-02-24 18:19:48 +0100

    remove log

--- packages/core-products/db/product-media/collections.js
+++ packages/core-products/db/product-media/collections.js
@@ -1,7 +1,6 @@
 import { Mongo } from 'meteor/mongo';
 import { createFilesCollection } from 'meteor/unchained:core-files';
-
-// import config from '/unchained-config.js';
+git 
 
 export const ProductMedia = new Mongo.Collection('product_media');
 export const ProductMediaTexts = new Mongo.Collection('product_media_texts');
