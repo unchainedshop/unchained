ce01edb5ef823597ffe59b07821aa999789a31c6
Pascal Kaufmann <kaufmae@me.com>
2019-02-25 14:22:32 +0100

    refactor: move reorder logic to the correct level of abstraction

--- packages/api/resolvers/mutations/reorderAssortmentFilters.js
+++ packages/api/resolvers/mutations/reorderAssortmentFilters.js
@@ -3,15 +3,5 @@ import { AssortmentFilters } from 'meteor/unchained:core-assortments';
 
 export default function (root, { sortKeys = [] }, { userId }) {
   log('mutation reorderAssortmentFilters', { userId });
-  const changedAssortmentFilterIds = sortKeys.map(({ assortmentFilterId, sortKey }) => {
-    AssortmentFilters.update({
-      _id: assortmentFilterId,
-    }, {
-      $set: { sortKey: sortKey + 1, updated: new Date() },
-    });
-    return assortmentFilterId;
-  });
-  return AssortmentFilters
-    .find({ _id: { $in: changedAssortmentFilterIds } })
-    .fetch();
+  return AssortmentFilters.updateManualOrder({ sortKey });
 }
