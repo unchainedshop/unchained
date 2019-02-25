825d7cba82d9e7ea763e5a2cdf3f5da67a081eb1
Pascal Kaufmann <kaufmae@me.com>
2019-02-25 13:36:32 +0100

    adds quotations alpha 0.1

---
+++ packages/api/resolvers/types/quotation.js
@@ -0,0 +1,14 @@
+import { Currencies } from 'meteor/unchained:core-currencies';
+import { actions } from '../../roles';
+import { checkTypeResolver } from '../../acl';
+
+export default {
+  status(obj) {
+    return obj.normalizedStatus();
+  },
+  currency(obj) {
+    return Currencies.findOne({ isoCode: obj.currency });
+  },
+
+  logs: checkTypeResolver(actions.viewLogs, 'logs'),
+};
---
+++ packages/core-quotations/plugins/manual.js
@@ -0,0 +1,42 @@
+import {
+  QuotationDirector,
+  QuotationAdapter,
+} from 'meteor/unchained:core-quotations';
+
+class ManualOffering extends QuotationAdapter {
+  static key = 'shop.unchained.quotations.manual'
+
+  static version = '1.0'
+
+  static label = 'Manual Offerings'
+
+  static orderIndex = 0
+
+  static isActivatedFor() {
+    return true;
+  }
+
+  configurationError() { // eslint-disable-line
+    return null;
+  }
+
+  async manualRequestVerificationNeeded() { // eslint-disable-line
+    return false;
+  }
+
+  async manualProposalNeeded(context) { // eslint-disable-line
+    return true;
+  }
+
+  async quote(config) { // eslint-disable-line
+    return {
+      expires: new Date() + (1000 * 3600),
+    };
+  }
+
+  async rejectWithReason() { // eslint-disable-line
+    return true;
+  }
+}
+
+QuotationDirector.registerAdapter(ManualOffering);
