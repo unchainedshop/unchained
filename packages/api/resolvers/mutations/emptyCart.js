d82854e15e9e210ff3e4bb4180e8fe6925d35357
Pascal Kaufmann <kaufmae@me.com>
2019-02-27 10:20:55 +0100

    adds emptyCart and sends more error data to client, closes #6

---
+++ packages/api/resolvers/mutations/emptyCart.js
@@ -0,0 +1,27 @@
+import { log } from 'meteor/unchained:core-logger';
+import { Users } from 'meteor/unchained:core-users';
+import { Orders, OrderStatus, OrderPositions } from 'meteor/unchained:core-orders';
+import {
+  UserNotFoundError,
+  OrderNotFoundError, OrderWrongStatusError,
+} from '../../errors';
+
+export default function (root, {
+  orderId,
+}, { userId, countryContext }) {
+  log('mutation emptyCart', { userId, orderId });
+  if (orderId) {
+    const order = Orders.findOne({ _id: orderId });
+    if (!order) throw new OrderNotFoundError({ orderId });
+    if (order.status !== OrderStatus.OPEN) {
+      throw new OrderWrongStatusError({ data: { status: order.status } });
+    }
+    OrderPositions.removePositions({ orderId });
+    return order;
+  }
+  const user = Users.findOne({ _id: userId });
+  if (!user) throw new UserNotFoundError({ userId });
+  const cart = user.cart({ countryContext });
+  OrderPositions.removePositions({ orderId: cart._id });
+  return cart;
+}
