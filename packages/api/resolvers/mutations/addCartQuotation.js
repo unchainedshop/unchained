6215e4a0cff880a35effb8469284130fdf574fde
Pascal Kaufmann <kaufmae@me.com>
2019-03-06 00:33:38 +0100

    it’s only allowed to add a quotation to a cart that is in status proposed
    what happens with expires date?

--- packages/api/resolvers/mutations/addCartQuotation.js
+++ packages/api/resolvers/mutations/addCartQuotation.js
@@ -1,9 +1,9 @@
 import { log } from 'meteor/unchained:core-logger';
-import { Quotations } from 'meteor/unchained:core-quotations';
+import { Quotations, QuotationStatus } from 'meteor/unchained:core-quotations';
 import { Users } from 'meteor/unchained:core-users';
 import { Orders } from 'meteor/unchained:core-orders';
 import {
-  QuotationNotFoundError, UserNotFoundError,
+  QuotationNotFoundError, UserNotFoundError, QuotationWrongStatusError,
   OrderNotFoundError, OrderWrongStatusError,
 } from '../../errors';
 
@@ -11,8 +11,11 @@ export default function (root, {
   orderId, quotationId, quantity, configuration,
 }, { userId, countryContext }) {
   log(`mutation addCartQuotation ${quotationId} ${quantity} ${configuration ? JSON.stringify(configuration) : ''}`, { userId, orderId });
-  const quotationCount = Quotations.find({ _id: quotationId }).count();
-  if (quotationCount === 0) throw new QuotationNotFoundError({ data: { quotationId } });
+  const quotation = Quotations.findOne({ _id: quotationId });
+  if (!quotation) throw new QuotationNotFoundError({ data: { quotationId } });
+  if (quotation.status !== QuotationStatus.PROPOSED) {
+    throw new QuotationWrongStatusError({ data: { status: quotation.status } });
+  }
   if (orderId) {
     const order = Orders.findOne({ _id: orderId });
     if (!order) throw new OrderNotFoundError({ orderId });
