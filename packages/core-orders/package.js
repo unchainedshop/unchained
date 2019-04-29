da81bd862ebcdffe86e5135b524c75b984c469a4
Pascal Kaufmann <kaufmae@me.com>
2019-04-29 09:19:51 +0200

    0.27.0

--- packages/core-orders/package.js
+++ packages/core-orders/package.js
@@ -1,7 +1,7 @@
 /* globals Package */
 Package.describe({
   name: 'unchained:core-orders',
-  version: '0.26.0',
+  version: '0.27.0',
   summary: 'Unchained Engine Core: Orders',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md'
@@ -15,18 +15,18 @@ Package.onUse(api => {
   api.use('dburles:factory@1.1.0');
   api.use('dburles:collection-helpers@1.1.0');
 
-  api.use('unchained:utils@0.26.0');
-  api.use('unchained:core-files@0.26.0');
-  api.use('unchained:core-logger@0.26.0');
-  api.use('unchained:core-pricing@0.26.0');
-  api.use('unchained:core-users@0.26.0');
-  api.use('unchained:core-countries@0.26.0');
-  api.use('unchained:core-documents@0.26.0');
-  api.use('unchained:core-delivery@0.26.0');
-  api.use('unchained:core-products@0.26.0');
-  api.use('unchained:core-discounting@0.26.0');
-  api.use('unchained:core-payment@0.26.0');
-  api.use('unchained:core-quotations@0.26.0');
+  api.use('unchained:utils@0.27.0');
+  api.use('unchained:core-files@0.27.0');
+  api.use('unchained:core-logger@0.27.0');
+  api.use('unchained:core-pricing@0.27.0');
+  api.use('unchained:core-users@0.27.0');
+  api.use('unchained:core-countries@0.27.0');
+  api.use('unchained:core-documents@0.27.0');
+  api.use('unchained:core-delivery@0.27.0');
+  api.use('unchained:core-products@0.27.0');
+  api.use('unchained:core-discounting@0.27.0');
+  api.use('unchained:core-payment@0.27.0');
+  api.use('unchained:core-quotations@0.27.0');
 
   api.mainModule('orders.js', 'server');
 });
--- packages/core-payment/package.js
+++ packages/core-payment/package.js
@@ -1,7 +1,7 @@
 /* globals Package */
 Package.describe({
   name: 'unchained:core-payment',
-  version: '0.26.0',
+  version: '0.27.0',
   summary: 'Unchained Engine Core: Payment',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md'
@@ -15,8 +15,8 @@ Package.onUse(api => {
   api.use('percolate:migrations@1.0.2');
   api.use('dburles:factory@1.1.0');
   api.use('dburles:collection-helpers@1.1.0');
-  api.use('unchained:utils@0.26.0');
-  api.use('unchained:core-logger@0.26.0');
+  api.use('unchained:utils@0.27.0');
+  api.use('unchained:core-logger@0.27.0');
 
   api.mainModule('payment.js', 'server');
 });
