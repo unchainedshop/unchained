7056f56c81e4b07ce75a8723efe6e059d8f4aca4
Pascal Kaufmann <kaufmae@me.com>
2019-05-09 17:25:27 +0200

    create normal user and anonymous login

--- tests/seeds/users.js
+++ tests/seeds/users.js
@@ -1,6 +1,9 @@
 module.exports.ADMIN_TOKEN =
   'Bearer JOytDLyIQL3lOY1K1EfKx_S_nh13kOIe9h_J8E3mDWO';
 
+module.exports.USER_TOKEN =
+  'Bearer U8CGfEBGEFM9s97hFamxruxV9VlvfRw5CbQ27Ci0WWz';
+
 module.exports.Admin = {
   _id: 'admin',
   username: 'admin',
@@ -23,6 +26,34 @@ module.exports.Admin = {
         {
           when: new Date(new Date().getTime() + 1000000),
           hashedToken: 'XX1ivUOcPmJ/tzP0TRwoVuBLuRZod2t2QtZu+kHTThg='
+        }
+      ]
+    }
+  }
+};
+
+module.exports.User = {
+  _id: 'user',
+  username: 'user',
+  emails: [
+    {
+      address: 'user@localhost',
+      verified: true
+    }
+  ],
+  guest: false,
+  created: new Date(),
+  updated: new Date(),
+  roles: [],
+  services: {
+    password: {
+      bcrypt: '$2b$10$UjNk75pHOmaIiUMtfmNxPeLrs56qSpA4nRFf7ub6MPI7HF07usCJ2'
+    },
+    resume: {
+      loginTokens: [
+        {
+          when: new Date(new Date().getTime() + 1000000),
+          hashedToken: 'WeMY99U5lhhJJTpC7bw6voIN6Q2ETWEyEhkXpn9G9ls'
         }
       ]
     }
