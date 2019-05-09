d10789aa7753363f1f25bd7a470aa2f0649abb0d
Pascal Kaufmann <kaufmae@me.com>
2019-05-09 10:13:27 +0200

    remove dead stuff from schema

--- packages/api/schema/types/money.js
+++ packages/api/schema/types/money.js
@@ -1,11 +1,5 @@
 export default [
   /* GraphQL */ `
-    type ConversionRate {
-      sourceCurrency: String
-      date: Date!
-      quote(targetCurrency: String): Int!
-    }
-
     type Money {
       amount: Int
       currency: String
