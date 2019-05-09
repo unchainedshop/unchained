aa0631276593d6406d003e94998224d53a1d23c4
Pascal Kaufmann <kaufmae@me.com>
2019-05-09 16:53:24 +0200

    test warehousing providers

---
+++ tests/setup-warehousing.test.js
@@ -0,0 +1,61 @@
+const { setupDatabase, createAdminApolloFetch } = require('./helpers');
+
+describe('setup warehousing providers', () => {
+  let WarehousingProviders;
+  let connection;
+  let db;
+  let apolloFetch;
+
+  beforeAll(async () => {
+    [db, connection] = await setupDatabase();
+    apolloFetch = await createAdminApolloFetch();
+    WarehousingProviders = db.collection('warehousing-providers');
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('add a shipping warehousing provider', async () => {
+    const {
+      data: { createWarehousingProvider, errors }
+    } = await apolloFetch({
+      query: /* GraphQL */ `
+        mutation createWarehousingProvider(
+          $warehousingProvider: CreateProviderInput!
+        ) {
+          createWarehousingProvider(warehousingProvider: $warehousingProvider) {
+            _id
+            created
+            updated
+            deleted
+            type
+            interface {
+              _id
+              version
+              label
+            }
+            configuration
+            configurationError
+          }
+        }
+      `,
+      variables: {
+        warehousingProvider: {
+          type: 'PHYSICAL',
+          adapterKey: 'shop.unchained.warehousing.google-sheets'
+        }
+      }
+    });
+    expect(errors).toEqual(undefined);
+    expect(createWarehousingProvider).toMatchObject({
+      configurationError: null,
+      deleted: null,
+      interface: {
+        _id: 'shop.unchained.warehousing.google-sheets'
+      },
+      type: 'PHYSICAL'
+    });
+    expect(await WarehousingProviders.countDocuments()).toEqual(1);
+  });
+});
