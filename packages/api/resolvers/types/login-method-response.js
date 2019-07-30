b9922e90f0ced49b4d76cf9ef5fa91bcc2c3ba9d
Pascal Kaufmann <kaufmae@me.com>
2019-07-30 20:29:11 +0200

    enable for all users

--- packages/api/resolvers/types/login-method-response.js
+++ packages/api/resolvers/types/login-method-response.js
@@ -3,7 +3,7 @@ import { Users } from 'meteor/unchained:core-users';
 export default {
   user({ id }) {
     const user = Users.findOne({ _id: id });
-    user._bypassIsMyselfRule = true; // eslint-disable-line
+    user._inLoginMethodResponse = true; // eslint-disable-line
     return user;
   }
 };
