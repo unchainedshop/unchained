f0f8031b44f7608204ce58aa43f028d9480903bb
Pascal Kaufmann <kaufmae@me.com>
2019-09-14 12:25:02 +0200

    fix: could not make it work in a more concise way

--- packages/core-assortments/db/helpers.js
+++ packages/core-assortments/db/helpers.js
@@ -22,15 +22,57 @@ const fillToSameLengthArray = (a, b) => {
   return [fillUp(a, length), fillUp(b, length)];
 };
 
-const zipTreeByDeepness = arrayOfArrays => {
-  const shuffled = arrayOfArrays.reduce((a, b) => {
-    const [accumulator, currentArray] = fillToSameLengthArray(a, b);
-    return R.zip(accumulator, R.flatten(currentArray));
+const divideTreeByLevels = (array, level = 0) => {
+  const currentLevel = array.reduce((acc, item) => {
+    if (typeof item === 'string') {
+      return [...acc, item];
+    }
+    return acc;
   }, []);
+
+  const nextLevels = array.reduce((acc, item) => {
+    if (typeof item !== 'string') {
+      return [...acc, ...divideTreeByLevels(item, level + 1)];
+    }
+    return acc;
+  }, []);
+
+  return [
+    currentLevel.length && { level, items: currentLevel },
+    ...nextLevels
+  ].filter(Boolean);
+};
+
+const concatItemsByLevels = levelArray => {
+  return Object.values(
+    levelArray.reduce((acc, { level, items }) => {
+      return {
+        ...acc,
+        [level]: [...(acc[level] || []), items]
+      };
+    }, {})
+  );
+};
+
+const shuffleEachLevel = unshuffledLevels => {
+  return unshuffledLevels.map(subArrays => {
+    const shuffled = subArrays.reduce((a, b) => {
+      const [accumulator, currentArray] = fillToSameLengthArray(a, b);
+      return R.zip(accumulator, currentArray);
+    }, []);
+    return shuffled;
+  });
+};
+
+const zipTreeByDeepness = tree => {
+  const levels = divideTreeByLevels(tree);
+  const concattedLevels = concatItemsByLevels(levels);
+  const items = shuffleEachLevel(concattedLevels);
+
   return R.pipe(
     R.flatten,
     R.filter(Boolean)
-  )(shuffled);
+  )(items);
 };
 
 export const resolveAssortmentLinkFromDatabase = ({
@@ -575,7 +617,7 @@ Collections.Assortments.helpers({
     offset,
     query,
     sort = {},
-    forceLiveCollection = false,
+    forceLiveCollection = true,
     includeInactive = false
   } = {}) {
     const productIds = this.productIds({ forceLiveCollection });
@@ -679,7 +721,7 @@ Collections.Assortments.helpers({
       return [];
     });
 
-    return [ownProductIds, ...productIds];
+    return [...ownProductIds, ...productIds];
   },
   invalidateProductIdCache() {
     const linkedAssortments = this.linkedAssortments();
