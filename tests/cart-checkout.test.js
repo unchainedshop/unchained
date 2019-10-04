6ac0ec84b3906560e91acc53bb58f303b429b009
Pascal Kaufmann <kaufmae@me.com>
2019-10-04 11:08:45 +0200

    refactor: add add updateCart test

--- tests/cart-checkout.test.js
+++ tests/cart-checkout.test.js
@@ -33,6 +33,68 @@ describe('cart checkout', () => {
     });
   });
 
+  describe('Mutation.updateCart', () => {
+    it('update the billingAddress', async () => {
+      const Orders = db.collection('orders');
+      const order = Orders.findOne({ orderNumber: 'wishlist' });
+      const { data: { updateCart } = {} } = await graphqlFetch({
+        query: /* GraphQL */ `
+          mutation updateCart($billingAddress: AddressInput, $orderId: ID) {
+            updateCart(orderId: $orderId, billingAddress: $billingAddress) {
+              _id
+              orderNumber
+              billingAddress {
+                firstName
+              }
+            }
+          }
+        `,
+        variables: {
+          orderId: order._id,
+          billingAddress: {
+            firstName: 'Hallo',
+            lastName: 'Velo',
+            addressLine: 'Strasse 1',
+            addressLine2: 'Postfach',
+            postalCode: '8000',
+            city: 'Zürich'
+          }
+        }
+      });
+      expect(updateCart).toMatchObject({
+        orderNumber: 'wishlist',
+        billingAddress: {
+          firstName: 'Hallo'
+        }
+      });
+    });
+    // it('update the contact', async () => {
+    //   const { data: { updateCart } = {} } = await graphqlFetch({
+    //     query: /* GraphQL */ `
+    //       mutation updateCart($contact: ContactInput) {
+    //         updateCart(orderId: "wishlist", contact: $contact) {
+    //           _id
+    //           orderNumber
+    //         }
+    //       }
+    //     `,
+    //     variables: {
+    //       contact: {
+    //         firstName: 'Hallo',
+    //         lastName: 'Velo',
+    //         addressLine: 'Strasse 1',
+    //         addressLine2: 'Postfach',
+    //         postalCode: '8000',
+    //         city: 'Zürich'
+    //       }
+    //     }
+    //   });
+    //   expect(updateCart).toMatchObject({
+    //     orderNumber: 'wishlist'
+    //   });
+    // });
+  });
+
   describe('Mutation.addCartProduct', () => {
     it('add a product to the cart', async () => {
       const { data: { addCartProduct } = {} } = await graphqlFetch({
