8c11289b081a8afacb2566f19f2baad3db8bad77
Pascal Kaufmann <kaufmae@me.com>
2020-01-16 16:15:32 +0100

    Feat: Add multi mail editing to controlpanel

---
+++ examples/controlpanel/components/account/EmailsList.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { compose, pure, withProps } from 'recompose';
+import { graphql, withApollo } from 'react-apollo';
+import gql from 'graphql-tag';
+import EmailsListItem from './EmailsListItem';
+import FormAddEmail from './FormAddEmail';
+
+const EmailsList = ({
+  userId,
+  emails,
+  onSubmitSuccess,
+  disableResendVerificationEmail
+}) => (
+  <>
+    {emails.map(({ address, verified }) => (
+      <EmailsListItem
+        key={address}
+        userId={userId}
+        address={address}
+        verified={verified}
+        disableResendVerificationEmail={disableResendVerificationEmail}
+        onSubmitSuccess={onSubmitSuccess}
+      />
+    ))}
+    <FormAddEmail userId={userId} onSubmitSuccess={onSubmitSuccess} />
+  </>
+);
+
+export default compose(
+  withApollo,
+  graphql(gql`
+    query emails($userId: ID) {
+      user(userId: $userId) {
+        _id
+        emails {
+          verified
+          address
+        }
+      }
+    }
+  `),
+  withProps(({ data: { user: { emails = [] } = {} } }) => ({
+    emails
+  })),
+  pure
+)(EmailsList);
