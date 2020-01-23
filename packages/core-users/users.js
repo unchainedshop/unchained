394b0e5648f8e57d5b55433e1e9cb68132147cbd
Pascal Kaufmann <kaufmae@me.com>
2020-01-23 20:52:05 +0100

    feat: export all user schemas

--- packages/core-users/users.js
+++ packages/core-users/users.js
@@ -2,6 +2,7 @@ import './db/factories';
 import runMigrations from './db/schema';
 import './db/helpers';
 
+export * from './db/schema';
 export * from './db/collections';
 export default () => {
   // configure
