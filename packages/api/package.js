84fea9f00efeec7e7a04b05a50cc28c17c4e700b
Pascal Kaufmann <kaufmae@me.com>
2020-02-24 22:51:54 +0100

    release api@0.42.0

--- packages/api/package.js
+++ packages/api/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:api',
-  version: '0.41.0', // eslint-disable-line
+  version: '0.42.0', // eslint-disable-line
   summary: 'Unchained Engine: GraphQL API',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md'
