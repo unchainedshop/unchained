eaaf44a814a4fd1caab41df538cbcc540604c39c
Pascal Kaufmann <kaufmae@me.com>
2020-02-24 22:50:26 +0100

    fix: assortment path link should actually return a link

--- packages/api/resolvers/types/assortment-path-link.js
+++ packages/api/resolvers/types/assortment-path-link.js
@@ -2,7 +2,7 @@ import { AssortmentLinks } from 'meteor/unchained:core-assortments';
 
 export default {
   async link({ assortmentId, childAssortmentId }) {
-    AssortmentLinks.findOne({
+    return AssortmentLinks.findOne({
       parentAssortmentId: assortmentId,
       childAssortmentId
     });
