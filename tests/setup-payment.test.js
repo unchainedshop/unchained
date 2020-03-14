bcb7f2ac79083749df59784c5e44844c278b9660
Pascal Kaufmann <kaufmae@me.com>
2020-03-14 17:23:47 +0100

    chore: add tests for shipping & delivery providers

--- tests/setup-payment.test.js
+++ tests/setup-payment.test.js
@@ -1,60 +1,141 @@
 import { setupDatabase, createLoggedInGraphqlFetch } from './helpers';
+import { SimplePaymentProvider } from './seeds/payments';
 
 describe('setup payment providers', () => {
-  let PaymentProviders;
   let connection;
-  let db;
   let graphqlFetch;
 
   beforeAll(async () => {
-    [db, connection] = await setupDatabase();
+    [, connection] = await setupDatabase();
     graphqlFetch = await createLoggedInGraphqlFetch();
-    PaymentProviders = db.collection('payment-providers');
   });
 
   afterAll(async () => {
     await connection.close();
   });
 
-  it('add an invoice payment provider', async () => {
-    const {
-      data: { createPaymentProvider, errors }
-    } = await graphqlFetch({
-      query: /* GraphQL */ `
-        mutation createPaymentProvider($paymentProvider: CreateProviderInput!) {
-          createPaymentProvider(paymentProvider: $paymentProvider) {
-            _id
-            created
-            updated
-            deleted
-            type
-            interface {
+  describe('Mutation.createPaymentProvider', () => {
+    it('Add an invoice payment provider', async () => {
+      const {
+        data: { createPaymentProvider, errors }
+      } = await graphqlFetch({
+        query: /* GraphQL */ `
+          mutation createPaymentProvider(
+            $paymentProvider: CreateProviderInput!
+          ) {
+            createPaymentProvider(paymentProvider: $paymentProvider) {
               _id
-              version
-              label
+              created
+              updated
+              deleted
+              type
+              interface {
+                _id
+                version
+                label
+              }
+              configuration
+              configurationError
             }
-            configuration
-            configurationError
+          }
+        `,
+        variables: {
+          paymentProvider: {
+            type: 'INVOICE',
+            adapterKey: 'shop.unchained.invoice'
           }
         }
-      `,
-      variables: {
-        paymentProvider: {
-          type: 'INVOICE',
-          adapterKey: 'shop.unchained.invoice'
+      });
+      expect(errors).toEqual(undefined);
+      expect(createPaymentProvider).toMatchObject({
+        configuration: [],
+        configurationError: null,
+        deleted: null,
+        interface: {
+          _id: 'shop.unchained.invoice'
+        },
+        type: 'INVOICE'
+      });
+    });
+  });
+
+  describe('Mutation.updatePaymentProvider', () => {
+    it('Update a payment provider', async () => {
+      const {
+        data: { updatePaymentProvider, errors }
+      } = await graphqlFetch({
+        query: /* GraphQL */ `
+          mutation updatePaymentProvider(
+            $paymentProvider: UpdateProviderInput!
+            $paymentProviderId: ID!
+          ) {
+            updatePaymentProvider(
+              paymentProvider: $paymentProvider
+              paymentProviderId: $paymentProviderId
+            ) {
+              _id
+              type
+              deleted
+              interface {
+                _id
+              }
+              configuration
+              configurationError
+            }
+          }
+        `,
+        variables: {
+          paymentProviderId: SimplePaymentProvider._id,
+          paymentProvider: {
+            configuration: [
+              {
+                key: 'gugus',
+                value: 'blub'
+              }
+            ]
+          }
         }
-      }
+      });
+      expect(errors).toEqual(undefined);
+      expect(updatePaymentProvider).toMatchObject({
+        configuration: [
+          {
+            key: 'gugus',
+            value: 'blub'
+          }
+        ],
+        configurationError: null,
+        deleted: null,
+        interface: {
+          _id: 'shop.unchained.invoice'
+        },
+        type: 'INVOICE'
+      });
     });
-    expect(errors).toEqual(undefined);
-    expect(createPaymentProvider).toMatchObject({
-      configuration: [],
-      configurationError: null,
-      deleted: null,
-      interface: {
-        _id: 'shop.unchained.invoice'
-      },
-      type: 'INVOICE'
+  });
+
+  describe('Mutation.removePaymentProvider', () => {
+    it('Remove a payment provider', async () => {
+      const {
+        data: { removePaymentProvider, errors }
+      } = await graphqlFetch({
+        query: /* GraphQL */ `
+          mutation removePaymentProvider($paymentProviderId: ID!) {
+            removePaymentProvider(paymentProviderId: $paymentProviderId) {
+              _id
+              deleted
+            }
+          }
+        `,
+        variables: {
+          paymentProviderId: SimplePaymentProvider._id
+        }
+      });
+      expect(errors).toEqual(undefined);
+      expect(removePaymentProvider).toMatchObject({
+        deleted: expect.anything(),
+        _id: SimplePaymentProvider._id
+      });
     });
-    expect(await PaymentProviders.countDocuments()).toEqual(4);
   });
 });
