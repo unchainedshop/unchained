6888f33153d37469cc5dde23f2be458eca4941c8
Pascal Kaufmann <kaufmae@me.com>
2020-03-17 13:58:34 +0100

    chore: publish api

--- packages/api/package.js
+++ packages/api/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:api',
-  version: '0.43.0',
+  version: '0.44.1',
   summary: 'Unchained Engine: GraphQL API',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md'
