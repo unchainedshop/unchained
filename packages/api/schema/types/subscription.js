65b0aa6ade222418df3e985d3768064947062156
Pascal Kaufmann <kaufmae@me.com>
2020-04-03 22:33:10 +0200

    add graphql types we have to work with

--- packages/api/schema/types/subscription.js
+++ packages/api/schema/types/subscription.js
@@ -22,12 +22,36 @@ export default [
       TERMINATED
     }
 
+    type SubscriptionPlan {
+      _id: ID!
+      product: Product!
+      quantity: Int!
+      configuration: [ProductConfigurationParameter!]
+    }
+
+    type SubscriptionPayment {
+      _id: ID!
+      provider: PaymentProvider
+      meta: JSON
+    }
+
+    type SubscriptionDelivery {
+      _id: ID!
+      provider: DeliveryProvider
+      meta: JSON
+    }
+
     """
     Subscription
     """
     type Subscription {
       _id: ID!
       user: User!
+      plans: [SubscriptionPlan!]!
+      payment: SubscriptionPayment!
+      delivery: SubscriptionDelivery!
+      billingAddress: Address
+      contact: Contact
       status: SubscriptionStatus!
       created: Date!
       expires: Date
