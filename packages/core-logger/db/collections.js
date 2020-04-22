403131505b2b9146166ad362492f51b80d8542b3
Pascal Kaufmann <kaufmae@me.com>
2020-04-22 09:03:09 +0200

    feat: also add ttl index to work queue, but hold work 30 days

--- packages/core-logger/db/collections.js
+++ packages/core-logger/db/collections.js
@@ -2,13 +2,13 @@ import { Mongo } from 'meteor/mongo';
 
 export const Logs = new Mongo.Collection('logs');
 
-const twoDaysInSeconds = 172800;
+const ONE_DAY_IN_SECONDS = 86400;
 
 Logs.rawCollection().createIndex(
   {
     created: -1,
   },
-  { expireAfterSeconds: twoDaysInSeconds }
+  { expireAfterSeconds: 2 * ONE_DAY_IN_SECONDS }
 );
 
 export default Logs;
--- packages/core-worker/db/collections.js
+++ packages/core-worker/db/collections.js
@@ -2,4 +2,13 @@ import { Mongo } from 'meteor/mongo';
 
 export const WorkQueue = new Mongo.Collection('work_queue');
 
+const ONE_DAY_IN_SECONDS = 86400;
+
+WorkQueue.rawCollection().createIndex(
+  {
+    created: -1,
+  },
+  { expireAfterSeconds: 30 * ONE_DAY_IN_SECONDS }
+);
+
 export default WorkQueue;
