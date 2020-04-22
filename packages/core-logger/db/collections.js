47ca867a5b2cc1653ffec09b3df6dbe717936885
Pascal Kaufmann <kaufmae@me.com>
2020-04-22 08:59:58 +0200

    fix: create a ttl index for logs to make mongodb clean the items out of a collection which are older than 48h

--- packages/core-logger/db/collections.js
+++ packages/core-logger/db/collections.js
@@ -2,4 +2,13 @@ import { Mongo } from 'meteor/mongo';
 
 export const Logs = new Mongo.Collection('logs');
 
+const twoDaysInSeconds = 172800;
+
+Logs.rawCollection().createIndex(
+  {
+    created: -1,
+  },
+  { expireAfterSeconds: twoDaysInSeconds }
+);
+
 export default Logs;
