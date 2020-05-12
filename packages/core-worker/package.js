6ea1aff0e07270db222bfaa4c72f1a44eef227e8
Pascal Kaufmann <kaufmae@me.com>
2020-05-12 14:12:57 +0200

    fix: publish patches

--- packages/core-worker/package.js
+++ packages/core-worker/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:core-worker',
-  version: '0.48.0',
+  version: '0.48.2',
   summary: 'Unchained Engine Core: Worker',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
--- packages/platform/package.js
+++ packages/platform/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:platform',
-  version: '0.48.1',
+  version: '0.48.2',
   summary: 'Unchained Engine',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
