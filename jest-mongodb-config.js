094f23def7c247f221b379bb961ac68b2692aa75
Pascal Kaufmann <kaufmae@me.com>
2020-05-25 15:36:21 +0200

    feat: add codeship building

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -2,12 +2,12 @@ module.exports = {
   mongodbMemoryServerOptions: {
     instance: {
       dbName: 'jest',
-      port: 4011
+      port: 4011,
     },
     binary: {
       version: '4.0.3',
-      skipMD5: true
+      skipMD5: true,
     },
-    autoStart: false
-  }
+    autoStart: false,
+  },
 };
