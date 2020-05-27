aa70136e1e67da0d920700bfca1134460fef4ff0
mikearaya <mikaelaraya12@gmail.com>
2020-05-27 23:37:30 +0300

    Add Query.warehousingprovider and Query.warehousingInterface test

---
+++ tests/warehousing-interface.test.js
@@ -0,0 +1,62 @@
+import {
+  setupDatabase,
+  createLoggedInGraphqlFetch,
+  createAnonymousGraphqlFetch,
+} from "./helpers";
+import { ADMIN_TOKEN } from "./seeds/users";
+
+let connection;
+let graphqlFetch;
+
+describe("WarehousingInterfaces", () => {
+  beforeAll(async () => {
+    [, connection] = await setupDatabase();
+    graphqlFetch = await createLoggedInGraphqlFetch(ADMIN_TOKEN);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  describe("For logged in users", () => {
+    it("should return list of warehousingInterfaces by type", async () => {
+      const {
+        errors,
+        data: { warehousingInterfaces },
+      } = await graphqlFetch({
+        query: /* GraphQL */ `
+          query WarehousingInterfaces($type: WarehousingProviderType!) {
+            warehousingInterfaces(type: $type) {
+              _id
+            }
+          }
+        `,
+        variables: {
+          type: "PHYSICAL",
+        },
+      });
+
+      expect(Array.isArray(warehousingInterfaces)).toBe(true);
+    });
+  });
+
+  describe("For Anonymous user", () => {
+    it("should return error", async () => {
+      const graphqlAnonymousFetch = await createAnonymousGraphqlFetch();
+      const { errors } = await graphqlAnonymousFetch({
+        query: /* GraphQL */ `
+          query WarehousingInterfaces($type: WarehousingProviderType!) {
+            warehousingInterfaces(type: $type) {
+              _id
+            }
+          }
+        `,
+        variables: {
+          type: "PHYSICAL",
+        },
+      });
+
+      expect(errors.length).toEqual(1);
+    });
+  });
+});
