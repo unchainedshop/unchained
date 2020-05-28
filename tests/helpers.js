f077454f22ed0bd6c426213b25f3c464727a22e7
mikearaya <mikaelaraya12@gmail.com>
2020-05-28 17:42:57 +0300

    Add Query.Logs tests

--- tests/helpers.js
+++ tests/helpers.js
@@ -13,6 +13,7 @@ import seedWarehousings from "./seeds/warehousings";
 import seedOrders from "./seeds/orders";
 import seedQuotations from "./seeds/quotations";
 import seedFilters from "./seeds/filters";
+import seedLogs from "./seeds/logs";
 
 Collection.prototype.findOrInsertOne = async function findOrInsertOne(
   doc,
@@ -42,6 +43,7 @@ export const setupDatabase = async () => {
   await seedOrders(db);
   await seedQuotations(db);
   await seedFilters(db);
+  await seedLogs(db);
 
   return [db, connection];
 };
