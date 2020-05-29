a607159bb07c649788624aeabb4d9be0ea24f54d
mikearaya <mikaelaraya12@gmail.com>
2020-05-29 22:31:07 +0300

    Add Mutation.publishProduct & Mutation.uppublishProduct tests

--- tests/products.test.js
+++ tests/products.test.js
@@ -1,10 +1,10 @@
-import { setupDatabase, createLoggedInGraphqlFetch } from './helpers';
-import { ADMIN_TOKEN } from './seeds/users';
-
+import { setupDatabase, createLoggedInGraphqlFetch } from "./helpers";
+import { ADMIN_TOKEN } from "./seeds/users";
+import { SimpleProduct, UnpublishedProduct } from "./seeds/products";
 let connection;
 let graphqlFetch;
 
-describe('Products', () => {
+describe("Products", () => {
   beforeAll(async () => {
     [, connection] = await setupDatabase();
     graphqlFetch = await createLoggedInGraphqlFetch(ADMIN_TOKEN);
@@ -14,8 +14,8 @@ describe('Products', () => {
     await connection.close();
   });
 
-  describe('Mutation.createProduct', () => {
-    it('create a new product', async () => {
+  describe("Mutation.createProduct", () => {
+    it("create a new product", async () => {
       const { data: { createProduct } = {} } = await graphqlFetch({
         query: /* GraphQL */ `
           mutation createProduct($product: CreateProductInput!) {
@@ -46,21 +46,149 @@ describe('Products', () => {
         `,
         variables: {
           product: {
-            title: 'Simple Product',
-            type: 'SimpleProduct',
-            tags: ['simple'],
+            title: "Simple Product",
+            type: "SimpleProduct",
+            tags: ["simple"],
           },
         },
       });
       expect(createProduct).toMatchObject({
-        tags: ['simple'],
-        status: 'DRAFT',
+        tags: ["simple"],
+        status: "DRAFT",
         texts: {
-          title: 'Simple Product',
+          title: "Simple Product",
         },
         catalogPrice: null,
         simulatedPrice: null,
       });
+    });
+  });
+
+  describe("Mutation.unpublishProduct", () => {
+    it("unpublish product", async () => {
+      const { data: { unpublishProduct } = {} } = await graphqlFetch({
+        query: /* GraphQL */ `
+          mutation UnPublishProduct($productId: ID!) {
+            unpublishProduct(productId: $productId) {
+              _id
+              sequence
+              status
+              published
+              tags
+              created
+              updated
+              texts {
+                _id
+              }
+              media {
+                _id
+              }
+              reviews {
+                _id
+              }
+              meta
+              assortmentPaths {
+                assortmentProduct {
+                  _id
+                }
+                links {
+                  assortmentId
+                  link {
+                    _id
+                  }
+                }
+              }
+              siblings {
+                _id
+              }
+            }
+          }
+        `,
+        variables: {
+          productId: SimpleProduct._id,
+        },
+      });
+
+      expect(unpublishProduct.published).toBe(null);
+    });
+    it("return error for non-existing product id", async () => {
+      const { errors = {} } = await graphqlFetch({
+        query: /* GraphQL */ `
+          mutation UnpublishProduct($productId: ID!) {
+            unpublishProduct(productId: $productId) {
+              _id
+            }
+          }
+        `,
+        variables: {
+          productId: "non-existing-id",
+        },
+      });
+      expect(errors.length).toEqual(1);
+    });
+  });
+
+  describe("mutation.publishProduct", () => {
+    xit("publish production", async () => {
+      const { errors = {} } = await graphqlFetch({
+        query: /* GraphQL */ `
+          mutation PublishProduct($productId: ID!) {
+            publishProduct(productId: $productId) {
+              _id
+              sequence
+              status
+              published
+              tags
+              created
+              updated
+              texts {
+                _id
+              }
+              media {
+                _id
+              }
+              reviews {
+                _id
+              }
+              meta
+              assortmentPaths {
+                assortmentProduct {
+                  _id
+                }
+                links {
+                  assortmentId
+                  link {
+                    _id
+                  }
+                }
+              }
+              siblings {
+                _id
+              }
+            }
+          }
+        `,
+        variables: {
+          productId: UnpublishedProduct._id,
+        },
+      });
+      console.log(errors);
+    });
+
+    it("return error for non-existing product id", async () => {
+      const { errors = {} } = await graphqlFetch({
+        query: /* GraphQL */ `
+          mutation PublishProduct($productId: ID!) {
+            publishProduct(productId: $productId) {
+              _id
+            }
+          }
+        `,
+        variables: {
+          productId: "non-existing-id",
+        },
+      });
+      expect(errors.length).toEqual(1);
     });
   });
 });
--- tests/seeds/products.js
+++ tests/seeds/products.js
@@ -1,18 +1,18 @@
 export const SimpleProduct = {
-  _id: 'simpleproduct',
-  created: new Date('2019-07-30T09:23:26.253+0000'),
-  type: 'SIMPLE_PRODUCT',
-  status: 'ACTIVE',
+  _id: "simpleproduct",
+  created: new Date("2019-07-30T09:23:26.253+0000"),
+  type: "SIMPLE_PRODUCT",
+  status: "ACTIVE",
   sequence: 0,
-  authorId: 'admin',
-  slugs: ['old-slug-de', 'slug-de', 'slug-fr'],
-  updated: new Date('2019-09-10T14:29:37.015+0000'),
-  published: new Date('2019-07-30T09:23:57.329+0000'),
+  authorId: "admin",
+  slugs: ["old-slug-de", "slug-de", "slug-fr"],
+  updated: new Date("2019-09-10T14:29:37.015+0000"),
+  published: new Date("2019-07-30T09:23:57.329+0000"),
   warehousing: {
-    sku: 'SKU',
-    baseUnit: 'ST',
+    sku: "SKU",
+    baseUnit: "ST",
   },
-  tags: ['tag-1', 'tag-2', 'highlight'],
+  tags: ["tag-1", "tag-2", "highlight"],
   commerce: {
     pricing: [
       {
@@ -20,8 +20,8 @@ export const SimpleProduct = {
         maxQuantity: 0,
         isTaxable: true,
         isNetPrice: false,
-        currencyCode: 'CHF',
-        countryCode: 'CH',
+        currencyCode: "CHF",
+        countryCode: "CH",
       },
     ],
   },
@@ -33,16 +33,32 @@ export const SimpleProduct = {
   },
 };
 
-export const PlanProduct = {
-  _id: 'plan-product',
-  created: new Date('2019-07-30T09:23:26.253+0000'),
-  type: 'PLAN_PRODUCT',
-  status: 'ACTIVE',
+export const UnpublishedProduct = {
+  _id: "un-published",
   sequence: 0,
-  authorId: 'admin',
-  slugs: ['plan'],
-  updated: new Date('2019-09-10T14:29:37.015+0000'),
-  published: new Date('2019-07-30T09:23:57.329+0000'),
+  status: "DRAFT",
+  published: null,
+  tags: ["tag-1", "tag-2", "highlight"],
+  created: 1564478606253,
+  updated: 1590774989680,
+  texts: { _id: "german" },
+  media: [{ _id: "jpeg-product" }],
+  reviews: [{ _id: "product-review" }],
+  meta: null,
+  assortmentPaths: [],
+  siblings: [],
+};
+
+export const PlanProduct = {
+  _id: "plan-product",
+  created: new Date("2019-07-30T09:23:26.253+0000"),
+  type: "PLAN_PRODUCT",
+  status: "ACTIVE",
+  sequence: 0,
+  authorId: "admin",
+  slugs: ["plan"],
+  updated: new Date("2019-09-10T14:29:37.015+0000"),
+  published: new Date("2019-07-30T09:23:57.329+0000"),
   commerce: {
     pricing: [
       {
@@ -50,143 +66,145 @@ export const PlanProduct = {
         maxQuantity: 0,
         isTaxable: true,
         isNetPrice: false,
-        currencyCode: 'CHF',
-        countryCode: 'CH',
+        currencyCode: "CHF",
+        countryCode: "CH",
       },
     ],
   },
   plan: {
-    billingInterval: 'WEEK',
+    billingInterval: "WEEK",
     billingIntervalCount: 1,
-    usageCalculationType: 'LICENSED',
+    usageCalculationType: "LICENSED",
     trialInterval: null,
     trialIntervalCount: null,
   },
 };
 
 export const GermanProductText = {
-  _id: 'german',
-  locale: 'de',
-  productId: 'simpleproduct',
-  slug: 'slug-de',
-  title: 'simple product title de',
-  updated: new Date('2019-09-10T14:28:46.103+0000'),
-  brand: 'brand-de',
-  description: 'text-de',
-  labels: ['label-de-1', 'label-de-2'],
-  subtitle: 'subtitle-de',
-  vendor: 'vendor-de',
+  _id: "german",
+  locale: "de",
+  productId: "simpleproduct",
+  slug: "slug-de",
+  title: "simple product title de",
+  updated: new Date("2019-09-10T14:28:46.103+0000"),
+  brand: "brand-de",
+  description: "text-de",
+  labels: ["label-de-1", "label-de-2"],
+  subtitle: "subtitle-de",
+  vendor: "vendor-de",
 };
 
 export const GermanPlanProductText = {
   ...GermanProductText,
-  productId: 'plan-product',
-  slug: 'plan',
-  title: 'weekly subscription',
+  productId: "plan-product",
+  slug: "plan",
+  title: "weekly subscription",
 };
 
 export const FrenchProductText = {
-  _id: 'french',
-  locale: 'fr',
-  productId: 'simpleproduct',
-  labels: ['label-fr-1'],
-  slug: 'slug-fr',
-  title: 'simple product title fr',
-  updated: new Date('2019-09-10T14:28:46.105+0000'),
-  brand: 'brand-fr-1',
-  description: 'text-fr-1',
-  subtitle: 'subtitle-fr',
-  vendor: 'vendor-fr-1',
+  _id: "french",
+  locale: "fr",
+  productId: "simpleproduct",
+  labels: ["label-fr-1"],
+  slug: "slug-fr",
+  title: "simple product title fr",
+  updated: new Date("2019-09-10T14:28:46.105+0000"),
+  brand: "brand-fr-1",
+  description: "text-fr-1",
+  subtitle: "subtitle-fr",
+  vendor: "vendor-fr-1",
 };
 
 export const JpegMedia = {
-  _id: 'jpeg',
-  name: 'iu.jpeg',
-  extension: 'jpeg',
-  ext: 'jpeg',
-  extensionWithDot: '.jpeg',
-  path: 'assets/app/uploads/media/jpeg.jpeg',
+  _id: "jpeg",
+  name: "iu.jpeg",
+  extension: "jpeg",
+  ext: "jpeg",
+  extensionWithDot: ".jpeg",
+  path: "assets/app/uploads/media/jpeg.jpeg",
   meta: {},
-  type: 'image/jpeg',
-  mime: 'image/jpeg',
-  'mime-type': 'image/jpeg',
+  type: "image/jpeg",
+  mime: "image/jpeg",
+  "mime-type": "image/jpeg",
   size: 1745971,
-  userId: 'admin',
+  userId: "admin",
   versions: {
     original: {
-      path: 'assets/app/uploads/media/jpeg.jpeg',
+      path: "assets/app/uploads/media/jpeg.jpeg",
       size: 1745971,
-      type: 'image/jpeg',
-      extension: 'jpeg',
+      type: "image/jpeg",
+      extension: "jpeg",
     },
   },
-  _downloadRoute: '/cdn/storage',
-  _collectionName: 'media',
+  _downloadRoute: "/cdn/storage",
+  _collectionName: "media",
   isVideo: false,
   isAudio: false,
   isImage: true,
   isText: false,
   isJSON: false,
   isPDF: false,
-  _storagePath: 'assets/app/uploads/media',
+  _storagePath: "assets/app/uploads/media",
 };
 
 export const JpegProductMedia = {
-  _id: 'jpeg-product',
-  mediaId: 'jpeg',
+  _id: "jpeg-product",
+  mediaId: "jpeg",
   tags: [],
   sortKey: 1,
-  productId: 'simpleproduct',
-  created: new Date('2019-09-10T14:29:01.093+0000'),
+  productId: "simpleproduct",
+  created: new Date("2019-09-10T14:29:01.093+0000"),
 };
 
 export const GermanJpegProductMediaText = {
-  _id: 'german-jpeg-product',
-  locale: 'de',
-  productMediaId: 'jpeg-product',
-  subtitle: 'product-media-subtitle-de',
-  title: 'product-media-title-de',
-  updated: new Date('2019-09-10T14:42:16.175+0000'),
+  _id: "german-jpeg-product",
+  locale: "de",
+  productMediaId: "jpeg-product",
+  subtitle: "product-media-subtitle-de",
+  title: "product-media-title-de",
+  updated: new Date("2019-09-10T14:42:16.175+0000"),
 };
 
 export const FrenchJpegProductMediaText = {
-  _id: 'french-jpeg-product',
-  locale: 'fr',
-  productMediaId: 'jpeg-product',
-  subtitle: 'product-media-subtitle-fr',
-  title: 'product-media-title-fr',
-  updated: new Date('2019-09-10T14:42:16.177+0000'),
+  _id: "french-jpeg-product",
+  locale: "fr",
+  productMediaId: "jpeg-product",
+  subtitle: "product-media-subtitle-fr",
+  title: "product-media-title-fr",
+  updated: new Date("2019-09-10T14:42:16.177+0000"),
 };
 
 export const SimpleProductReview = {
-  _id: 'product-review',
-  productId: 'simpleproduct',
-  authorId: 'admin',
+  _id: "product-review",
+  productId: "simpleproduct",
+  authorId: "admin",
   rating: 2,
-  title: 'Title of my Review',
-  review: 'Explanatory comment why I love or hate this product',
-  updated: new Date('2019-09-10T14:42:16.177+0000'),
-  created: new Date('2019-09-10T14:42:16.177+0000'),
+  title: "Title of my Review",
+  review: "Explanatory comment why I love or hate this product",
+  updated: new Date("2019-09-10T14:42:16.177+0000"),
+  created: new Date("2019-09-10T14:42:16.177+0000"),
   votes: [],
 };
 
 export default async function seedProducts(db) {
-  await db.collection('product_texts').createIndex({
-    title: 'text',
+  await db.collection("product_texts").createIndex({
+    title: "text",
   });
-  await db.collection('products').findOrInsertOne(SimpleProduct);
-  await db.collection('product_reviews').findOrInsertOne(SimpleProductReview);
-  await db.collection('product_texts').findOrInsertOne(GermanProductText);
-  await db.collection('product_texts').findOrInsertOne(FrenchProductText);
-  await db.collection('product_media').findOrInsertOne(JpegProductMedia);
   await db
-    .collection('product_media_texts')
+    .collection("products")
+    .insertMany([SimpleProduct, UnpublishedProduct]);
+  await db.collection("product_reviews").findOrInsertOne(SimpleProductReview);
+  await db.collection("product_texts").findOrInsertOne(GermanProductText);
+  await db.collection("product_texts").findOrInsertOne(FrenchProductText);
+  await db.collection("product_media").findOrInsertOne(JpegProductMedia);
+  await db
+    .collection("product_media_texts")
     .findOrInsertOne(GermanJpegProductMediaText);
   await db
-    .collection('product_media_texts')
+    .collection("product_media_texts")
     .findOrInsertOne(FrenchJpegProductMediaText);
 
-  await db.collection('media').findOrInsertOne(JpegMedia);
-  await db.collection('products').findOrInsertOne(PlanProduct);
-  await db.collection('product_texts').findOrInsertOne(GermanPlanProductText);
+  await db.collection("media").findOrInsertOne(JpegMedia);
+  await db.collection("products").findOrInsertOne(PlanProduct);
+  await db.collection("product_texts").findOrInsertOne(GermanPlanProductText);
 }
