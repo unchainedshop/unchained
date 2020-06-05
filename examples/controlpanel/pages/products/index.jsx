07a6e488d3cac26499caf6bb33f2eb7ab0659cac
Harry Adel <harryadelb@gmail.com>
2020-06-05 07:30:25 +0200

    Add products search feature

--- examples/controlpanel/pages/products/index.jsx
+++ examples/controlpanel/pages/products/index.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import { Container } from 'semantic-ui-react';
-import App from '../../components/App';
-import ProductList from '../../components/products/ProductList';
-import connectApollo from '../../lib/connectApollo';
+import React from "react";
+import { Container } from "semantic-ui-react";
+import { withRouter } from "next/router";
+import App from "../../components/App";
+import ProductList from "../../components/products/ProductList";
+import connectApollo from "../../lib/connectApollo";
 
-export default connectApollo((props) => (
-  <App {...props}>
-    <Container>
-      <h2>Products</h2>
-      <ProductList />
-    </Container>
-  </App>
-));
+export default connectApollo(
+  withRouter(({ router, ...rest }) => {
+    return (
+      <App {...rest}>
+        <Container>
+          <h2>Products</h2>
+          <ProductList router={router}/>
+        </Container>
+      </App>
+    );
+  })
+);
