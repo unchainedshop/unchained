a3ac99e522e4de473cd898e67db27b53a4492baf
mikearaya <mikaelaraya12@gmail.com>
2020-06-05 17:45:04 +0300

    Add mutation.addProductAssignment tests

--- tests/seeds/products.js
+++ tests/seeds/products.js
@@ -33,6 +33,41 @@ export const SimpleProduct = {
   },
 };
 
+export const ConfigurableProduct = {
+  _id: 'configurable-product-id',
+  created: new Date('2019-07-30T09:23:26.253+0000'),
+  type: 'CONFIGURABLE_PRODUCT',
+  status: 'ACTIVE',
+  sequence: 0,
+  authorId: 'admin',
+  slugs: ['old-slug-de', 'slug-de', 'slug-fr'],
+  updated: new Date('2019-09-10T14:29:37.015+0000'),
+  published: new Date('2019-07-30T09:23:57.329+0000'),
+  warehousing: {
+    sku: 'SKU',
+    baseUnit: 'ST',
+  },
+  tags: ['tag-1', 'tag-2', 'highlight'],
+  commerce: {
+    pricing: [
+      {
+        amount: 10000,
+        maxQuantity: 0,
+        isTaxable: true,
+        isNetPrice: false,
+        currencyCode: 'CHF',
+        countryCode: 'CH',
+      },
+    ],
+  },
+  supply: {
+    weightInGram: 1570,
+    heightInMillimeters: 250,
+    lengthInMillimeters: 300,
+    widthInMillimeters: 400,
+  },
+};
+
 export const SimpleProductBundle = {
   _id: 'simpleproduct-bundle',
   created: new Date('2019-07-30T09:23:26.253+0000'),
@@ -245,7 +280,12 @@ export default async function seedProducts(db) {
   });
   await db
     .collection('products')
-    .insertMany([SimpleProduct, UnpublishedProduct, SimpleProductBundle]);
+    .insertMany([
+      SimpleProduct,
+      UnpublishedProduct,
+      SimpleProductBundle,
+      ConfigurableProduct,
+    ]);
   await db.collection('product_reviews').findOrInsertOne(SimpleProductReview);
   await db.collection('product_texts').findOrInsertOne(GermanProductText);
   await db.collection('product_texts').findOrInsertOne(FrenchProductText);
