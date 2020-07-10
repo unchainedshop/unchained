6ec229f21a1a0e1fe8f15e13d967f80c0a0dae06
Pascal Kaufmann <kaufmae@me.com>
2020-07-10 15:35:24 +0200

    Update Unchained to 0.51.0

--- packages/core-documents/package.js
+++ packages/core-documents/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:core-documents',
-  version: '0.50.2',
+  version: '0.51.0',
   summary: 'Unchained Engine Core: Documents',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
@@ -10,8 +10,8 @@ Package.onUse((api) => {
   api.versionsFrom('1.10');
   api.use('ecmascript');
   api.use('http');
-  api.use('unchained:utils@0.50.2');
-  api.use('unchained:core-logger@0.50.2');
+  api.use('unchained:utils@0.51.0');
+  api.use('unchained:core-logger@0.51.0');
 
   api.mainModule('documents.js', 'server');
 });
