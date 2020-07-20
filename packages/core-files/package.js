cffe7202e01513d7f37551bb0f73103dd6b716ec
Pascal Kaufmann <kaufmae@me.com>
2020-07-20 09:46:22 +0200

    fix: the new version of ostrio:files (1.14.2) checks for storagePath even when gridfs is selected as storage provider

--- packages/core-files/package.js
+++ packages/core-files/package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'unchained:core-files',
-  version: '0.51.1',
+  version: '0.51.2',
   summary: 'Unchained Engine Core: Files',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
 });
 
 Npm.depends({
-  'lodash.merge': '4.6.1',
+  'lodash.merge': '4.6.2',
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('1.8.0.2');
+  api.versionsFrom('1.10.2');
   api.use('ostrio:files@1.14.2');
   api.use('ecmascript');
   api.use('unchained:core-settings@0.51.0');
