9952ffbc74bc73711edbcfd0ba7b82567e3770e6
Harry Adel <harryadelb@gmail.com>
2020-07-31 10:31:55 +0200

    Enhance search dropdown items

--- examples/controlpanel/components/products/ProductSearchDropdown.jsx
+++ examples/controlpanel/components/products/ProductSearchDropdown.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { Search } from 'semantic-ui-react';
-import { debounce } from 'lodash';
+import { debounce, has } from 'lodash';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
@@ -14,6 +14,16 @@ const SEARCH_PRODUCTS = gql`
         texts {
           _id
           title
+          description
+        }
+        media {
+          texts {
+            title
+          }
+          file {
+            url
+            name
+          }
         }
       }
     }
@@ -36,12 +46,51 @@ const ProductSearchDropdown = () => {
   const handleSearchChange = (e, { value }) => {
     setQueryString(value);
   };
-  const resultRenderer = (result) => <div>{result.texts.title}</div>;
+
+  const pollyfillImageUrl = 'https://via.placeholder.com/150';
+
+  const selectImage = (result) => {
+    if (!result.media) return pollyfillImageUrl;
+
+    const foundImageObj = result.media.find((mediaObj) => {
+      if (has(mediaObj, 'file.url')) {
+        const imageObj = new Image();
+        imageObj.src = mediaObj.file.url;
+        return imageObj.complete;
+      }
+      return false;
+    });
+    return (
+      <img
+        src={
+          (foundImageObj && foundImageObj.file && foundImageObj.file.url) ||
+          pollyfillImageUrl
+        }
+        alt={
+          (foundImageObj && foundImageObj.file && foundImageObj.texts.title) ||
+          (foundImageObj && foundImageObj.file && foundImageObj.file.name) || // a user-uploaded image is always accompained by name otherwise it's the pollyfill image
+          'Placeholder Image'
+        }
+      />
+    );
+  };
+
+  const resultRenderer = (result) => {
+    return (
+      <div className="result">
+        <div className="image">{selectImage(result)}</div>
+        <div className="content">
+          <div className="title">{result.texts.title}</div>
+          <div className="description">{result.texts.description}</div>
+        </div>
+      </div>
+    );
+  };
   const results = data?.search?.products || [];
   return (
     <Search
-      style={{ float: 'left' }}
       loading={loading}
+      style={{ float: 'left' }}
       onResultSelect={handleResultSelect}
       onSearchChange={debounce(handleSearchChange, 500, {
         leading: true,
