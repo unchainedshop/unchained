d67d257bc9e242de37f73d11ea3d287f6b004d6f
Harry Adel <harryadelb@gmail.com>
2020-08-03 11:30:20 +0200

    Add search capability to assortment products select

--- examples/controlpanel/components/assortments/FormNewAssortmentProduct.jsx
+++ examples/controlpanel/components/assortments/FormNewAssortmentProduct.jsx
@@ -1,16 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { compose, mapProps, withHandlers } from 'recompose';
-import { Segment } from 'semantic-ui-react';
+import {
+  Segment,
+  Dropdown,
+  Header,
+  Image as SemanticImage,
+} from 'semantic-ui-react';
 import { withRouter } from 'next/router';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { debounce, has } from 'lodash';
+import { useQuery } from '@apollo/react-hooks';
+import { connectField } from 'uniforms';
 import AutoField from 'uniforms-semantic/AutoField';
 import SubmitField from 'uniforms-semantic/SubmitField';
 import ErrorsField from 'uniforms-semantic/ErrorsField';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import withFormSchema from '../../lib/withFormSchema';
 import withFormErrorHandlers from '../../lib/withFormErrorHandlers';
+
+const SEARCH_PRODUCTS = gql`
+  query search($queryString: String) {
+    search(queryString: $queryString, includeInactive: true) {
+      totalProducts
+      products {
+        _id
+        texts {
+          _id
+          title
+          description
+        }
+        media {
+          texts {
+            title
+          }
+          file {
+            url
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+const ProductSearchDropdown = ({ onChange, value }) => {
+  const [queryString, setQueryString] = useState('');
+  const { data, loading } = useQuery(SEARCH_PRODUCTS, {
+    variables: {
+      queryString,
+    },
+    skip: !queryString,
+  });
+
+  const handleOnChange = (e, result) => {
+    onChange(result.value);
+  };
+
+  const handleSearchChange = (e, result) => {
+    setQueryString(result.searchQuery);
+  };
+
+  const pollyfillImageUrl = 'https://via.placeholder.com/150';
+
+  const selectImage = (result) => {
+    if (!result.media) return pollyfillImageUrl;
+
+    const foundImageObj = result.media.find((mediaObj) => {
+      if (has(mediaObj, 'file.url')) {
+        const imageObj = new Image();
+        imageObj.src = mediaObj.file.url;
+        return imageObj.complete;
+      }
+      return false;
+    });
+    return (
+      (foundImageObj && foundImageObj.file && foundImageObj.file.url) ||
+      pollyfillImageUrl
+    );
+  };
+
+  const options =
+    data?.search?.products.map((opt) => {
+      return {
+        key: opt.texts._id,
+        value: opt.texts._id,
+        text: opt.texts.title,
+        content: (
+          <Header>
+            <SemanticImage src={selectImage(opt.media)} />
+            <Header.Content>
+              {opt.texts.title}
+              <Header.Subheader>{opt.texts.description}</Header.Subheader>
+            </Header.Content>
+          </Header>
+        ),
+      };
+    }) || [];
+  return (
+    <Dropdown
+      search
+      fluid
+      clearable
+      selection
+      placeholder="Select Product"
+      name="product"
+      loading={loading}
+      onChange={handleOnChange}
+      onSearchChange={debounce(handleSearchChange, 500, {
+        leading: true,
+      })}
+      options={options}
+      value={value}
+      style={{ marginBottom: '1em' }}
+    />
+  );
+};
+
+const ProductSearchDropdownField = connectField(ProductSearchDropdown);
 
 const FormNewAssortmentProduct = ({
   products,
@@ -68,6 +176,9 @@ export default compose(
       type: String,
       optional: false,
       label: 'Product',
+      uniforms: {
+        component: ProductSearchDropdownField,
+      },
     },
   }),
   withHandlers({
