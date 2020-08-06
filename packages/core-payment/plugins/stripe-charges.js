9e9453735109f5b0a6b049b80f901ade7de55db9
Pascal Kaufmann <kaufmae@me.com>
2020-08-06 13:58:11 +0200

    Re-Add legacy Stripe Plugin
    PaymentIntent Support is limited to cards at the moment. If you want to accept Apple Pay, Alipay etc. you will still need to use this plugin atm.

---
+++ packages/core-payment/plugins/stripe-charges.js
@@ -0,0 +1,62 @@
+import {
+  PaymentDirector,
+  PaymentAdapter,
+  PaymentError,
+} from 'meteor/unchained:core-payment';
+
+const { STRIPE_SECRET, EMAIL_WEBSITE_NAME } = process.env;
+
+class Stripe extends PaymentAdapter {
+  static key = 'shop.unchained.payment.stripe-charges';
+
+  static label = 'Stripe';
+
+  static version = '1.0';
+
+  static initialConfiguration = [];
+
+  static typeSupported(type) {
+    return type === 'CARD';
+  }
+
+  configurationError() {
+    // eslint-disable-line
+    if (!STRIPE_SECRET) {
+      return PaymentError.INCOMPLETE_CONFIGURATION;
+    }
+    if (this.wrongCredentials) {
+      return PaymentError.WRONG_CREDENTIALS;
+    }
+    return null;
+  }
+
+  isActive() {
+    // eslint-disable-line
+    if (this.configurationError() === null) return true;
+    return false;
+  }
+
+  isPayLaterAllowed() {
+    // eslint-disable-line
+    return false;
+  }
+
+  async charge({ stripeToken, stripeCustomerId } = {}) {
+    if (!stripeToken)
+      throw new Error('You have to provide stripeToken in paymentContext');
+    const StripeAPI = require('stripe'); // eslint-disable-line
+    const stripe = StripeAPI(STRIPE_SECRET);
+    const pricing = this.context.order.pricing();
+    const stripeChargeReceipt = await stripe.charges.create({
+      amount: Math.round(pricing.total().amount),
+      currency: this.context.order.currency.toLowerCase(),
+      description: `${EMAIL_WEBSITE_NAME} Order #${this.context.order._id}`,
+      source: stripeToken.id,
+      customer: stripeCustomerId,
+    });
+    this.log('Stripe -> ', stripeToken, stripeChargeReceipt);
+    return stripeChargeReceipt;
+  }
+}
+
+PaymentDirector.registerAdapter(Stripe);
