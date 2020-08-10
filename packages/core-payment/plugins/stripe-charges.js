e8ffa95d3e4c35e9b8abb6d25bcedb46d15e272d
Pascal Kaufmann <kaufmae@me.com>
2020-08-10 17:36:08 +0200

    Add webhook

--- packages/core-payment/plugins/stripe-charges.js
+++ packages/core-payment/plugins/stripe-charges.js
@@ -3,8 +3,72 @@ import {
   PaymentAdapter,
   PaymentError,
 } from 'meteor/unchained:core-payment';
+import { createLogger } from 'meteor/unchained:core-logger';
+import bodyParser from 'body-parser';
+import { OrderPayments } from 'meteor/unchained:core-orders';
 
-const { STRIPE_SECRET, EMAIL_WEBSITE_NAME } = process.env;
+const {
+  STRIPE_SECRET,
+  STRIPE_ENDPOINT_SECRET,
+  EMAIL_WEBSITE_NAME,
+  STRIPE_WEBHOOK_PATH = '/graphql/stripe-charges',
+} = process.env;
+
+/*
+Test Webhooks:
+
+brew install stripe/stripe-cli/stripe
+stripe login --api-key sk_....
+stripe listen --forward-to http://localhost:3000/graphql/stripe
+stripe trigger payment_intent.succeeded
+*/
+
+const logger = createLogger('unchained:core-payment:stripe-webhook');
+
+const stripe = require('stripe')(STRIPE_SECRET);
+
+WebApp.connectHandlers.use(
+  STRIPE_WEBHOOK_PATH,
+  bodyParser.raw({ type: 'application/json' }),
+);
+
+WebApp.connectHandlers.use(STRIPE_WEBHOOK_PATH, (request, response) => {
+  const sig = request.headers['stripe-signature'];
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      request.body,
+      sig,
+      STRIPE_ENDPOINT_SECRET,
+    );
+  } catch (err) {
+    response.writeHead(400);
+    return response.end(`Webhook Error: ${err.message}`);
+  }
+
+  if (event.type === 'source.chargeable') {
+    const source = event.data.object;
+    // eslint-disable-next-line
+    const orderPaymentId = source.request?.idempotency_key;
+    const orderPayment = OrderPayments.findOne({ _id: orderPaymentId });
+    const order = orderPayment.order().checkout({
+      paymentContext: {
+        stripeToken: source,
+      },
+    });
+    logger.info(
+      `Stripe Webhook: Unchained confirmed checkout for order ${order.orderNumber}`,
+      { orderId: order._id },
+    );
+  } else {
+    response.writeHead(400);
+    return response.end();
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  return response.end(JSON.stringify({ received: true }));
+});
 
 class Stripe extends PaymentAdapter {
   static key = 'shop.unchained.payment.stripe-charges';
@@ -44,8 +108,6 @@ class Stripe extends PaymentAdapter {
   async charge({ stripeToken, stripeCustomerId } = {}) {
     if (!stripeToken)
       throw new Error('You have to provide stripeToken in paymentContext');
-    const StripeAPI = require('stripe'); // eslint-disable-line
-    const stripe = StripeAPI(STRIPE_SECRET);
     const pricing = this.context.order.pricing();
     const stripeChargeReceipt = await stripe.charges.create({
       amount: Math.round(pricing.total().amount),
