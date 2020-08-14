39bb947f621c6737bc3bd54d18417a7ac809f91a
Pascal Kaufmann <kaufmae@me.com>
2020-08-14 08:58:19 +0200

    Patch release of filters to 0.51.7

--- packages/core-filters/package.js
+++ packages/core-filters/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:core-filters',
-  version: '0.51.6',
+  version: '0.51.7',
   summary: 'Unchained Engine Core: Filters',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
