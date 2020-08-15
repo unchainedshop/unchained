952cb5535c73eb29a12947da2a3af69503133f08
Harry Adel <harryadelb@gmail.com>
2020-08-15 22:58:27 +0200

    Create Cytoscape component

---
+++ examples/controlpanel/components/assortments/CytoscapeComponent.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Cytoscape from 'cytoscape';
+import CytoscapeComponent from 'react-cytoscapejs';
+import dagre from 'cytoscape-dagre';
+
+Cytoscape.use(dagre);
+
+const layout = { name: 'dagre', fit: true };
+
+const CytoscapeComponentWrapper = ({data}) => {
+  const elements = [];
+
+  data.assortments?.forEach((item) => {
+    elements.push({
+      data: {
+        id: item._id,
+        label: item.texts.title,
+      },
+    });
+    if (item.linkedAssortments) {
+      item.linkedAssortments.forEach(({ parent, child }) => {
+        elements.push({
+          data: {
+            source: parent._id,
+            target: child._id,
+          },
+        });
+      });
+    }
+  });
+
+  const stylesheet = [
+    {
+      selector: 'node',
+      style: {
+        content: 'data(label)',
+        'text-opacity': 0.5,
+        'text-valign': 'center',
+        'text-halign': 'right',
+        'background-color': '#11479e',
+      },
+    },
+
+    {
+      selector: 'edge',
+      style: {
+        width: 4,
+        'target-arrow-shape': 'triangle',
+        'line-color': '#9dbaea',
+        'target-arrow-color': '#9dbaea',
+        'curve-style': 'bezier',
+      },
+    },
+  ];
+  return (
+    <CytoscapeComponent
+      elements={elements}
+      layout={layout}
+      style={{ width: '100%', height: '600px' }}
+      stylesheet={stylesheet}
+      zoom={2}
+    />
+  );
+};
+
+export default CytoscapeComponentWrapper;
