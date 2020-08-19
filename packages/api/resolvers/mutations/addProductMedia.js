c905912873ab6dd7b4f10b12cfbaada22af9a0c6
mikearaya <mikaelaraya12@gmail.com>
2020-08-19 16:37:20 +0300

    Add invalid product id check for mutation.addProductMedia

--- packages/api/resolvers/mutations/addProductMedia.js
+++ packages/api/resolvers/mutations/addProductMedia.js
@@ -1,10 +1,10 @@
 import { log } from 'meteor/unchained:core-logger';
 import { Products } from 'meteor/unchained:core-products';
-import { ProductNotFoundError } from '../../errors';
+import { ProductNotFoundError, InvalidIdError } from '../../errors';
 
 export default function (root, { media, productId }, { userId }) {
   log(`mutation addProductMedia ${productId}`, { userId });
-  if (!productId) throw new Error('Invalid product ID provided');
+  if (!productId) throw new InvalidIdError({ productId });
   const product = Products.findOne({ _id: productId });
   if (!product) throw new ProductNotFoundError({ productId });
   const productMedia = product.addMedia({ rawFile: media, authorId: userId });
