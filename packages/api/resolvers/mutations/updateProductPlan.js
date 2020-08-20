d6c4f49cbaa296b75c217cdef3a5a1e47733de57
mikearaya <mikaelaraya12@gmail.com>
2020-08-20 19:30:44 +0300

    Add productId check for mutation.updateProductPlan

--- packages/api/resolvers/mutations/updateProductPlan.js
+++ packages/api/resolvers/mutations/updateProductPlan.js
@@ -1,10 +1,10 @@
 import { log } from 'meteor/unchained:core-logger';
 import { Products } from 'meteor/unchained:core-products';
-import { ProductNotFoundError } from '../../errors';
+import { ProductNotFoundError, InvalidIdError } from '../../errors';
 
 export default function (root, { plan, productId }, { userId }) {
   log(`mutation updateProductPlan ${productId}`, { userId });
-  if (!productId) throw new Error('invalid product ID provided');
+  if (!productId) throw new InvalidIdError({ productId });
   const productObject = Products.updateProduct({ productId, plan });
   if (!productObject) throw new ProductNotFoundError({ productId });
   return productObject;
