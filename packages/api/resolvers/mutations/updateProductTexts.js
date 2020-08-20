6214cd86ee7be606defd46032867aca9d2299684
mikearaya <mikaelaraya12@gmail.com>
2020-08-20 19:41:50 +0300

    Add productId check and update tests for mutation.updateProductTexts

--- packages/api/resolvers/mutations/updateProductTexts.js
+++ packages/api/resolvers/mutations/updateProductTexts.js
@@ -1,9 +1,10 @@
 import { log } from 'meteor/unchained:core-logger';
 import { Products } from 'meteor/unchained:core-products';
-import { ProductNotFoundError } from '../../errors';
+import { ProductNotFoundError, InvalidIdError } from '../../errors';
 
 export default function (root, { texts, productId }, { userId }) {
   log(`mutation updateProductTexts ${productId}`, { userId });
+  if (!productId) throw new InvalidIdError({ productId });
   const product = Products.findOne({ _id: productId });
   if (!product) throw new ProductNotFoundError({ productId });
   const changedLocalizations = texts.map(({ locale, ...localizations }) =>
