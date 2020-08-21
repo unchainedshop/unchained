36713fecbc1cbcab491dafb2cedbb984ffc2a44c
mikearaya <mikaelaraya12@gmail.com>
2020-08-21 21:13:34 +0300

    Add workId check for query.work

--- packages/api/resolvers/queries/work.js
+++ packages/api/resolvers/queries/work.js
@@ -1,11 +1,11 @@
 import { log } from 'meteor/unchained:core-logger';
 import { WorkerDirector } from 'meteor/unchained:core-worker';
-import { WorkNotFoundError } from '../../errors';
+import { WorkNotFoundError, InvalidIdError } from '../../errors';
 
 export default function (root, { workId }, { userId }) {
   log(`query work ${workId}`, { userId });
 
-  if (!workId) throw new Error('Invalid work ID provided');
+  if (!workId) throw new InvalidIdError({ workId });
   const work = WorkerDirector.work({ workId });
   if (!work) throw new WorkNotFoundError({ workId });
 
