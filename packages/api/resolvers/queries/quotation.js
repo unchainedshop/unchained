4c7eb8892095b4898b52a1015d3fda08169b3b6c
mikearaya <mikaelaraya12@gmail.com>
2020-08-21 20:49:38 +0300

    Add quotationId check and update tests for query.quotation

--- packages/api/resolvers/queries/quotation.js
+++ packages/api/resolvers/queries/quotation.js
@@ -1,12 +1,11 @@
 import { log } from 'meteor/unchained:core-logger';
 import { Quotations } from 'meteor/unchained:core-quotations';
-import { QuotationNotFoundError } from '../../errors';
+import { QuotationNotFoundError, InvalidIdError } from '../../errors';
 
 export default function (root, { quotationId }, { userId }) {
   log(`query quotation ${quotationId}`, { userId, quotationId });
 
-  if (!quotationId) throw new Error('Invalid quotation ID provided');
-
+  if (!quotationId) throw new InvalidIdError({ quotationId });
   const selector = { _id: quotationId };
   const quotation = Quotations.findOne(selector);
 
