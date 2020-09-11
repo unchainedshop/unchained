214f60ffaebfc1ba6b20690c31e2e56ccf5724b0
Harry Adel <harryadelb@gmail.com>
2020-09-11 15:59:04 +0200

    Replace assortment linking dropdown with an searchdropdown

--- examples/controlpanel/components/assortments/FormNewAssortmentLink.jsx
+++ examples/controlpanel/components/assortments/FormNewAssortmentLink.jsx
@@ -4,6 +4,7 @@ import { compose, mapProps, withHandlers } from 'recompose';
 import { Segment } from 'semantic-ui-react';
 import { withRouter } from 'next/router';
 import gql from 'graphql-tag';
+import { connectField } from 'uniforms';
 import { graphql } from '@apollo/client/react/hoc';
 import AutoField from 'uniforms-semantic/AutoField';
 import SubmitField from 'uniforms-semantic/SubmitField';
@@ -11,37 +12,36 @@ import ErrorsField from 'uniforms-semantic/ErrorsField';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import withFormSchema from '../../lib/withFormSchema';
 import withFormErrorHandlers from '../../lib/withFormErrorHandlers';
+import SearchDropdown from '../SearchDropdown';
 import FormTagInput from '../FormTagInput';
+
+const AssortmentSearchDropdownField = connectField(SearchDropdown);
 
 const FormNewAssortmentLink = ({
   assortments,
   removeCountry,
   ...formProps
-}) => (
-  <AutoForm {...formProps}>
-    <Segment basic>
-      <AutoField name={'parentAssortmentId'} type="hidden" />
-      <AutoField name={'childAssortmentId'} options={assortments} />
-      <AutoField name="tags" component={FormTagInput} options={[]} />
-      <ErrorsField />
-      <SubmitField value="Add Link" className="primary" />
-    </Segment>
-  </AutoForm>
-);
+}) => {
+  return (
+    <AutoForm {...formProps}>
+      <Segment basic>
+        <AutoField name={'parentAssortmentId'} type="hidden" />
+        <AutoField
+          name={'childAssortmentId'}
+          queryType={'assortments'}
+          filterIds={[formProps.model.parentAssortmentId]}
+          uniforms
+        />
+        <AutoField name="tags" component={FormTagInput} options={[]} />
+        <ErrorsField />
+        <SubmitField value="Add Link" className="primary" />
+      </Segment>
+    </AutoForm>
+  );
+};
 
 export default compose(
   withRouter,
-  graphql(gql`
-    query assortments {
-      assortments(offset: 0, limit: 0, includeLeaves: true) {
-        _id
-        texts {
-          _id
-          title
-        }
-      }
-    }
-  `),
   graphql(
     gql`
       mutation addAssortmentLink(
@@ -75,6 +75,9 @@ export default compose(
       type: String,
       optional: false,
       label: 'Subassortment',
+      uniforms: {
+        component: AssortmentSearchDropdownField,
+      },
     },
     tags: {
       type: Array,
@@ -102,25 +105,10 @@ export default compose(
     },
   }),
   withFormErrorHandlers,
-  mapProps(
-    ({
+  mapProps(({ parentAssortmentId, addAssortmentLink, ...rest }) => ({
+    model: {
       parentAssortmentId,
-      addAssortmentLink,
-      data: { assortments = [] },
-      ...rest
-    }) => ({
-      assortments: [{ label: 'Select', value: false }].concat(
-        assortments
-          .map((assortment) => ({
-            label: assortment.texts.title,
-            value: assortment._id,
-          }))
-          .filter(({ value }) => parentAssortmentId !== value)
-      ),
-      model: {
-        parentAssortmentId,
-      },
-      ...rest,
-    })
-  )
+    },
+    ...rest,
+  }))
 )(FormNewAssortmentLink);
