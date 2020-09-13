40beb77bb1fb11bf4535ef2a4046886d46d39446
mikearaya <mikaelaraya12@gmail.com>
2020-09-13 15:54:10 +0300

    Replace aldeed index assignment for Bookmarks #216

--- packages/core-bookmarks/db/schema.js
+++ packages/core-bookmarks/db/schema.js
@@ -5,10 +5,15 @@ import { Bookmarks } from './collections';
 Bookmarks.attachSchema(
   new SimpleSchema(
     {
-      userId: { type: String, required: true, index: true },
-      productId: { type: String, required: true, index: true },
+      userId: { type: String, required: true },
+      productId: { type: String, required: true },
       ...Schemas.timestampFields,
     },
     { requiredByDefault: false },
   ),
 );
+
+export default () => {
+  Bookmarks.rawCollection().createIndex({ userId: 1 });
+  Bookmarks.rawCollection().createIndex({ productId: 1 });
+};
