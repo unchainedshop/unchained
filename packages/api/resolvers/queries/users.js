c5ae2a34c0a93e99e6138391d53788158c63e72f
Harry Adel <harryadelb@gmail.com>
2020-09-16 12:48:45 +0200

    Move Users index to initialization

--- packages/api/resolvers/queries/users.js
+++ packages/api/resolvers/queries/users.js
@@ -1,17 +1,6 @@
 import { log } from 'meteor/unchained:core-logger';
 import { Users } from 'meteor/unchained:core-users';
 
-Users.rawCollection().createIndex({
-  username: 'text',
-  'emails.$.address': 'text',
-  'profile.displayName': 'text',
-  'lastBillingAddress.firstName': 'text',
-  'lastBillingAddress.lastName': 'text',
-  'lastBillingAddress.company': 'text',
-  'lastBillingAddress.addressLine': 'text',
-  'lastBillingAddress.addressLine2': 'text',
-});
-
 export default function users(
   root,
   { limit, offset, includeGuests, queryString },
