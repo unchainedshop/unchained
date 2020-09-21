640c80e76c6a6768ca88e415012041a0b3a5e01c
Harry Adel <harryadelb@gmail.com>
2020-09-21 17:04:52 +0200

    Add missing package.js file

---
+++ packages/core-accountsjs/package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  name: 'unchained:core-accountsjs',
+  version: '0.52.0',
+  summary: 'Unchained Engine Core: Accounts',
+  git: 'https://github.com/unchainedshop/unchained',
+  documentation: 'README.md',
+});
+
+Npm.depends({
+  '@accounts/database-manager': '0.29.0',
+  '@accounts/mongo': '0.29.0',
+  '@accounts/password': '0.29.0',
+  '@accounts/server': '0.29.0',
+});
+
+Package.onUse((api) => {
+  api.versionsFrom('1.10.2');
+  api.use('ecmascript');
+  api.mainModule('core-accountsjs.js');
+});
+
+Package.onTest((api) => {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('unchained:core-accountsjs');
+  api.mainModule('core-accountsjs-tests.js');
+});
