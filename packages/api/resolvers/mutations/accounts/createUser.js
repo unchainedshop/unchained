46180757b4c276eaaacbe956b7eab1dcb419eb2e
Harry Adel <harryadelb@gmail.com>
2020-09-21 18:45:09 +0200

    Revert user creation for the time being

--- packages/api/resolvers/mutations/accounts/createUser.js
+++ packages/api/resolvers/mutations/accounts/createUser.js
@@ -1,7 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import callMethod from '../../../callMethod';
 import hashPassword from '../../../hashPassword';
-import { accountsPassword } from '../../../api';
 
 export default async function createUser(root, options, context) {
   Meteor._nodeCodeMustBeInFiber(); // eslint-disable-line
@@ -13,5 +12,5 @@ export default async function createUser(root, options, context) {
     mappedOptions.password = hashPassword(mappedOptions.plainPassword);
     delete mappedOptions.plainPassword;
   }
-  await accountsPassword.createUser(mappedOptions);
+  return callMethod(context, 'createUser', mappedOptions);
 }
