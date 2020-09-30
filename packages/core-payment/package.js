d9373849e6430ccd5c04b45e3d4ce9eb0e3947a1
mikearaya <mikaelaraya12@gmail.com>
2020-09-30 11:38:23 +0300

    Remove dburles package specification from all packages #215

--- packages/core-payment/package.js
+++ packages/core-payment/package.js
@@ -16,7 +16,6 @@ Package.onUse((api) => {
   api.use('mongo');
   api.use('promise');
   api.use('percolate:migrations@1.0.2');
-  api.use('dburles:factory@1.1.0');
   api.use('dburles:collection-helpers@1.1.0');
   api.use('aldeed:collection2@3.2.1');
 
