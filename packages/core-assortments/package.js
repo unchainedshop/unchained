c7355d8f16b8b475de6d423e402c61a56d8fecc2
Pascal Kaufmann <kaufmae@me.com>
2020-10-02 21:14:10 +0200

    Remove api.use

--- packages/core-assortments/package.js
+++ packages/core-assortments/package.js
@@ -10,7 +10,6 @@ Package.onUse((api) => {
   api.versionsFrom('1.10');
   api.use('ecmascript');
   api.use('mongo');
-  api.use('dburles:factory@1.1.0');
   api.use('dburles:collection-helpers@1.1.0');
   api.use('aldeed:collection2@3.2.1');
   api.use('percolate:migrations@1.0.2');
--- packages/core-countries/package.js
+++ packages/core-countries/package.js
@@ -14,7 +14,6 @@ Package.onUse((api) => {
   api.versionsFrom('1.10');
   api.use('ecmascript');
   api.use('mongo');
-  api.use('dburles:factory@1.1.0');
   api.use('dburles:collection-helpers@1.1.0');
   api.use('aldeed:collection2@3.2.1');
   api.use('unchained:utils@0.53.0');
