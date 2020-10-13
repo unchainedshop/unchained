8a68e79e0c30d76c3ca0a3d4ed46609a11dc6122
Harry Adel <harryadelb@gmail.com>
2020-10-13 16:42:38 +0200

    Bump core-accountsjs version

--- packages/core-accountsjs/package.js
+++ packages/core-accountsjs/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:core-accountsjs',
-  version: '0.53.1',
+  version: '0.53.2',
   summary: 'Unchained Engine Core: Accounts',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
