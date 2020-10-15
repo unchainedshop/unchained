2dc5ece9c7ebd97f715df3f5ca749a6b20bce1af
Harry Adel <harryadelb@gmail.com>
2020-10-15 15:52:28 +0200

    Fix db manager error

--- packages/core-accountsjs/accountsjs.js
+++ packages/core-accountsjs/accountsjs.js
@@ -1,6 +1,7 @@
 import settings from './settings';
 
 export * from './helpers';
+export * from './db-manager';
 export * from './accounts-server';
 export * from './accounts-password';
 export default (options) => {
