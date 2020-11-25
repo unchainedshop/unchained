05e94f793ff0461a7e1cf51952a19b6b84cd1e40
mikearaya <mikaelaraya12@gmail.com>
2020-11-25 22:09:51 +0300

    Add type assertion for mutation.updateProductSupply #266

--- packages/api/resolvers/mutations/updateProductSupply.js
+++ packages/api/resolvers/mutations/updateProductSupply.js
@@ -1,6 +1,10 @@
 import { log } from 'meteor/unchained:core-logger';
-import { Products } from 'meteor/unchained:core-products';
-import { ProductNotFoundError, InvalidIdError } from '../../errors';
+import { Products, ProductTypes } from 'meteor/unchained:core-products';
+import {
+  ProductNotFoundError,
+  InvalidIdError,
+  ProductWrongStatusError,
+} from '../../errors';
 
 export default function updateProductSupply(
   root,
@@ -11,5 +15,10 @@ export default function updateProductSupply(
   if (!productId) throw new InvalidIdError({ productId });
   const productObject = Products.updateProduct({ productId, supply });
   if (!productObject) throw new ProductNotFoundError({ productId });
+  if (productObject?.type !== ProductTypes.SimpleProduct)
+    throw new ProductWrongStatusError({
+      received: productObject?.type,
+      required: ProductTypes.SimpleProduct,
+    });
   return productObject;
 }
