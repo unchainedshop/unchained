95af7cadbdd40036ad1f5e527f5ab43681dde1e6
Harry Adel <harryadelb@gmail.com>
2020-12-09 17:48:14 +0200

    Configure typescript supported tests

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.ts?$': 'ts-jest',
+  },
   preset: '@shelf/jest-mongodb',
   globalSetup: './tests/jest-global-setup.js',
   globalTeardown: './tests/jest-global-teardown.js',
   moduleNameMapper: {
     '^meteor/unchained:core-users$':
       '<rootDir>/tests/jest-meteor-stubs/core-users',
-    '^@share911/meteor-check$':
-      '<rootDir>/tests/jest-meteor-stubs/meteor-check',
-    'lodash.clone': '<rootDir>/tests/jest-meteor-stubs/lodash-clone',
+    '^lodash.clone$': '<rootDir>/tests/jest-meteor-stubs/lodash-clone',
+    '^meteor/meteor$': '<rootDir>/tests/jest-meteor-stubs/meteor',
   },
 };
