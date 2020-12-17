7766e688fb9ca5b6320cbd5546e198ead583c885
Pascal Kaufmann <kaufmae@me.com>
2020-12-17 20:14:13 +0100

    Hotfix release for platform

--- packages/platform/package.js
+++ packages/platform/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:platform',
-  version: '0.55.4',
+  version: '0.55.5',
   summary: 'Unchained Engine',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
