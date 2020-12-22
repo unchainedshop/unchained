8ef722b9283bb423034d5c9115ab879e8295bdba
Harry Adel <harryadelb@gmail.com>
2020-12-22 13:25:10 +0200

    Fix typescript issue

--- packages/roles/package.js
+++ packages/roles/package.js
@@ -7,13 +7,15 @@ Package.describe({
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('1.11.1');
+  api.versionsFrom('1.12');
 
   Npm.depends({
     'lodash.clone': '4.5.0',
   });
 
-  api.use(['ecmascript', 'typescript', 'unchained:core-users']);
+  api.use('ecmascript');
+  api.use('unchained:core-users');
+  api.use('typescript');
 
   api.addFiles(['helpers.ts', 'roles.ts'], 'server');
 
