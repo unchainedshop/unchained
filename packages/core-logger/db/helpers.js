504563f54f78d4d09a9006a556795083277fdec9
mikearaya <mikaelaraya12@gmail.com>
2020-12-25 21:48:23 +0300

    Move log queries business logic to core-logger package #275

--- packages/core-logger/db/helpers.js
+++ packages/core-logger/db/helpers.js
@@ -2,3 +2,16 @@ import 'meteor/dburles:collection-helpers';
 import { Logs } from './collections';
 
 Logs.helpers({});
+
+Logs.findLogs = ({ limit, offset }) => {
+  return Logs.find(
+    {},
+    {
+      skip: offset,
+      limit,
+      sort: {
+        created: -1,
+      },
+    }
+  ).fetch();
+};
