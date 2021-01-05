76fc3f8cd2e2fed637968bb19d64203cb92fbeff
mikearaya <mikaelaraya12@gmail.com>
2021-01-05 23:17:53 +0300

    Use default catalogPrice function

--- packages/api/resolvers/types/product.js
+++ packages/api/resolvers/types/product.js
@@ -1,6 +1,5 @@
 import { ProductTypes } from 'meteor/unchained:core-products';
 import { objectInvert } from 'meteor/unchained:utils';
-import crypto from 'crypto';
 
 export default {
   __resolveType(obj) {
@@ -8,21 +7,6 @@ export default {
     return invertedProductTypes[obj.type];
   },
   async catalogPrices(obj) {
-    const prices = (obj.commerce && obj.commerce.pricing) || [];
-    return prices.map((price) => ({
-      _id: crypto
-        .createHash('sha256')
-        .update(
-          [
-            obj._id,
-            price.countryCode,
-            price.currencyCode,
-            price.maxQuantity,
-            price.amount,
-          ].join('')
-        )
-        .digest('hex'),
-      ...price,
-    }));
+    return obj.catalogPrices();
   },
 };
