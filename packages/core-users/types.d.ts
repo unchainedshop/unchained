b8997e884567517dd21d0c6fa2fa5ae8c0c23841
Simon Emanuel Schmid <simon@unchained.shop>
2021-01-10 15:41:04 +0000

    Minimalistic core-users types

---
+++ packages/core-users/types.d.ts
@@ -0,0 +1,22 @@
+declare module 'meteor/unchained:core-users' {
+  type User = {
+    displayName?: string;
+    birthday?: Date;
+    phoneMobile?: string;
+    gender?: string;
+    address?: any;
+    customFields?: any;
+  };
+
+  // TODO: should be `extends Mongo.Collection<User>`
+  class Users {
+    static findUser(args: {
+      userId?: string;
+      resetToken?: string;
+      hashedToken?: string;
+    }): User;
+  }
+
+  // eslint-disable-next-line import/prefer-default-export
+  export { Users };
+}
