a2d8e27d6e86dfada0c060e44ed65e5a4a30f783
Pascal Kaufmann <kaufmae@me.com>
2021-01-11 20:04:53 +0100

    Update changelog

--- packages/api/resolvers/types/bookmark.ts
+++ packages/api/resolvers/types/bookmark.ts
@@ -2,10 +2,10 @@ import { Users } from 'meteor/unchained:core-users';
 import { Products } from 'meteor/unchained:core-products';
 
 export default {
-  product(obj) {
+  async product(obj) {
     return Products.findOne({ _id: obj.productId });
   },
-  user(obj) {
+  async user(obj) {
     return Users.findOne({ _id: obj.userId });
   },
 };
