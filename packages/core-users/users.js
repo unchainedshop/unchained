68eafd50d938647f18193535b2c8db98193fe7f6
Pascal Kaufmann <kaufmae@me.com>
2021-01-12 15:45:12 +0100

    Add Recurring Stale User Removal Job

--- packages/core-users/users.js
+++ packages/core-users/users.js
@@ -1,10 +1,21 @@
+import { WorkerDirector } from 'meteor/unchained:core-worker';
+import RemoveStaleGuests from './worker/remove-stale-guests';
+import settings from './settings';
+
 import runMigrations from './db/schema';
 import './db/helpers';
-import settings from './settings';
 
 export * from './db/schema';
 export * from './db/collections';
+
 export default (options) => {
+  // configure
   settings.load(options);
+  if (settings.autoSchedulingCronText) {
+    WorkerDirector.configureAutoscheduling(RemoveStaleGuests, {
+      cronText: settings.autoSchedulingCronText,
+      input: settings.autoSchedulingInput,
+    });
+  }
   runMigrations();
 };
