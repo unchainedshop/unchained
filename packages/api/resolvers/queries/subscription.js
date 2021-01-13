05b9aa9594b46def35682844b63bea146bdbc1d5
mikearaya <mikaelaraya12@gmail.com>
2021-01-13 23:01:31 +0300

    Remove SubscriptionNotFoundError from query.subscription and update test #246

--- packages/api/resolvers/queries/subscription.js
+++ packages/api/resolvers/queries/subscription.js
@@ -1,12 +1,10 @@
 import { log } from 'meteor/unchained:core-logger';
 import { Subscriptions } from 'meteor/unchained:core-subscriptions';
-import { SubscriptionNotFoundError, InvalidIdError } from '../../errors';
+import { InvalidIdError } from '../../errors';
 
 export default function subscription(root, { subscriptionId }, { userId }) {
   log(`query subscription ${subscriptionId}`, { userId, subscriptionId });
+
   if (!subscriptionId) throw new InvalidIdError({ subscriptionId });
-  const foundSubscription = Subscriptions.findSubscription({ subscriptionId });
-  if (!foundSubscription)
-    throw new SubscriptionNotFoundError({ subscriptionId });
-  return foundSubscription;
+  return Subscriptions.findSubscription({ subscriptionId });
 }
