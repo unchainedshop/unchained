11cb12c55633465f80833be6fc8c30939593608e
Pascal Kaufmann <kaufmae@me.com>
2021-02-08 20:00:55 +0100

    Fix VerifyEmailSuccess hook fired with pre-verified userdata

--- packages/api/package.js
+++ packages/api/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:api',
-  version: '0.61.4',
+  version: '0.61.5',
   summary: 'Unchained Engine: GraphQL API',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
--- packages/api/resolvers/mutations/verifyEmail.js
+++ packages/api/resolvers/mutations/verifyEmail.js
@@ -8,8 +8,8 @@ import { Users } from 'meteor/unchained:core-users';
 
 export default async function verifyEmail(root, { token }, context) {
   log('mutation verifyEmail');
-  const verifiedUser = await dbManager.findUserByEmailVerificationToken(token);
   await accountsPassword.verifyEmail(token);
+  const verifiedUser = await dbManager.findUserByEmailVerificationToken(token);
   await accountsServer.getHooks().emit('VerifyEmailSuccess', verifiedUser);
   return Users.createLoginToken(verifiedUser, context);
 }
