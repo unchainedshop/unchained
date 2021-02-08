4fcd3e66ce71f6af3c459c3aa09df889d11de55a
Pascal Kaufmann <kaufmae@me.com>
2021-02-08 16:23:28 +0100

    Add order payment provider type check and update changelog #300

--- packages/api/resolvers/mutations/signPaymentProviderForCheckout.js
+++ packages/api/resolvers/mutations/signPaymentProviderForCheckout.js
@@ -1,6 +1,12 @@
 import { log } from 'meteor/unchained:core-logger';
 import { OrderPayments } from 'meteor/unchained:core-orders';
-import { InvalidIdError, OrderPaymentNotFoundError } from '../../errors';
+import { PaymentProviderType } from 'meteor/unchained:core-payment';
+
+import {
+  InvalidIdError,
+  OrderPaymentNotFoundError,
+  OrderPaymentTypeError,
+} from '../../errors';
 
 export default (root, { orderPaymentId, transactionContext }, { userId }) => {
   log(`mutation signPaymentProviderForCheckout ${orderPaymentId}`, {
@@ -9,5 +15,12 @@ export default (root, { orderPaymentId, transactionContext }, { userId }) => {
   if (!orderPaymentId) throw new InvalidIdError({ orderPaymentId });
   const orderPayment = OrderPayments.findPayment({ orderPaymentId });
   if (!orderPayment) throw new OrderPaymentNotFoundError({ orderPaymentId });
+  const providerType = orderPayment?.provider()?.type;
+  if (providerType !== PaymentProviderType.GENERIC)
+    throw new OrderPaymentTypeError({
+      orderPaymentId,
+      received: providerType,
+      required: PaymentProviderType.GENERIC,
+    });
   return orderPayment.sign({ transactionContext });
 };
