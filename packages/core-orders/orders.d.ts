3802a875b92c5b2bffb21184a643ad5717cd0fca
mikearaya <mikaelaraya12@gmail.com>
2021-02-08 22:08:29 +0300

    Remove order event definitions

--- packages/core-orders/orders.d.ts
+++ packages/core-orders/orders.d.ts
@@ -3,7 +3,6 @@ import { OrderEvent } from './events';
 declare module 'meteor/unchained:core-orders' {
   const Orders: any;
 
-  const OrderEvents: OrderEvent;
   // eslint-disable-next-line import/prefer-default-export
-  export { Orders, OrderEvents };
+  export { Orders };
 }
