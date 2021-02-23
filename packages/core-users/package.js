a9c3656be6173d5cec9895ca068789c310b7dae3
Pascal Kaufmann <kaufmae@me.com>
2021-02-23 22:23:16 +0100

    Users Hotfixes
    commit a369e4133d2667ccf7a5cedc39b7ee1f09174587
    Author: Pascal Kaufmann <kaufmae@me.com>
    Date:   Mon Feb 22 09:47:15 2021 +0100
    
        Users 0.61.4
    
    commit 44b0a9c1422f7a7936dcb70f43d065e26c5322c4
    Author: Pascal Kaufmann <kaufmae@me.com>
    Date:   Mon Feb 22 09:46:08 2021 +0100
    
        Revert "Fix user recreate indexes with unique usernames and email addresses"
    
        This reverts commit 73efbebb769db4ddb52d54b00508f6a62b7c0439.
    
    commit 73efbebb769db4ddb52d54b00508f6a62b7c0439
    Author: Pascal Kaufmann <kaufmae@me.com>
    Date:   Thu Feb 18 11:43:58 2021 +0100
    
        Fix user recreate indexes with unique usernames and email addresses

--- packages/core-users/package.js
+++ packages/core-users/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:core-users',
-  version: '0.61.3',
+  version: '0.61.4',
   summary: 'Unchained Engine Core: Users',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
