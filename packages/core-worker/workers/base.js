fa27a21222fabf2cb9fd33c8ce96d82213d9cd12
Pascal Kaufmann <kaufmae@me.com>
2021-02-23 22:34:37 +0100

    Revert regression

--- packages/core-worker/workers/base.js
+++ packages/core-worker/workers/base.js
@@ -54,6 +54,7 @@ class BaseWorker {
         async ([type, configuration]) => {
           const { cronText, input, ...rest } = configuration;
           const schedule = later.parse.text(cronText);
+          schedule.schedules[0].s = [0]; // ignore seconds
           const nextDate = later.schedule(schedule).next(1, referenceDate);
           nextDate.setMilliseconds(0);
           await this.WorkerDirector.ensureOneWork({
