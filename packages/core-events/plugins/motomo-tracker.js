c0f5a9108601d0c038a57760bd1690eaca4863f6
Mikael Araya <mikaelaraya12@gmail.com>
2021-03-01 22:39:05 +0300

    Make matomo plugable

--- packages/core-events/plugins/motomo-tracker.js
+++ packages/core-events/plugins/motomo-tracker.js
@@ -1,8 +1,21 @@
 import fetch from 'isomorphic-unfetch';
+import { encode } from 'querystring';
 import { subscribe } from '../director';
 
-subscribe('PAGE_VIEW', async (e) => {
-  const data = await fetch(
-    `https://matomo.ucc.dev/matomo.php?idsite=1&rec=1&action_name=${e.payload.path}`
-  );
-});
+const MatomoTracker = (siteId, siteUrl, subscribeTo, options = {}) => {
+  if (!siteId && (typeof siteId !== 'number' || siteId !== 'string'))
+    throw new Error('Matomo siteId is required');
+  if (!siteUrl && typeof siteUrl !== 'string')
+    throw new Error('Matomo tracker URL is required');
+  if (!subscribeTo && typeof subscribeTo !== 'string')
+    throw new Error('Event that triggers tracking should be provided');
+  subscribe(subscribeTo, async (e) => {
+    await fetch(
+      `${siteUrl}?idsite=${siteId}&rec=1&action_name=${e.payload.path}&${encode(
+        options
+      )}`
+    );
+  });
+};
+
+export default MatomoTracker;
