3478102c3f1f1322291f45bcc9c7046bb0ad31c0
Mikael Araya <mikaelaraya12@gmail.com>
2021-03-28 16:26:27 +0300

    Add BigInt scalar #302

--- packages/api/resolvers/index.js
+++ packages/api/resolvers/index.js
@@ -1,4 +1,5 @@
 import { GraphQLUpload } from 'graphql-upload';
+import { BigIntResolver } from 'graphql-scalars';
 import Query from './queries';
 import Mutation from './mutations';
 import Date from './scalar-date';
@@ -9,6 +10,7 @@ export default {
   ...Types,
   Upload: GraphQLUpload,
   JSON,
+  BigInt: BigIntResolver,
   Date,
   Query,
   Mutation,
--- packages/api/schema/scalars.js
+++ packages/api/schema/scalars.js
@@ -1,5 +1,6 @@
 export default [
   /* GraphQL */ `
+    scalar BigInt
     scalar Date
     scalar JSON
     scalar Upload
