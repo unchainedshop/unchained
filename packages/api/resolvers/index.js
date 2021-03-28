15e89054398c5d94b1dbad482d481c9e22503fba
Mikael Araya <mikaelaraya12@gmail.com>
2021-03-28 21:02:06 +0300

    Change JSON type to use graphql-scalars

--- packages/api/resolvers/index.js
+++ packages/api/resolvers/index.js
@@ -1,16 +1,15 @@
 import { GraphQLUpload } from 'graphql-upload';
-import { BigIntResolver } from 'graphql-scalars';
+import { BigIntResolver, JSONResolver } from 'graphql-scalars';
 import Query from './queries';
 import Mutation from './mutations';
 import Date from './scalar-date';
-import JSON from './scalar-json';
 import Types from './types';
 
 export default {
   ...Types,
   Upload: GraphQLUpload,
-  JSON,
   BigInt: BigIntResolver,
+  JSON: JSONResolver,
   Date,
   Query,
   Mutation,
--- packages/api/schema/scalars.js
+++ packages/api/schema/scalars.js
@@ -2,6 +2,10 @@ export default [
   /* GraphQL */ `
     scalar Date
     scalar JSON
+<<<<<<< HEAD
     scalar Upload
+=======
+    scalar BigInt
+>>>>>>> Change JSON type to use graphql-scalars
   `,
 ];
