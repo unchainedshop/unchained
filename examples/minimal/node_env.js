86b961d08b4b6396f86adb55e85dfd3cfd4f080a
Mikael Araya <mikaelaraya12@gmail.com>
2021-06-09 17:07:58 +0300

    Fix #330

--- examples/minimal/node_env.js
+++ examples/minimal/node_env.js
@@ -1,6 +1,6 @@
 require('dotenv-extended').load({
   silent: process.env.SUPPRESS_ENV_ERRORS,
-  errorOnMissing: process.env.SUPPRESS_ENV_ERRORS,
+  errorOnMissing: !process.env.SUPPRESS_ENV_ERRORS,
   errorOnRegex: !process.env.SUPPRESS_ENV_ERRORS,
   errorOnExtra: !process.env.SUPPRESS_ENV_ERRORS,
   includeProcessEnv: true,
