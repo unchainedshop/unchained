fd52d8df2ca0bfc849060930468e924d39a3a26d
Pascal Kaufmann <kaufmae@me.com>
2021-06-10 11:01:40 +0200

    Release api 0.61.21

--- packages/api/package.js
+++ packages/api/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:api',
-  version: '0.61.20',
+  version: '0.61.21',
   summary: 'Unchained Engine: GraphQL API',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
