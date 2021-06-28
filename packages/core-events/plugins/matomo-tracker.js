25b484ab281f21074e5a1c9f127fa29fcaeb6d63
Mikael Araya <mikaelaraya12@gmail.com>
2021-06-28 20:36:37 +0300

    Update pageView resolver

--- packages/core-events/plugins/matomo-tracker.js
+++ packages/core-events/plugins/matomo-tracker.js
@@ -1,43 +1,72 @@
 import fetch from 'isomorphic-unfetch';
 import { encode } from 'querystring';
 import { OrderStatus, Orders } from 'meteor/unchained:core-orders';
+import crypto from 'crypto';
 import { subscribe } from '../director';
 
 const parseCurrency = (amount) => parseFloat(amount / 100);
+const actionMap = {
+  ORDER_ADD_PRODUCT: 'addEcommerceItem',
+  ORDER_UPDATE_CART_ITEM: 'addEcommerceItem',
+  ORDER_REMOVE_CART_ITEM: 'removeEcommerceItem',
+  OrDER_CHECKOUT: 'trackEcommerceOrder',
+  PAGE_VIEW: 'trackPageView',
+};
 
-const extractOrderParameters = (currentOrder) => {
+const extractPageInformation = ({ path, referrer, context }) => {
+  const pageOptions = {};
+  pageOptions.url = context.headers.origin + path;
+  pageOptions.urlref = referrer || context.headers.referrer;
+
+  return pageOptions;
+};
+
+const extractOrderParameters = (currentEvent, currentOrder) => {
   const orderOptions = {};
   const order = Orders.findOrder({ orderId: currentOrder._id });
-
-  if (order.status === OrderStatus.CONFIRMED) {
-    orderOptions.ec_id = order._id;
-    orderOptions.action = 'trackEcommerceOrder';
-  }
-  if (order.isCart()) {
-    orderOptions.action = 'addEcommerceItem';
-  }
-
   orderOptions.isCart = order.isCart();
-  // eslint-disable-next-line no-underscore-dangle
-  orderOptions._ects = new Date().getTime();
-  orderOptions.uid = order.user().username;
-  orderOptions._id = order.userId;
-  const pricing = order.pricing();
-  orderOptions.revenue = parseCurrency(order.pricing().total().amount);
-  orderOptions.ec_tx = parseCurrency(pricing.taxSum());
-  orderOptions.ec_dt = pricing.discountSum();
 
-  orderOptions.ec_items = JSON.stringify(
-    order
-      .items()
-      .map((item) => [
-        `${item.product()?.warehousing?.sku}`,
-        item.product()?.getLocalizedTexts()?.title,
-        '',
-        parseCurrency(item.pricing().unitPrice().amount),
-        item.quantity,
-      ])
-  );
+  if (currentEvent === 'ORDER_REMOVE_CART_ITEM') {
+    orderOptions.ec_items = JSON.stringify(
+      order.items().map((item) => [item.product()?.warehousing?.sku])
+    );
+  } else {
+    if (order.status === OrderStatus.CONFIRMED) {
+      orderOptions.ec_id = order._id;
+    }
+    console.log(
+      crypto
+        .createHash('sha256')
+        .update(order.user()._id)
+        .digest('hex')
+        .substr(0, 16)
+    );
+    // eslint-disable-next-line no-underscore-dangle
+    orderOptions._ects = new Date().getTime();
+    orderOptions.uid = order.user().username;
+    orderOptions._id = order.user().userId;
+    orderOptions.cid = crypto
+      .createHash('sha256')
+      .update(order.user()._id)
+      .digest('hex')
+      .substr(0, 16);
+    const pricing = order.pricing();
+    orderOptions.revenue = parseCurrency(order.pricing().total().amount);
+    orderOptions.ec_tx = parseCurrency(pricing.taxSum());
+    orderOptions.ec_dt = pricing.discountSum();
+
+    orderOptions.ec_items = JSON.stringify(
+      order
+        .items()
+        .map((item) => [
+          `${item.product()?.warehousing?.sku}`,
+          item.product()?.getLocalizedTexts()?.title,
+          '',
+          parseCurrency(item.pricing().unitPrice().amount),
+          item.quantity,
+        ])
+    );
+  }
   orderOptions.idgoal = 0;
   return orderOptions;
 };
@@ -50,23 +79,37 @@ const MatomoTracker = (siteId, siteUrl, subscribeTo, options = {}) => {
   if (!subscribeTo && typeof subscribeTo !== 'string')
     throw new Error('Event that triggers tracking should be provided');
 
+  const extraOptions = Object.keys(options || {}).length
+    ? `&${encode(options)}`
+    : '';
+
   subscribe(subscribeTo, async ({ payload }) => {
     let orderOptions = {};
-    if (payload?.order) orderOptions = extractOrderParameters(payload.order);
-    else if (payload?.orderPosition)
-      orderOptions = extractOrderParameters(payload.orderPosition?.order());
-
-    const { isCart, action } = orderOptions;
-    delete orderOptions.isCart;
-    delete orderOptions.action;
+    if (payload?.order || payload?.payload?.order)
+      orderOptions = extractOrderParameters(
+        subscribeTo,
+        payload?.order || payload?.payload?.order
+      );
+    else if (payload?.orderPosition || payload?.payload?.orderPosition)
+      orderOptions = extractOrderParameters(
+        subscribeTo,
+        payload?.orderPosition?.order() ||
+          payload?.payload?.orderPosition?.order()
+      );
+    else if (subscribeTo === 'PAGE_VIEW') {
+      orderOptions = extractPageInformation(payload);
+    }
     await fetch(
       `${siteUrl}?idsite=${siteId}&rec=1&action_name=${
-        action ?? payload?.path
-      }&${encode(orderOptions)}`
+        actionMap[subscribeTo]
+      }&${encode(orderOptions)}${extraOptions}`
     );
-    if (orderOptions && isCart) {
+    if (
+      actionMap[subscribeTo] !== 'trackEcommerceOrder' &&
+      actionMap[subscribeTo] !== 'trackPageView'
+    ) {
       await fetch(
-        `${siteUrl}?idsite=${siteId}&rec=1&action_name=trackEcommerceCartUpdate&ec_st=${orderOptions?.revenue}`
+        `${siteUrl}?idsite=${siteId}&rec=1&action_name=trackEcommerceCartUpdate&ec_st=${orderOptions?.revenue}${extraOptions}`
       );
     }
   });
