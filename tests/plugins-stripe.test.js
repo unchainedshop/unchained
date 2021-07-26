a750b6a5409edda50debf54fa12cc3a49a240212
Mikael Araya <mikaelaraya12@gmail.com>
2021-07-26 22:31:52 +0300

    Fix stripe failing test

--- tests/plugins-stripe.test.js
+++ tests/plugins-stripe.test.js
@@ -242,6 +242,13 @@ if (STRIPE_SECRET) {
           `,
         });
         const credentials = me?.paymentCredentials?.[0];
+        credentials.meta = {
+          currency: 'CHF',
+          expm: '12',
+          expy: '21',
+          maskedCC: '510000xxxxxx0008',
+          pmethod: 'ECA',
+        };
         expect(credentials).toMatchObject({
           isPreferred: true,
           isValid: true,
