08ffd3a4a0c83cbce5354696f202e54fb6754909
Mikael Araya <mikaelaraya12@gmail.com>
2021-07-28 19:05:57 +0300

    clear

--- packages/api/resolvers/index.js
+++ packages/api/resolvers/index.js
@@ -1,20 +1,16 @@
 import { GraphQLUpload } from 'graphql-upload';
 import {
-  BigIntResolver,
   JSONResolver,
-  PositiveFloatResolver,
   TimestampResolver,
   DateTimeResolver,
 } from 'graphql-scalars';
 import Query from './queries';
 import Mutation from './mutations';
 import Types from './types';
-import BigInt from './scalar-bigint';
 
 export default {
   ...Types,
   Upload: GraphQLUpload,
-  BigInt: BigIntResolver,
   JSON: JSONResolver,
   Query,
   Mutation,
