5790df0a4da76478e04d60cdbd815940bbc8ad30
Pascal Kaufmann <kaufmae@me.com>
2021-07-30 16:23:08 +0200

    Hotfix worker

--- packages/core-worker/package.js
+++ packages/core-worker/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:core-worker',
-  version: '1.0.0-beta10',
+  version: '1.0.0-beta11',
   summary: 'Unchained Engine Core: Worker',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
