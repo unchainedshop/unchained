7c0af7da8517d043853f517b95472ad0732ed5a8
Pascal Kaufmann <kaufmae@me.com>
2021-08-06 11:31:05 +0200

    Stabilize interval

--- packages/core-worker/workers/interval.js
+++ packages/core-worker/workers/interval.js
@@ -1,4 +1,5 @@
 import later from 'later';
+import { Meteor } from 'meteor/meteor';
 import BaseWorker from './base';
 
 const { NODE_ENV } = process.env;
@@ -6,6 +7,17 @@ const { NODE_ENV } = process.env;
 const defaultSchedule = later.parse.text(
   NODE_ENV !== 'production' ? 'every 2 seconds' : 'every 30 seconds'
 );
+
+export const scheduleToInterval = (scheduleRaw) => {
+  const referenceDate = new Date(1000);
+  const schedule =
+    typeof scheduleRaw === 'string'
+      ? later.parse.text(scheduleRaw)
+      : scheduleRaw;
+  const [one, two] = later.schedule(schedule).next(2, referenceDate);
+  const diff = new Date(two).getTime() - new Date(one).getTime();
+  return Math.min(1000 * 60 * 60, diff); // at least once every hour!
+};
 
 class IntervalWorker extends BaseWorker {
   static key = 'shop.unchained.worker.cron';
@@ -25,26 +37,20 @@ class IntervalWorker extends BaseWorker {
   }) {
     super({ WorkerDirector, workerId });
     this.batchCount = batchCount;
-
-    const [one, two] = later.schedule(schedule).next(2);
-    const diff = new Date(two).getTime() - new Date(one).getTime();
-    this.intervalDelay = Math.min(1000, diff);
+    this.intervalDelay = scheduleToInterval(schedule);
   }
 
   start() {
-    this.intervalHandle = setInterval(() => {
+    this.intervalHandle = Meteor.setInterval(() => {
       this.process({
         maxWorkItemCount: this.batchCount,
         referenceDate: new Date(Math.floor(new Date().getTime() / 1000) * 1000),
       });
     }, this.intervalDelay);
-    setTimeout(() => {
-      this.autorescheduleTypes();
-    }, 300);
   }
 
   stop() {
-    clearInterval(this.intervalHandle);
+    Meteor.clearInterval(this.intervalHandle);
   }
 }
 
