40170d0d1c80a854df220fdc3ecd0aeabcddf329
Mikael Araya <mikaelaraya12@gmail.com>
2021-09-28 21:14:06 +0300

    Rebase with master

---
+++ examples/controlpanel/components/users/UploadAvatar.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { graphql } from '@apollo/client/react/hoc';
+import { compose, pure, withHandlers, withState, mapProps } from 'recompose';
+import gql from 'graphql-tag';
+import { Image } from 'semantic-ui-react';
+import Dropzone from 'react-dropzone';
+import squareImage from '../../public/square-image.png';
+
+const UploadAvatar = ({ avatarUrl, handleChange }) => (
+  <div className="fixed-height">
+    <Dropzone onDrop={handleChange} multiple={false} accept="image/*">
+      {({ getRootProps, getInputProps }) => {
+        const inputProps = getInputProps();
+        return (
+          <div
+            {...getRootProps()}
+            style={{ border: 0 }}
+            className="ui container"
+          >
+            <input {...inputProps} />
+            <Image
+              label={{ color: 'blue', corner: 'right', icon: 'edit' }}
+              src={avatarUrl}
+              width={150}
+              height={150}
+              bordered
+              shape="rounded"
+            />
+          </div>
+        );
+      }}
+    </Dropzone>
+    <style jsx>
+      {`
+        .fixed-height {
+          height: 150px;
+        }
+      `}
+    </style>
+  </div>
+);
+
+const FRAGMENT_AVATAR_FIELDS = gql`
+  fragment avatarFields on User {
+    _id
+    avatar {
+      _id
+      url
+    }
+  }
+`;
+
+export default compose(
+  withState('imageUrl', 'updateImageUrl', null),
+  graphql(gql`
+    query getProfile($userId: ID) {
+      user(userId: $userId) {
+        ...avatarFields
+      }
+    }
+    ${FRAGMENT_AVATAR_FIELDS}
+  `),
+  graphql(gql`
+    mutation updateUserAvatar($avatar: Upload!, $userId: ID) {
+      updateUserAvatar(avatar: $avatar, userId: $userId) {
+        ...avatarFields
+      }
+    }
+    ${FRAGMENT_AVATAR_FIELDS}
+  `),
+  withHandlers({
+    handleChange:
+      ({ mutate, userId, updateImageUrl }) =>
+      async (files) => {
+        const avatar = files[0];
+        updateImageUrl(URL.createObjectURL(avatar));
+        await mutate({
+          variables: {
+            userId,
+            avatar,
+          },
+        });
+      },
+  }),
+  mapProps(({ mutate, imageUrl, data: { user }, ...rest }) => ({
+    avatarUrl:
+      imageUrl || (user && user.avatar && user.avatar.url) || squareImage,
+    ...rest,
+  })),
+  pure
+)(UploadAvatar);
