c488670fd8bb90ddce2da67967b719c8c62291f2
Pascal Kaufmann <kaufmae@me.com>
2021-11-15 20:10:59 +0100

    Bump core-filters version to 0.61.1

--- packages/core-filters/package.js
+++ packages/core-filters/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:core-filters',
-  version: '0.61.0',
+  version: '0.61.1',
   summary: 'Unchained Engine Core: Filters',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
