0c6ecc23a35a7b2492f777e0d8dbb9a8fa56dce5
joelmeiller <joel@oscillate.ch>
2021-11-18 22:58:23 +0100

    set history adapter only if not already set

--- packages/core-events/src/module/configureEventHistoryAdapter.ts
+++ packages/core-events/src/module/configureEventHistoryAdapter.ts
@@ -1,21 +1,27 @@
 import { Collection } from 'unchained-core-types';
 import { Event } from 'unchained-core-types/events';
 
-import { setEmitHistoryAdapter, EmitAdapter } from 'unchained-events';
+import {
+  getEmitHistoryAdapter,
+  setEmitHistoryAdapter,
+  EmitAdapter,
+} from 'unchained-events';
 
 export const configureEventHistoryAdapter = (Events: Collection<Event>) => {
-  const adapter: EmitAdapter = {
-    subscribe: () => {
-      // Do nothing
-    },
-    publish: async (eventName, payload: any) => {
-      await Events.insertOne({
-        type: eventName,
-        payload,
-        context: {},
-        created: new Date(),
-      });
-    },
-  };
-  setEmitHistoryAdapter(adapter);
+  if (!getEmitHistoryAdapter()) {
+    const adapter: EmitAdapter = {
+      subscribe: () => {
+        // Do nothing
+      },
+      publish: async (eventName, payload: any) => {
+        await Events.insertOne({
+          type: eventName,
+          payload,
+          context: {},
+          created: new Date(),
+        });
+      },
+    };
+    setEmitHistoryAdapter(adapter);
+  }
 };
