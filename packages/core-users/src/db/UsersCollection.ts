68c7d057e43c1972669d08b197e2668546d100ea
joelmeiller <joel@oscillate.ch>
2021-12-03 17:15:01 +0100

    no message

---
+++ packages/core-users/src/db/UsersCollection.ts
@@ -0,0 +1,90 @@
+import { Db } from '@unchainedshop/types/common';
+import { User } from '@unchainedshop/types/users';
+import { buildDbIndexes } from 'meteor/unchained:utils';
+
+export const UsersCollection = async (db: Db) => {
+  const Users = db.collection<User>('users');
+
+  await buildDbIndexes<User>(Users, [
+    () =>
+      Users.rawCollection().createIndex(
+        {
+          username: 1,
+        },
+        {
+          unique: true,
+          sparse: true,
+        }
+      ),
+    () =>
+      Users.rawCollection().createIndex(
+        {
+          'emails.address': 1,
+        },
+        {
+          unique: true,
+          sparse: true,
+        }
+      ),
+
+    () =>
+      Users.rawCollection().createIndex(
+        {
+          'services.email.verificationTokens.token': 1,
+        },
+        {
+          sparse: true,
+        }
+      ),
+
+    () =>
+      Users.rawCollection().createIndex(
+        {
+          'services.password.reset.token': 1,
+        },
+        {
+          sparse: true,
+        }
+      ),
+
+    () =>
+      Users.rawCollection().createIndex(
+        {
+          'services.resume.loginTokens.hashedToken': 1,
+        },
+        {
+          sparse: true,
+        }
+      ),
+
+    () =>
+      Users.rawCollection().createIndex(
+        {
+          _id: 'text',
+          username: 'text',
+          'emails.address': 'text',
+          'profile.displayName': 'text',
+          'lastBillingAddress.firstName': 'text',
+          'lastBillingAddress.lastName': 'text',
+          'lastBillingAddress.company': 'text',
+          'lastBillingAddress.addressLine': 'text',
+          'lastBillingAddress.addressLine2': 'text',
+        },
+        {
+          weights: {
+            _id: 9,
+            'emails.address': 7,
+            'profile.displayName': 5,
+            'lastBillingAddress.firstName': 3,
+            'lastBillingAddress.lastName': 3,
+            'lastBillingAddress.company': 1,
+            'lastBillingAddress.addressLine': 1,
+            'lastBillingAddress.addressLine2': 1,
+          },
+          name: 'user_fulltext_search',
+        }
+      ),
+  ]);
+
+  return Users;
+};
