a26c234477a918eee3b85fca1c2dc6e88a456807
Roman Böhringer <r.boehringer@opencore.ch>
2022-02-03 18:56:05 +0100

    tiny-secp256k1 downgrade

--- examples/minimal/boot.js
+++ examples/minimal/boot.js
@@ -14,6 +14,7 @@ import "meteor/unchained:core-payment/plugins/invoice";
 import "meteor/unchained:core-payment/plugins/invoice-prepaid";
 // import 'meteor/unchained:core-payment/plugins/datatrans-v2';
 import "meteor/unchained:core-payment/plugins/paypal-checkout";
+import "meteor/unchained:core-payment/plugins/cryptopay";
 import { configureAppleTransactionsModule } from "meteor/unchained:core-payment/plugins/apple-iap";
 import "meteor/unchained:core-payment/plugins/stripe";
 import "meteor/unchained:core-payment/plugins/stripe-charges";
