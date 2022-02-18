cb388e356f1800329207b6823056eca945271d4d
Roman Böhringer <r.boehringer@opencore.ch>
2022-02-18 11:10:24 +0100

    Basic signing setup with redirect

--- packages/core-payment/plugins/postfinance-checkout.ts
+++ packages/core-payment/plugins/postfinance-checkout.ts
@@ -2,6 +2,7 @@ import { Context } from '@unchainedshop/types/api';
 import { IPaymentAdapter } from '@unchainedshop/types/payments';
 import bodyParser from 'body-parser';
 import { useMiddlewareWithCurrentContext } from 'meteor/unchained:api';
+import { OrderPricingSheet } from 'meteor/unchained:core-orders';
 import {
   PaymentAdapter,
   PaymentDirector,
@@ -18,17 +19,23 @@ const {
   PFCHECKOUT_FAIL_WEBHOOK_PATH = '/graphql/postfinance-checkout-fail',
 } = process.env;
 
-useMiddlewareWithCurrentContext(PFCHECKOUT_SUCCESS_WEBHOOK_PATH, bodyParser.json());
+// useMiddlewareWithCurrentContext(PFCHECKOUT_SUCCESS_WEBHOOK_PATH, bodyParser.json());
 
-useMiddlewareWithCurrentContext(PFCHECKOUT_SUCCESS_WEBHOOK_PATH, async (request, response) => {
+// useMiddlewareWithCurrentContext(PFCHECKOUT_SUCCESS_WEBHOOK_PATH, async (request, response) => {
 
-});
+// });
 
-useMiddlewareWithCurrentContext(PFCHECKOUT_FAIL_WEBHOOK_PATH, bodyParser.json());
+// useMiddlewareWithCurrentContext(PFCHECKOUT_FAIL_WEBHOOK_PATH, bodyParser.json());
 
-useMiddlewareWithCurrentContext(PFCHECKOUT_FAIL_WEBHOOK_PATH, async (request, response) => {
+// useMiddlewareWithCurrentContext(PFCHECKOUT_FAIL_WEBHOOK_PATH, async (request, response) => {
 
-});
+// });
+
+enum IntegrationModes {
+  PaymentPage = 'PaymentPage',
+  Lightbox = 'Lightbox',
+  iFrame = 'iFrame',
+};
 
 const PostfinanceCheckout: IPaymentAdapter = {
   ...PaymentAdapter,
@@ -44,10 +51,13 @@ const PostfinanceCheckout: IPaymentAdapter = {
   actions: (params) => {
     const { modules } = params.context;
 
+    const SPACE_ID = parseInt(PFCHECKOUT_SPACE_ID, 10);
+    const USER_ID = parseInt(PFCHECKOUT_USER_ID, 10);
+
     const getConfig = () => {
       return {
-        space_id: PFCHECKOUT_SPACE_ID,
-        user_id: PFCHECKOUT_USER_ID,
+        space_id: SPACE_ID,
+        user_id: USER_ID,
         api_secret: PFCHECKOUT_SECRET,
       };
     };
@@ -89,12 +99,50 @@ const PostfinanceCheckout: IPaymentAdapter = {
 
       },
 
-      sign: async (transactionContext = {}) => {
-
+      sign: async (context: any = {}) => {
+        const { integrationMode }: { integrationMode: IntegrationModes } =
+          context.transactionContext || {};
+        // Option for fetching payment methods (iFrame / Lightbox) -> Return id, JS url, possible payment methods
+        const { orderPayment } = params.paymentContext;
+        // Return redirect url
+        const transactionService = getTransactionService();
+        const transactionPaymentPageService = getTransactionPaymentPageService();
+        const order = await modules.orders.findOrder({
+          orderId: orderPayment.orderId,
+        });
+        const pricing = OrderPricingSheet({
+          calculation: order.calculation,
+          currency: order.currency,
+        });
+        const totalAmount = pricing?.total({ useNetPrice: false }).amount;
+        if (totalAmount) {
+          const lineItemSum = new PostFinanceCheckout.model.LineItemCreate();
+          lineItemSum.name = `Bestellung ${orderPayment.orderId}`;
+          lineItemSum.type = PostFinanceCheckout.model.LineItemType.FEE;
+          lineItemSum.quantity = 1;
+          lineItemSum.uniqueId = orderPayment.orderId;
+          lineItemSum.amountIncludingTax = totalAmount / 100;
+          const transaction = new PostFinanceCheckout.model.TransactionCreate();
+          transaction.currency = order.currency;
+          transaction.lineItems = [lineItemSum];
+          const transactionCreateRes = await transactionService.create(SPACE_ID, transaction);
+          const transactionCreate = transactionCreateRes.body;
+          const paymentPageUrl = await transactionPaymentPageService.paymentPageUrl(
+            SPACE_ID,
+            transactionCreate.id,
+          );
+          const redirectUrl = paymentPageUrl.body;
+          return redirectUrl;
+        }
       },
 
       charge: async () => {
+        // if you return true, the status will be changed to PAID
 
+        // if you return false, the order payment status stays the
+        // same but the order status might change
+
+        // if you throw an error, you cancel the checkout process
       },
 
     };
