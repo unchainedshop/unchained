8350a2c58d17fb8c4b53b02c35b3067430864766
Pascal Kaufmann <kaufmae@me.com>
2022-02-28 13:38:37 +0100

    Fix lint

--- packages/core-payment/plugins/datatrans-v2/roundedAmountFromOrder.ts
+++ packages/core-payment/plugins/datatrans-v2/roundedAmountFromOrder.ts
@@ -1,7 +1,10 @@
-import { Context } from "@unchainedshop/types/api";
-import { Order } from "@unchainedshop/types/orders";
+import { Context } from '@unchainedshop/types/api';
+import { Order } from '@unchainedshop/types/orders';
 
-const roundedAmountFromOrder = (order: Order, context: Context): { currency: string; amount: number } => {
+const roundedAmountFromOrder = (
+  order: Order,
+  context: Context,
+): { currency: string; amount: number } => {
   const pricing = context.modules.orders.pricingSheet(order);
   const { currency, amount } = pricing.total({ useNetPrice: false });
   return {
