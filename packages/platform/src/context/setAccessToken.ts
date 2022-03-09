f1e0b3039469f70a60a00bd4475f49ca8739758b
Pascal Kaufmann <kaufmae@me.com>
2022-03-09 13:37:54 +0100

    Add setAccessToken helper

---
+++ packages/platform/src/context/setAccessToken.ts
@@ -0,0 +1,19 @@
+import { Context } from '@unchainedshop/types/api';
+
+export default async (
+  unchainedAPI: Context,
+  username: string,
+  secret: string,
+) => {
+  await unchainedAPI.modules.users.updateUser(
+    { username },
+    {
+      $set: {
+        'services.token': {
+          secret,
+        },
+      },
+    },
+    {},
+  );
+};
