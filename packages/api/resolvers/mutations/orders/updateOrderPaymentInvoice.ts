eeacd861fb6a4e7e7cb9041a14666d422dfb28d6
Pascal Kaufmann <kaufmae@me.com>
2022-03-14 14:48:30 +0100

    Fix updateOrderpaymentInvoice

--- packages/api/resolvers/mutations/orders/updateOrderPaymentInvoice.ts
+++ packages/api/resolvers/mutations/orders/updateOrderPaymentInvoice.ts
@@ -34,7 +34,7 @@ export default async function updateOrderPaymentInvoice(
 
   return modules.orders.payments.updateContext(
     orderPayment._id,
-    { orderId: orderPayment.orderId, context: meta },
+    { orderId: orderPayment.orderId, context: { meta } },
     context,
   );
 }
