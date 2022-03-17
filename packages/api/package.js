c987640f381abf9caf5e539864062481ddac0a49
Pascal Kaufmann <kaufmae@me.com>
2022-03-17 12:11:43 +0100

    Release api@rc.9.2

--- packages/api/package.js
+++ packages/api/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'unchained:api',
-  version: '1.0.0-rc.9.1',
+  version: '1.0.0-rc.9.2',
   summary: 'Unchained Engine: GraphQL API',
   git: 'https://github.com/unchainedshop/unchained',
   documentation: 'README.md',
