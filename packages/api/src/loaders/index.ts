702e8726dbb290f0cb2aa561d0d9fa5e64ac322b
Pascal Kaufmann <kaufmae@me.com>
2022-05-18 14:35:27 +0200

    Implement a working dataloader for product texts! hooray

--- packages/api/src/loaders/index.ts
+++ packages/api/src/loaders/index.ts
@@ -1,6 +1,8 @@
 import { UnchainedAPI, UnchainedLoaders } from '@unchainedshop/types/api';
 import DataLoader from 'dataloader';
 import { IncomingMessage } from 'http';
+import { systemLocale } from 'meteor/unchained:utils';
+import { Locale } from 'locale';
 
 export default async (req: IncomingMessage, unchainedAPI: UnchainedAPI): Promise<UnchainedLoaders> => {
   return {
@@ -23,12 +25,31 @@ export default async (req: IncomingMessage, unchainedAPI: UnchainedAPI): Promise
       return ids.map((key) => results.find((result) => result._id === key) || null);
     }),
     productTextLoader: new DataLoader(async (queries) => {
-      console.log(queries);
-      // return modules.products.texts.findLocalizedText({
-      //   productId: obj._id,
-      //   locale: forceLocale || localeContext.normalized,
-      // });
-      return [];
-    });
+      const productIds = [...new Set(queries.map((q) => q.productId).filter(Boolean))];
+
+      const texts = await unchainedAPI.modules.products.texts.findTexts(
+        { productId: { $in: productIds } },
+        {
+          sort: {
+            productId: 1,
+          },
+        },
+      );
+
+      const systemLocaleStrings = [systemLocale.normalized, systemLocale.language];
+
+      return queries.map(({ productId, locale }) => {
+        const localeObj = new Locale(locale);
+        const productTexts = texts.filter((text) => text.productId === productId);
+        if (!productTexts.length) return null;
+        const localeStrings = [localeObj.normalized, localeObj.language, ...systemLocaleStrings];
+
+        const productText = localeStrings.reduce((acc, localeString) => {
+          if (acc) return acc;
+          return productTexts.find((p) => p.locale === localeString);
+        }, null);
+        return productText || productTexts[0];
+      });
+    }),
   };
 };
