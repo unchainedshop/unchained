7a5d17f4e4553eabdfd5f92cdfda486073a43f17
Pascal Kaufmann <kaufmae@me.com>
2022-06-29 14:02:36 +0200

    Add mongodb package that works in meteor and in non-meteor environments

--- packages/mongodb/src/db/initDb.ts
+++ packages/mongodb/src/db/initDb.ts
@@ -1,30 +1,36 @@
-import { MongoInternals } from 'meteor/mongo';
-import { Db /* MongoClient */ } from 'mongodb';
-import { NpmModuleMongodb } from 'meteor/npm-mongo';
-// const url = process.env.MONGO_URL || 'mongodb://localhost:4011';
+import { Db, MongoClient } from 'mongodb';
 
-// const initDbNative = async (): Promise<Db> => {
-//   const client = new MongoClient(url);
+const url = process.env.MONGO_URL || 'mongodb://localhost:4011';
 
-//   const dbName = 'meteor';
-//   await client.connect();
-//   const db = client.db(dbName);
-//   return db;
-// };
-const originalFn = NpmModuleMongodb.Collection.prototype.updateOne;
-NpmModuleMongodb.Collection.prototype.updateOne = async function updateOne(...rest) {
-  const result = await originalFn.bind(this)(...rest);
-  if (!result) return result;
-  return {
-    result: { nModified: result.modifiedCount },
-    ...result,
-  };
+const initDbNative = async (): Promise<Db> => {
+  const client = new MongoClient(url);
+
+  const dbName = 'meteor';
+  await client.connect();
+  const db = client.db(dbName);
+  return db;
 };
 
+const isMeteor = typeof Meteor === "object";
+
+if (isMeteor) {
+  const { NpmModuleMongodb } = await import('meteor/npm-mongo');
+  const originalFn = NpmModuleMongodb.Collection.prototype.updateOne;
+  NpmModuleMongodb.Collection.prototype.updateOne = async function updateOne(...rest) {
+    const result = await originalFn.bind(this)(...rest);
+    if (!result) return result;
+    return {
+      result: { nModified: result.modifiedCount },
+      ...result,
+    };
+  };
+}
+
 const initDbMeteor = async (): Promise<Db> => {
+  const { MongoInternals } = await import('meteor/mongo');
   return MongoInternals.defaultRemoteCollectionDriver().mongo.db as Db;
 };
 
-const initDb = initDbMeteor;
+const initDb = isMeteor ? initDbMeteor : initDbNative;
 
 export { initDb };
