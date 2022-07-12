4b259ee719eda5c10e7094641cb8b79d3d5b0c64
Pascal Kaufmann <kaufmae@me.com>
2022-07-12 14:00:41 +0200

    esbuild: types

--- packages/types/esbuild.js
+++ packages/types/esbuild.js
@@ -3,7 +3,7 @@ import fg from 'fast-glob';
 
 export const buildNode = async ({ ...args }) => {
   await build({
-    entryPoints: await fg('src/**/*.ts'),
+    entryPoints: await fg('*.ts'),
     platform: 'node',
     target: 'node16',
     format: 'esm',
