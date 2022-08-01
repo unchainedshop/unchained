a37aabe26ebc82c04be09c0c7e1be1fe89cafdfb
Mikael Araya <mikaelaraya12@gmail.com>
2022-08-01 14:53:25 +0300

    Change import from meteor to unchainedshop

--- packages/core-orders/src/module/configureOrdersModule-queries.ts
+++ packages/core-orders/src/module/configureOrdersModule-queries.ts
@@ -1,7 +1,7 @@
 import { SortDirection, SortOption } from '@unchainedshop/types/api';
 import { Collection, FindOptions, Query } from '@unchainedshop/types/common';
 import { Order, OrderQueries, OrderQuery } from '@unchainedshop/types/orders';
-import { generateDbFilterById, buildSortOptions } from 'meteor/unchained:utils';
+import { generateDbFilterById, buildSortOptions } from '@unchainedshop/utils';
 
 const buildFindSelector = ({ includeCarts, status, userId, queryString }: OrderQuery) => {
   const selector: Query = {};
