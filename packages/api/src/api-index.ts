be20fb1903a337cabd04b2bd7b0079401ffaf76c
Pascal Kaufmann <pascal@unchained.shop>
2022-11-10 21:07:10 +0100

    v1.2.47 & legacy-meteor-minimal on meteor 2.8.0

--- packages/api/src/api-index.ts
+++ packages/api/src/api-index.ts
@@ -1,5 +1,7 @@
 import { UnchainedServerOptions } from '@unchainedshop/types/api';
 import { ApolloServer } from 'apollo-server-express';
+import fs from 'fs';
+import path from 'path';
 import createBulkImportServer from './createBulkImportServer';
 import createGraphQLServer from './createGraphQLServer';
 import { createContextResolver, getCurrentContextResolver, setCurrentContextResolver } from './context';
@@ -9,10 +11,25 @@ export * as acl from './acl';
 export * as errors from './errors';
 export { hashPassword } from './hashPassword';
 export * as roles from './roles';
-
 export { createContextResolver, getCurrentContextResolver, setCurrentContextResolver };
 
-const UNCHAINED_API_VERSION = '1.2.44'; // eslint-disable-line
+const loadJSON = (filename) => {
+  try {
+    const base = typeof __filename !== 'undefined' && __filename;
+    if (!base)
+      return {
+        version: process.env.npm_package_version,
+      };
+    const absolutePath = path.resolve(path.dirname(base), filename);
+    const data = JSON.parse(fs.readFileSync(absolutePath, 'utf-8'));
+    return data;
+  } catch (e) {
+    return null;
+  }
+};
+const packageJson = loadJSON('../package.json');
+
+const UNCHAINED_API_VERSION = process.env.UNCHAINED_API_VERSION || packageJson?.version || '1.2.x';
 
 export const startAPIServer = (
   options: UnchainedServerOptions,
