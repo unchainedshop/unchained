bc9bff6cfdd4d02f5ecc702b2f4db7ada8f816d1
Mikael Araya <mikaelaraya12@gmail.com>
2022-12-04 20:37:27 +0300

    Add test for file uploader

--- packages/file-upload/tests/file-upload-index.test.ts
+++ packages/file-upload/tests/file-upload-index.test.ts
@@ -1,5 +1,8 @@
+import buildHashedFilename from "../lib/buildHashedFilename";
+
+
 describe('File Uploader', () => {
-  it('Init', async () => {
-    expect(true).toBeTruthy()
+  it('Generate unique file name', async () => {
+    expect(buildHashedFilename('root', 'file1.jpg', new Date(new Date("2022-12-04T17:13:09.285Z")))).toEqual("4hI2YuPDacR8ERoR7iM6cQ-file1.jpg")
   });
 });
