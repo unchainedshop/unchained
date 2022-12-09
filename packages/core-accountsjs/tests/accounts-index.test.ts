2018578fe4fc2a82a69b7bbc49d81191dee94b70
Mikael Araya <mikaelaraya12@gmail.com>
2022-12-09 18:16:16 +0300

    Add unit test to accounts core functions

--- packages/core-accountsjs/tests/accounts-index.test.ts
+++ packages/core-accountsjs/tests/accounts-index.test.ts
@@ -1,6 +1,9 @@
 
 import {hashPassword} from '../src/module/utils/hashPassword'
 import {buf2hex, toArrayBuffer } from '../src/module/configureAccountsWebAuthnModule'
+import {dateProvider, idProvider} from '../src/accounts/dbManager'
+import {evaluateContext} from '../src/module/utils/evaluateContext'
+import {filterContext} from '../src/module/utils/filterContext'
 
 describe('Accounts', () => {
     it('Should hash plain string sha256 and return a hex ', async () => {      
@@ -13,4 +16,106 @@ describe('Accounts', () => {
     it('toArrayBuffer should return ArrayBuffer', () => {
       expect(toArrayBuffer(Buffer.from("hello"))).toBeInstanceOf(ArrayBuffer)
     })
+
+    describe('idProvider', () => {
+      it('should generate a string of length 17', () => {
+        expect(idProvider()).toHaveLength(17);
+      });
+    
+      it('should only contain alphanumeric characters', () => {
+        expect(idProvider()).toMatch(/^[a-zA-Z0-9]+$/);
+      });
+      it('should generate a unique ID each time it is called', () => {
+        const id1 = idProvider();
+        const id2 = idProvider();
+        expect(id1).not.toEqual(id2);
+      });
+    });
+
+
+describe('dateProvider', () => {
+  it('should return a Date object', () => {
+    const date = dateProvider(null);
+    expect(date).toBeInstanceOf(Date);
+  });
+
+  it('should return the current date by default', () => {
+    const date = dateProvider(null);
+    expect(date.toISOString()).toEqual(new Date().toISOString());
+  });
+
+  it('should return the provided date if passed as an argument', () => {
+    const testDate = new Date(2022, 11, 9);
+    const date = dateProvider(testDate);
+    expect(date).toEqual(testDate);
+  });
+});
+
+describe('evaluateContext', () => {
+  it('should return the expected context user object', () => {
+    const filteredContext: any = {
+      userId: '123',
+      user: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+      },
+      localeContext: {
+        normalized: 'en-US',
+      },
+      otherField: 'foo',
+    };
+
+    const expected = {
+      userIdBeforeLogin: '123',
+      userBeforeLogin: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+      },
+      normalizedLocale: 'en-US',
+      otherField: 'foo',
+    };
+
+    expect(evaluateContext(filteredContext)).toEqual(expected);
+  });
+});
+
+
+describe('filterContext', () => {
+  it('should return the expected use object with EXCLUDED_CONTEXT_FIELDS removed', () => {
+    const graphqlContext: any = {
+      userId: '123',
+      user: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+      },
+      localeContext: {
+        normalized: 'en-US',
+      },
+      otherField: 'foo',
+      modules: {},
+      services: {},
+      bulkImporter: {},
+      loaders: {},
+      req: {},
+      res: {},
+      _privateField: 'bar',
+    };
+
+    const expected: any = {
+      userId: '123',
+      user: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+      },
+      localeContext: {
+        normalized: 'en-US',
+      },
+      otherField: 'foo',
+    };
+
+    expect(filterContext(graphqlContext)).toEqual(expected);
+  });
+});
+
+
 });
