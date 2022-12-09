23857325eb80b0ac311b6aa8c9b1a8abaaa99096
Mikael Araya <mikaelaraya12@gmail.com>
2022-12-09 19:28:46 +0300

    Add assortment unit tests

--- packages/core-assortments/tests/assortments-index.test.ts
+++ packages/core-assortments/tests/assortments-index.test.ts
@@ -1,4 +1,6 @@
+import { Tree } from "@unchainedshop/types/common";
 import { buildFindSelector } from "../src/module/configureAssortmentsModule";
+import { divideTreeByLevels, concatItemsByLevels, fillToSameLengthArray, fillUp } from "../src/utils/tree-zipper/zipTreeByDeepness";
 
 
 
@@ -75,6 +77,74 @@ describe('Assortment', () => {
     });
   })
   
+  
+
+describe('divideTreeByLevels', () => {
+  it('should return the expected result', () => {
+    const array: Tree<string> = [
+      'a',
+      [
+        'b',
+        [
+          'c',
+          'd',
+        ],
+      ],
+      'e',
+    ];
+    const expected = [
+      {
+        level: 0,
+        items: ['a', 'e'],
+      },
+      {
+        level: 1,
+        items: ['b'],
+      },
+      {
+        level: 2,
+        items: ['c', 'd'],
+      },
+    ];
+
+    expect(divideTreeByLevels(array)).toEqual(expected);
+  });
+});
+
+
+describe('fillUp', () => {
+  it('should fill empty spaces with null based on the passed size parameter', () => {
+    const arr = [1, 2, 3];
+    const size = 5;
+  
+    const result = fillUp(arr, size);
+  
+    expect(result).toEqual([1, 2, 3, null, null]);
+
+  })
+  
+});
+
+describe('fillToSameLengthArray', () => {
+  it('should fill missing value place with null', () => {
+  const a = [1, 2, 3];
+  const b = [4, 5];
+
+  const result = fillToSameLengthArray(a, b);
+
+  expect(result).toEqual([[1, 2, 3], [4, 5, null]]);
+})
+});
+
+test('concatItemsByLevels', () => {
+  const levelArray = [    { level: 1, items: ['a', 'b'] },
+    { level: 2, items: ['c', 'd'] },
+    { level: 1, items: ['e', 'f'] },
+  ];
+  const result = concatItemsByLevels(levelArray);
+  expect(result.length).toBe(2);
+  
+});
 
 
 });
