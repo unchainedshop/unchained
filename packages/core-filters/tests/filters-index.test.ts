9ef5422187357c5ba7bdf2a701522b428a889430
Mikael Araya <mikaelaraya12@gmail.com>
2022-12-10 20:43:48 +0300

    Add unit test to core-filters

--- packages/core-filters/tests/filters-index.test.ts
+++ packages/core-filters/tests/filters-index.test.ts
@@ -1,4 +1,7 @@
+import range from "../src/filter-value-parsers/range";
 import { buildFindSelector } from "../src/module/configureFiltersModule";
+import { intersectSet } from "../src/utils/intersectSet";
+import { parseQueryArray } from "../src/utils/parseQueryArray";
 
 describe('Filters', () => {
 
@@ -35,5 +38,70 @@ describe('Filters', () => {
     });
 
   })
+
+  describe('intersectSet', () => {
+    it('should return an empty set if the two input sets are empty', () => {
+      const productIdSet: Set<string> = new Set();
+      const filterProductIdSet: Set<string> = new Set();
+      const result = intersectSet(productIdSet, filterProductIdSet);
+      expect(result).toEqual(new Set());
+    });
+  
+    it('should return an empty set if the two input sets do not have any common elements', () => {
+      const productIdSet = new Set(['1', '2', '3']);
+      const filterProductIdSet = new Set(['4', '5', '6']);
+      const result = intersectSet(productIdSet, filterProductIdSet);
+      expect(result).toEqual(new Set());
+    });
+  
+    it('should return a set containing only the common elements of the two input sets', () => {
+      const productIdSet = new Set(['1', '2', '3', '4']);
+      const filterProductIdSet = new Set(['2', '3', '5', '6']);
+      const result = intersectSet(productIdSet, filterProductIdSet);
+      expect(result).toEqual(new Set(['2', '3']));
+    });
+  
+    it('should not modify the original input sets', () => {
+      const productIdSet = new Set(['1', '2', '3']);
+      const filterProductIdSet = new Set(['2', '3', '4', '5']);
+      intersectSet(productIdSet, filterProductIdSet);
+      expect(productIdSet).toEqual(new Set(['1', '2', '3']));
+      expect(filterProductIdSet).toEqual(new Set(['2', '3', '4', '5']));
+    });
+  });
+
+
+describe('parseQueryArray', () => {
+  it('parses empty array', () => {
+    const query = [];
+    const expected = {};
+    const result = parseQueryArray(query);
+    expect(result).toEqual(expected);
+  });
+
+  it('parses array with one key-value pair', () => {
+    const query = [{ key: 'foo', value: 'bar' }];
+    const expected = { foo: ['bar'] };
+    const result = parseQueryArray(query);
+    expect(result).toEqual(expected);
+  });
+
+  it('parses array with multiple key-value pairs', () => {
+    const query = [      { key: 'foo', value: 'bar' },      { key: 'baz', value: 'qux' },    ];
+    const expected = { foo: ['bar'], baz: ['qux'] };
+    const result = parseQueryArray(query);
+    expect(result).toEqual(expected);
+  });
+
+  it('parses array with multiple values for the same key', () => {
+    const query = [      { key: 'foo', value: 'bar' },      { key: 'foo', value: 'baz' },    ];
+    const expected = { foo: ['bar', 'baz'] };
+    const result = parseQueryArray(query);
+    expect(result).toEqual(expected);
+  });
+});
+
+  
+  
   
 });
