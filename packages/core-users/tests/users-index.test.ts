513446a4b763a195ea7cd349aa8a88ce15f91baf
Pascal Kaufmann <pascal@unchained.shop>
2022-12-10 19:35:44 +0100

    Fix user tests

--- packages/core-users/tests/users-index.test.ts
+++ packages/core-users/tests/users-index.test.ts
@@ -15,16 +15,18 @@ describe('User', () => {
 
   describe('buildFindSelector', () => {
     it('Return the correct filter when no parameter is passed', async () => {
-      expect(buildFindSelector({})).toEqual({ guest: { '$ne': true } })
+      expect(buildFindSelector({})).toEqual({     "deleted": null,    guest: { '$in': [false, null] },
+    })
     });
     it('Return the correct filter when no parameter is passed queryString and includeGuest: true', async () => {
-      expect(buildFindSelector({queryString: "Hello world", includeGuests: true})).toEqual({ '$text': { '$search': 'Hello world' } })
+      expect(buildFindSelector({queryString: "Hello world", includeGuests: true})).toEqual({ "deleted": null,'$text': { '$search': 'Hello world' } })
     });
 
     it('Should include additional user field selector in addition too queryString and includeGuests', async () => {
       expect(buildFindSelector({queryString: "Hello world", includeGuests: false, "profile.displayName": "mikael"})).toEqual({
         'profile.displayName': 'mikael',
-        guest: { '$ne': true },
+        "deleted": null,
+        guest: { '$in': [false, null] },
         '$text': { '$search': 'Hello world' }
       })
     });
