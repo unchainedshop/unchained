c72c44cc160b0bba5ae34d4f18990b8be4dc7b84
Mikael Araya <mikaelaraya12@gmail.com>
2022-12-14 15:42:32 +0300

    Add unit test for baseDiscount adapter

---
+++ packages/utils/tests/base-discount-adapter.test.ts
@@ -0,0 +1,52 @@
+import { IDiscountAdapter } from '@unchainedshop/types/discount';
+import { BaseDiscountAdapter } from '../src/director/BaseDiscountAdapter';
+describe("BaseDiscountAdapter instantiation", () => {
+  const adapter: Omit<IDiscountAdapter, 'key' | 'label' | 'version'> = BaseDiscountAdapter;
+  const context: any = null
+  it("should initialize with default values", () => {
+    expect(adapter.orderIndex).toEqual(0);
+    expect(adapter.log).toEqual(expect.any(Function));
+    expect(adapter.isManualAdditionAllowed('')).resolves.toEqual(false);
+    expect(adapter.isManualRemovalAllowed()).resolves.toEqual(false);
+    
+    expect(adapter.actions(context)).resolves.toEqual({
+      isValidForSystemTriggering: expect.any(Function),
+      reserve: expect.any(Function),
+      release: expect.any(Function),
+      isValidForCodeTriggering: expect.any(Function),
+      discountForPricingAdapterKey: expect.any(Function),
+    });
+  });
+
+  describe('actions',  ()=> {
+    const actions =  adapter.actions(context)
+    
+    it('isValidForSystemTriggering', async () => {
+      const result = await adapter.actions(context);
+      
+      expect(await result.isValidForSystemTriggering()).toEqual(false)
+    })
+
+    it('reserve', async () => {
+      const result = await adapter.actions(context);
+      expect(await result.reserve({code: ''})).toEqual({})
+    })
+
+    it('release', async () => {
+      const result = await adapter.actions(context);
+      expect(await result.release()).toEqual(null)
+    })
+
+    it('isValidForCodeTriggering', async () => {
+      const result = await adapter.actions(context);
+      expect(await result.isValidForCodeTriggering({code: ''})).toEqual(false)
+    })
+
+    it('discountForPricingAdapterKey', async () => {
+      const result = await adapter.actions(context);
+      const calculationSheet: any = {}
+      expect(await result.discountForPricingAdapterKey({calculationSheet, pricingAdapterKey: 'key'})).toEqual(null)
+    })
+
+  })
+});
