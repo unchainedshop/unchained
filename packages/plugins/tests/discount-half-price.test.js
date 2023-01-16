38d9724e9f3f6465f3dac65c6e3a0bad1ea64373
Mikael Araya <mikaelaraya12@gmail.com>
2023-01-16 23:04:33 +0300

    Add utils unit test for HalfPrice pricing adapter

---
+++ packages/plugins/tests/discount-half-price.test.js
@@ -0,0 +1,46 @@
+import { HalfPrice  } from '../src/pricing/discount-half-price';
+import {jest} from '@jest/globals'
+describe('HundredOff', () => {
+  it('isManualAdditionAllowed', async () => {
+    expect(await HalfPrice.isManualAdditionAllowed()).toBeFalsy();
+  });
+
+  it('isManualRemovalAllowed', async () => {
+    expect(await HalfPrice.isManualRemovalAllowed()).toBeFalsy();
+  });
+
+  it('isValidForSystemTriggering', async () => {
+    const context = {
+      modules: {
+        users: {
+          findUserById: jest.fn(() => Promise.resolve({ tags: ['half-price'] }))
+        }
+      },
+      order: { userId: 'user-id' }
+    };
+    const actions = await HalfPrice.actions({ context });
+    expect(await actions.isValidForSystemTriggering()).toBeTruthy();
+
+    context.modules.users.findUserById.mockImplementationOnce(() => Promise.resolve({ tags: [] }));
+    expect(await actions.isValidForSystemTriggering()).toBeFalsy();
+  });
+  it('discountForPricingAdapterKey', async () => {
+    const context = {};
+    const actions = await HalfPrice.actions({ context });
+    expect(actions.discountForPricingAdapterKey({ pricingAdapterKey: 'shop.unchained.pricing.product-discount' })).toEqual({ rate: 0.5 });
+    expect(actions.discountForPricingAdapterKey({ pricingAdapterKey: 'shop.unchained.pricing.other-discount' })).toBeNull();
+  });
+
+  it('isValidForCodeTriggering', async () => {
+    const context = {};
+    const actions = await HalfPrice.actions({ context });
+    expect(await actions.isValidForCodeTriggering()).toBeFalsy();
+  });
+
+  it('has correct properties', () => {
+    expect(HalfPrice.key).toEqual('shop.unchained.discount.half-price');
+    expect(HalfPrice.label).toEqual('Half Price');
+    expect(HalfPrice.version).toEqual('1.0.0');
+    expect(HalfPrice.orderIndex).toEqual(3);
+  });
+});
