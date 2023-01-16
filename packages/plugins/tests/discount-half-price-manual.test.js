259865165c10f26a75acf80e7eeb5ad75e6db6c4
Mikael Araya <mikaelaraya12@gmail.com>
2023-01-16 22:21:09 +0300

    Add util unit tests for HalfPriceManual

---
+++ packages/plugins/tests/discount-half-price-manual.test.js
@@ -0,0 +1,49 @@
+import { HalfPriceManual } from '../src/pricing/discount-half-price-manual';
+
+describe('HalfPriceManual', () => {
+  it('isManualAdditionAllowed', async () => {
+    expect(await HalfPriceManual.isManualAdditionAllowed()).toBeTruthy();
+  });
+  
+  it('isManualRemovalAllowed', async () => {
+    expect(await HalfPriceManual.isManualRemovalAllowed()).toBeTruthy();
+  });
+  
+  it('isValidForSystemTriggering', async () => {
+    const context = {};
+    const actions = await HalfPriceManual.actions({ context });
+    expect(await actions.isValidForSystemTriggering()).toBeFalsy();
+  });
+  
+  it('isValidForCodeTriggering', async () => {
+    const context = {};
+    const actions = await HalfPriceManual.actions({ context });
+    expect(await actions.isValidForCodeTriggering({ code: 'HALFPRICE' })).toBeTruthy();
+    expect(await actions.isValidForCodeTriggering({ code: 'othercode' })).toBeFalsy();
+  });
+  
+  it('discountForPricingAdapterKey', async () => {
+    const context = {};
+    const actions = await HalfPriceManual.actions({ context });
+    expect(
+      actions.discountForPricingAdapterKey({ pricingAdapterKey: 'shop.unchained.pricing.product-discount' })
+    ).toEqual({ rate: 0.5 });
+    expect(actions.discountForPricingAdapterKey({ pricingAdapterKey: 'shop.unchained.pricing.other-discount' })).toBeNull();
+  });
+
+  it('Actions', async () => {
+    const context = {};
+    const actions = await HalfPriceManual.actions({ context });
+    expect(typeof actions.isValidForSystemTriggering).toBe('function');
+    expect(typeof actions.isValidForCodeTriggering).toBe('function');
+    expect(typeof actions.discountForPricingAdapterKey).toBe('function');
+  });
+
+  it('has correct properties', () => {
+    expect(HalfPriceManual.key).toEqual('shop.unchained.discount.half-price-manual');
+    expect(HalfPriceManual.label).toEqual('Half Price Manual');
+    expect(HalfPriceManual.version).toEqual('1.0.0');
+    expect(HalfPriceManual.orderIndex).toEqual(2);
+  });
+
+});
