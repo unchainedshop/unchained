917605759a38e7eeade0f1b6fb3051ef2764995e
Pascal Kaufmann <pascal@unchained.shop>
2023-02-17 22:37:01 +0100

    Reduce chars for hash

--- packages/utils/src/generate-random-hash.ts
+++ packages/utils/src/generate-random-hash.ts
@@ -1,6 +1,6 @@
 import Hashids from 'hashids';
 
-const hashids = new Hashids('unchained', 6, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890');
+const hashids = new Hashids('unchained', 6, 'ABCDEFGHIJKLMNPQRSTUVWXYZ23456789');
 
 export default () => {
   const randomNumber = Math.floor(Math.random() * (999999999 - 1)) + 1;
