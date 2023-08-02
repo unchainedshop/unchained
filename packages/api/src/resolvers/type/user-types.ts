5365149108722b616c76adb29c22947b6920d2c0
Mikael Araya <mikaelaraya12@gmail.com>
2023-08-02 21:21:54 +0300

    Fix type

--- packages/api/src/resolvers/type/user-types.ts
+++ packages/api/src/resolvers/type/user-types.ts
@@ -89,9 +89,9 @@ export interface UserHelperTypes {
       limit?: number;
       offset?: number;
     },
-    Promise<Array<ProductReview>>
+    Array<ProductReview>
   >;
-  reviewsCount: HelperType<any, Promise<number>>;
+  reviewsCount: HelperType<any, number>;
 }
 
 const {
