9831737fec58ec4602476199b60fd6ec4018dbd5
Pascal Kaufmann <pascal@unchained.shop>
2023-09-11 08:39:31 +0200

    Revert back to gross seeds
    This reverts commit b196891cb55d2d18ba6412b9b3a56b0d09330924.

--- tests/seeds/orders.js
+++ tests/seeds/orders.js
@@ -20,8 +20,7 @@ export const SimpleOrder = {
       category: 'ITEMS',
       amount: 30000,
     },
-    { category: 'ITEMS', amount: -2144.8467966573808 },
-    { category: 'TAXES', amount: 2144.8467966573808, baseCategory: 'ITEMS' },
+    { category: 'TAXES', amount: 2144.8467966573808 },
     {
       category: 'PAYMENT',
       amount: 0,
@@ -116,7 +115,6 @@ export const SimplePosition = {
       isTaxable: false,
       isNetPrice: false,
       rate: 0.077,
-      baseCategory: 'ITEM',
       meta: {
         adapter: 'shop.unchained.pricing.product-swiss-tax',
       },
@@ -217,13 +215,8 @@ export const DiscountedOrder = {
       amount: 100000,
     },
     {
-      category: 'ITEMS',
-      amount: -7149.489322191272,
-    },
-    {
       category: 'TAXES',
       amount: 7149.489322191272,
-      baseCategory: 'ITEMS',
     },
     {
       category: 'PAYMENT',
@@ -239,15 +232,8 @@ export const DiscountedOrder = {
       discountId: 'discounted-order-discount',
     },
     {
-      category: 'DISCOUNTS',
-      amount: 714.9489322191266,
-      discountId: 'discounted-order-discount',
-    },
-    {
       category: 'TAXES',
       amount: -714.9489322191266,
-      baseCategory: 'DISCOUNTS',
-      discountId: 'discounted-order-discount',
     },
   ],
 };
@@ -318,7 +304,6 @@ export const DiscountedPosition = {
       category: 'TAX',
       amount: 8579.387186629523,
       rate: 0.077,
-      baseCategory: 'ITEM',
       meta: {
         adapter: 'shop.unchained.pricing.product-swiss-tax',
       },
@@ -336,7 +321,6 @@ export const DiscountedPosition = {
       category: 'TAX',
       amount: -4289.6935933147615,
       rate: 0.077,
-      baseCategory: 'DISCOUNT',
       meta: {
         adapter: 'shop.unchained.pricing.product-swiss-tax',
       },
