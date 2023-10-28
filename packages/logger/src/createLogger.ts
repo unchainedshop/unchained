b70bd52f4ea1e2dd4c8b1599c1a2b4b0f79ae526
Mikael Araya <mikaelaraya12@gmail.com>
2023-10-28 22:06:26 +0300

    Enable ts strict mode in core-logger

--- packages/logger/src/createLogger.ts
+++ packages/logger/src/createLogger.ts
@@ -6,10 +6,23 @@ import { LogLevel } from './logger.types.js';
 const require = createRequire(import.meta.url);
 const { stringify } = require('safe-stable-stringify');
 
-const { DEBUG = '', LOG_LEVEL = LogLevel.Info, UNCHAINED_LOG_FORMAT = 'unchained' } = process.env;
+enum SupportedLogFormats {
+  'unchained',
+  'json',
+}
+
+const {
+  DEBUG = '',
+  LOG_LEVEL = LogLevel.Info,
+  UNCHAINED_LOG_FORMAT = 'unchained',
+} = process.env as unknown as {
+  DEBUG?: string;
+  LOG_LEVEL: LogLevel;
+  UNCHAINED_LOG_FORMAT: SupportedLogFormats;
+};
 
 const { combine, colorize, json } = format;
-
+const logFormat = (UNCHAINED_LOG_FORMAT as string).toLowerCase();
 const debugStringContainsModule = (debugString: string, moduleName: string) => {
   if (!debugString) return false;
   const loggingMatched = debugString.split(',').reduce((accumulator: any, name: string) => {
@@ -40,12 +53,12 @@ const myFormat = format.printf(({ level, message, label, timestamp, stack, ...re
     .join(' ');
 });
 
-const UnchainedLogFormats = {
+const UnchainedLogFormats: { [key: string]: ReturnType<typeof json | typeof combine> } = {
   unchained: combine(colorize(), myFormat),
   json: json(),
 };
 
-if (!UnchainedLogFormats[UNCHAINED_LOG_FORMAT.toLowerCase()]) {
+if (!UnchainedLogFormats[logFormat]) {
   throw new Error(
     `UNCHAINED_LOG_FORMAT is invalid, use one of ${Object.keys(UnchainedLogFormats).join(',')}`,
   );
@@ -63,7 +76,7 @@ export const createLogger = (moduleName: string, moreTransports: Array<Transport
     ),
     transports: [
       new transports.Console({
-        format: UnchainedLogFormats[UNCHAINED_LOG_FORMAT],
+        format: UnchainedLogFormats[logFormat],
         stderrLevels: [LogLevel.Error],
         consoleWarnLevels: [LogLevel.Warning],
         level: loggingMatched ? LogLevel.Debug : LOG_LEVEL,
