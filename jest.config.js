63d984bddf98c9bf444b35b955fb1aefde331c0c
Pascal Kaufmann <pascal@unchained.shop>
2023-12-01 17:20:01 +0100

    Fix package.json to use experimental vm modules

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 export default {
   testPathIgnorePatterns: ['<rootDir>/examples/', '<rootDir>/docs', '<rootDir>/packages'],
-  transform: {
-    // '^.+\\.ts?$': 'ts-jest',
-  },
+  transform: {},
   preset: '@shelf/jest-mongodb',
   globalSetup: './tests/jest-global-setup.js',
   globalTeardown: './tests/jest-global-teardown.js',
