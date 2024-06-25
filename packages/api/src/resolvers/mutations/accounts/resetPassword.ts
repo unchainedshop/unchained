acd9b89b156334dc75dbac85b583802960be67ef
Pascal Kaufmann <pascal@unchained.shop>
2024-06-25 12:47:21 +0200

    Fix typos and update to same deps

--- packages/api/src/resolvers/mutations/accounts/resetPassword.ts
+++ packages/api/src/resolvers/mutations/accounts/resetPassword.ts
@@ -50,7 +50,7 @@ export default async function resetPassword(
     //   } catch (e) {
     //     /* */
     //   }
-    }
+    // }
   } catch (e) {
     if (e.cause === 'PASSWORD_INVALID') throw new PasswordInvalidError({ userId: user._id });
     else throw e;
