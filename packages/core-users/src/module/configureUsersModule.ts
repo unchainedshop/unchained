9de66e16922825862175f581a170604174be6acb
Mikael Araya <mikaelaraya12@gmail.com>
2024-08-20 22:28:18 +0300

    Add missing configureUserSettingOptions call

--- packages/core-users/src/module/configureUsersModule.ts
+++ packages/core-users/src/module/configureUsersModule.ts
@@ -165,6 +165,7 @@ export const configureUsersModule = async ({
   options,
   migrationRepository,
 }: ModuleInput<UserSettingsOptions>): Promise<UsersModule> => {
+  userSettings.configureSettings(options || {}, db);
   registerEvents(USER_EVENTS);
   const Users = await UsersCollection(db);
 
