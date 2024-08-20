429157830289a3885228a847cd301de8547fba2f
Mikael Araya <mikaelaraya12@gmail.com>
2024-08-20 21:13:28 +0300

    Validate user ids

--- packages/api/src/resolvers/mutations/users/deleteAccount.ts
+++ packages/api/src/resolvers/mutations/users/deleteAccount.ts
@@ -1,10 +1,16 @@
 import { log } from '@unchainedshop/logger';
 import { Context } from '../../../types.js';
+import { InvalidIdError, UserNotFoundError } from '../../../errors.js';
 
 const deleteAccount = async (_, { userId }, context: Context) => {
   const { modules, userAgent, userId: currentUserId } = context;
   log(`mutation deleteAccount ${userId} ${userAgent}`, { userId });
-  await modules.users.deleteAccount({ userId: userId || currentUserId }, context);
+  const normalizedUserId = userId || currentUserId;
+  if (!normalizedUserId) throw new InvalidIdError({ userId: normalizedUserId });
+  if (!(await modules.users.userExists({ userId: normalizedUserId })))
+    throw new UserNotFoundError({ userId: normalizedUserId });
+
+  await modules.users.deleteAccount({ userId: normalizedUserId }, context);
   return true;
 };
 
