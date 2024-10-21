ac0eb1aa0c3f1d3017811c7a435130ed9c939fd6
Pascal Kaufmann <pascal@unchained.shop>
2024-10-21 23:52:32 +0200

    Fix double describe

--- packages/core-events/src/module/buildFindSelector.test.ts
+++ packages/core-events/src/module/buildFindSelector.test.ts
@@ -1,30 +1,28 @@
 import { buildFindSelector } from "./configureEventsModule.js";
 
-describe('Event', () => {
-  describe('buildFindSelector', () => {
-    it('Return correct filter object when passed create, queryString, types', () => {
+describe('buildFindSelector', () => {
+  it('Return correct filter object when passed create, queryString, types', () => {
     expect(buildFindSelector({created: new Date("2022-12-03T18:23:38.278Z"), queryString: "Hello world", types: ['PRODUCT_CREATED']})).toEqual({
-        type: { '$in': [ 'PRODUCT_CREATED' ] },
-        '$text': { '$search': 'Hello world' },
-        created: { '$gte': new Date( "2022-12-03T18:23:38.278Z" )}
-      })
-    });
+      type: { '$in': [ 'PRODUCT_CREATED' ] },
+      '$text': { '$search': 'Hello world' },
+      created: { '$gte': new Date( "2022-12-03T18:23:38.278Z" )}
+    })
+  });
 
-    it('Return correct filter object when passed create, queryString', () => {
-      expect(buildFindSelector({created: new Date("2022-12-03T18:23:38.278Z"), queryString: "Hello world"})).toEqual({       
-        '$text': { '$search': 'Hello world' },
-        created: { '$gte': new Date( "2022-12-03T18:23:38.278Z" )}
-      })
-    });
+  it('Return correct filter object when passed create, queryString', () => {
+    expect(buildFindSelector({created: new Date("2022-12-03T18:23:38.278Z"), queryString: "Hello world"})).toEqual({       
+      '$text': { '$search': 'Hello world' },
+      created: { '$gte': new Date( "2022-12-03T18:23:38.278Z" )}
+    })
+  });
 
-    it('Return correct filter object when passed create', () => {
-      expect(buildFindSelector({created: new Date("2022-12-03T18:23:38.278Z")})).toEqual({              
-        created: { '$gte': new Date( "2022-12-03T18:23:38.278Z" )}
-      })
-    });
+  it('Return correct filter object when passed create', () => {
+    expect(buildFindSelector({created: new Date("2022-12-03T18:23:38.278Z")})).toEqual({              
+      created: { '$gte': new Date( "2022-12-03T18:23:38.278Z" )}
+    })
+  });
 
-    it('Return correct filter object when passed no argument', () => {
-      expect(buildFindSelector({})).toEqual({})
-    });
-  })
-});
+  it('Return correct filter object when passed no argument', () => {
+    expect(buildFindSelector({})).toEqual({})
+  });
+})
