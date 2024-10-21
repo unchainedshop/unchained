664af5212f0698a9c41644bce946e9e030d8e12c
Pascal Kaufmann <pascal@unchained.shop>
2024-10-21 23:39:52 +0200

    Fix async test

--- packages/core-events/src/module/buildFindSelector.test.ts
+++ packages/core-events/src/module/buildFindSelector.test.ts
@@ -2,7 +2,7 @@ import { buildFindSelector } from "./configureEventsModule.js";
 
 describe('Event', () => {
   describe('buildFindSelector', () => {
-  it('Return correct filter object when passed create, queryString, types', async () => {
+  it('Return correct filter object when passed create, queryString, types', () => {
    expect(buildFindSelector({created: new Date("2022-12-03T18:23:38.278Z"), queryString: "Hello world", types: ['PRODUCT_CREATED']})).toEqual({
       type: { '$in': [ 'PRODUCT_CREATED' ] },
       '$text': { '$search': 'Hello world' },
@@ -10,20 +10,20 @@ describe('Event', () => {
     })
   });
 
-  it('Return correct filter object when passed create, queryString', async () => {
+  it('Return correct filter object when passed create, queryString', () => {
     expect(buildFindSelector({created: new Date("2022-12-03T18:23:38.278Z"), queryString: "Hello world"})).toEqual({       
        '$text': { '$search': 'Hello world' },
        created: { '$gte': new Date( "2022-12-03T18:23:38.278Z" )}
      })
    });
 
-   it('Return correct filter object when passed create', async () => {
+   it('Return correct filter object when passed create', () => {
     expect(buildFindSelector({created: new Date("2022-12-03T18:23:38.278Z")})).toEqual({              
        created: { '$gte': new Date( "2022-12-03T18:23:38.278Z" )}
      })
    });
 
-   it('Return correct filter object when passed no argument', async () => {
+   it('Return correct filter object when passed no argument', () => {
     expect(buildFindSelector({})).toEqual({})
    });
 })
