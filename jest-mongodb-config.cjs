681242ef315e97316ce1fee28dfb6b8e3455427d
Pascal Kaufmann <pascal@unchained.shop>
2024-10-29 18:07:22 +0100

    Update to Mongo 8
    # Conflicts:
    #	examples/minimal/.env.defaults
    #	jest-mongodb-config.cjs

--- jest-mongodb-config.cjs
+++ jest-mongodb-config.cjs
@@ -4,6 +4,10 @@ module.exports = {
       dbName: 'jest',
       port: 4011,
     },
+    binary: {
+      version: '8.0.1',
+      skipMD5: true,
+    },
     autoStart: false,
   },
   useSharedDBForAllJestWorkers: true,
