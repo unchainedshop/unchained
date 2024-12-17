10864a2c81756eb24ae1f1c130c504e8e9c87ad0
Pascal Kaufmann <pascal@unchained.shop>
2024-12-17 22:09:08 +0100

    Fix unit test

--- packages/core-users/src/module/removeConfidentialServiceHashes.test.ts
+++ packages/core-users/src/module/removeConfidentialServiceHashes.test.ts
@@ -1,6 +1,6 @@
-import { User } from '../types.js';
 import { removeConfidentialServiceHashes } from './configureUsersModule.js';
 import user from '../../tests/mock/user-mock.js';
+import { User } from '../db/UsersCollection.js';
 
 describe('removeConfidentialServiceHashes', () => {
   it('Should remove sensitive user credentials ', () => {
