3d7fac9e78a771c7abdaf0636915846771778312
Mikael Araya <mikaelaraya12@gmail.com>
2024-12-20 15:57:32 +0300

    Remove user from session when marking it as deleted

--- packages/core-users/src/module/configureUsersModule.ts
+++ packages/core-users/src/module/configureUsersModule.ts
@@ -583,6 +583,9 @@ export const configureUsersModule = async ({
     },
 
     markDeleted: async (userId: string): Promise<User> => {
+      await db.collection('sessions').deleteMany({
+        session: { $regex: `"user":"${userId}"` },
+      });
       const user = await Users.findOneAndUpdate(
         { _id: userId },
         {
@@ -611,9 +614,6 @@ export const configureUsersModule = async ({
     },
 
     deletePermanently: async ({ userId }: { userId: string }): Promise<User> => {
-      await db.collection('sessions').deleteMany({
-        session: { $regex: `"user":"${userId}"` },
-      });
       return Users.findOneAndDelete({ _id: userId });
     },
 
