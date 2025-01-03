e16819ecef85406053577a6f45d5250cf2b18f36
Pascal Kaufmann <pascal@unchained.shop>
2025-01-03 11:02:51 +0100

    Add missing plugins

--- packages/plugins/src/presets/all.ts
+++ packages/plugins/src/presets/all.ts
@@ -11,6 +11,10 @@ import '../payment/invoice-prepaid.js';
 import '../payment/paypal-checkout.js';
 import appleTransactionsModules from '../payment/apple-iap/index.js';
 import saferpayTransactionsModules from '../payment/saferpay/index.js';
+import '../payment/stripe/index.js';
+import '../payment/postfinance-checkout/index.js';
+import '../payment/datatrans-v2/index.js';
+import '../payment/payrexx/index.js';
 
 // Filter & Search
 import '../filters/strict-equal.js';
