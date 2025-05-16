f847d2a3bc24bbdb6cb8448c7d2040989de01b9f
Pascal Kaufmann <pascal@unchained.shop>
2025-05-16 10:53:37 +0200

    Backport E-Mail escape regex

--- packages/mongodb/src/email-regex-operator.ts
+++ packages/mongodb/src/email-regex-operator.ts
@@ -2,6 +2,10 @@ export function emailRegexOperator(string) {
   if (typeof string !== 'string') {
     throw new TypeError('Expected a string');
   }
-  const escappedEmail = string.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d');
-  return { $regex: new RegExp(`${escappedEmail}$`, 'i') };
+  const escapped = string.trim().replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+
+  if (escapped.length === 0) throw new Error('String is empty after trimming');
+  if (escapped.length > 255) throw new Error('String exceeds maximum allowed length');
+
+  return { $regex: new RegExp(`^${escapped}$`, 'i') };
 }
