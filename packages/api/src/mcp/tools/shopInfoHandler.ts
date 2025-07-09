5bc707400ccc70b8816070b7844e05ed5d1a247e
Mikael Araya <mikaelaraya12@gmail.com>
2025-07-09 14:28:18 +0300

    Fix shop info handler undefined bug

--- packages/api/src/mcp/tools/shopInfoHandler.ts
+++ packages/api/src/mcp/tools/shopInfoHandler.ts
@@ -5,12 +5,14 @@ export async function shopInfoHandler(context: Context) {
   const { version, userId, loaders, locale, countryCode } = context;
 
   try {
-    log('query shopInfo', { userId });
+    log('handler shopInfoHandler', { userId });
 
     const language = await loaders.languageLoader.load({ isoCode: locale.language });
 
     const country = await loaders.countryLoader.load({ isoCode: countryCode });
-    const defaultLanguageIsoCode = language.isoCode + '-' + country.isoCode;
+    const defaultLanguageIsoCode = language?.isoCode
+      ? `${language.isoCode}${country?.isoCode ? '-' + country.isoCode : ''}`
+      : null;
 
     return {
       content: [
