3a3bdf79486264ec3260301f93c4678ac5beddba
Mikael Araya <mikaelaraya12@gmail.com>
2025-10-09 14:54:48 +0300

    Add remove and verify web3 address integration tests

--- tests/auth-web3.test.js
+++ tests/auth-web3.test.js
@@ -5,6 +5,7 @@ import {
   createLoggedInGraphqlFetch,
   createAnonymousGraphqlFetch,
   disconnect,
+  getConnection,
 } from './helpers.js';
 import { USER_TOKEN } from './seeds/users.js';
 
@@ -114,6 +115,20 @@ test.describe('Web3 Authentication', () => {
 
   test.describe('Mutation.removeWeb3Address for logged in user', () => {
     test('should remove web3 address from user', async () => {
+      const testAddress = '0xabcdef9876543210abcdef9876543210abcdef98';
+      await graphqlFetchAsUser({
+        query: /* GraphQL */ `
+          mutation AddWeb3Address($address: String!) {
+            addWeb3Address(address: $address) {
+              _id
+            }
+          }
+        `,
+        variables: {
+          address: testAddress,
+        },
+      });
+
       const {
         data: { removeWeb3Address },
       } = await graphqlFetchAsUser({
@@ -128,14 +143,14 @@ test.describe('Web3 Authentication', () => {
           }
         `,
         variables: {
-          address: '0x1234567890123456789012345678901234567890',
+          address: testAddress,
         },
       });
 
       assert.ok(removeWeb3Address);
       assert.strictEqual(removeWeb3Address._id, 'user');
       const foundAddress = removeWeb3Address.web3Addresses?.find(
-        (a) => a.address === '0x1234567890123456789012345678901234567890',
+        (a) => a.address.toLowerCase() === testAddress.toLowerCase(),
       );
       assert.strictEqual(foundAddress, undefined);
     });
@@ -176,6 +191,144 @@ test.describe('Web3 Authentication', () => {
 
       assert.ok(errors);
       assert.strictEqual(errors[0]?.extensions?.code, 'NoPermissionError');
+    });
+  });
+
+  test.describe('Mutation.verifyWeb3Address', () => {
+    test('should return error when not logged in', async () => {
+      const { errors } = await graphqlFetchAsAnonymous({
+        query: /* GraphQL */ `
+          mutation VerifyWeb3Address($address: String!, $hash: String!) {
+            verifyWeb3Address(address: $address, hash: $hash) {
+              _id
+              username
+            }
+          }
+        `,
+        variables: {
+          address: '0x1234567890123456789012345678901234567890',
+          hash: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        },
+      });
+
+      assert.ok(errors);
+      assert.strictEqual(errors[0]?.extensions?.code, 'NoPermissionError');
+    });
+
+    test('should return error for non-existing address', async () => {
+      const { errors } = await graphqlFetchAsUser({
+        query: /* GraphQL */ `
+          mutation VerifyWeb3Address($address: String!, $hash: String!) {
+            verifyWeb3Address(address: $address, hash: $hash) {
+              _id
+              username
+            }
+          }
+        `,
+        variables: {
+          address: '0xnonexistent1234567890123456789012345678',
+          hash: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        },
+      });
+
+      assert.ok(errors);
+      assert.strictEqual(errors[0]?.extensions?.code, 'UserWeb3AddressNotFoundError');
+    });
+
+    test('should return error for invalid signature', async () => {
+      await graphqlFetchAsUser({
+        query: /* GraphQL */ `
+          mutation AddWeb3Address($address: String!) {
+            addWeb3Address(address: $address) {
+              _id
+            }
+          }
+        `,
+        variables: {
+          address: '0x1234567890123456789012345678901234567890',
+        },
+      });
+
+      const { errors } = await graphqlFetchAsUser({
+        query: /* GraphQL */ `
+          mutation VerifyWeb3Address($address: String!, $hash: String!) {
+            verifyWeb3Address(address: $address, hash: $hash) {
+              _id
+              username
+            }
+          }
+        `,
+        variables: {
+          address: '0x1234567890123456789012345678901234567890',
+          hash: '0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+        },
+      });
+
+      assert.ok(errors);
+      assert.ok(errors.length > 0);
+    });
+  });
+
+  test.describe('Mutation.verifyWeb3Address with valid signature', () => {
+    test('should successfully verify valid signature', async () => {
+      const address = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+      const nonce = 'test';
+      const signature =
+        '0xf755d9a72d5b7386765e7f0e833af68795b739a267122dae933f41b781b5aed0626ce3263308ebd4c37bed84319b66da2794368771046825bd89b98ba68c4e871b';
+
+      await graphqlFetchAsUser({
+        query: /* GraphQL */ `
+          mutation AddWeb3Address($address: String!) {
+            addWeb3Address(address: $address) {
+              _id
+            }
+          }
+        `,
+        variables: {
+          address,
+        },
+      });
+
+      const connection = getConnection();
+      const db = connection.db('test');
+      const Users = db.collection('users');
+
+      await Users.updateOne(
+        { _id: 'user', 'services.web3.address': address },
+        {
+          $set: {
+            'services.web3.$.nonce': nonce,
+          },
+        },
+      );
+
+      const {
+        data: { verifyWeb3Address },
+      } = await graphqlFetchAsUser({
+        query: /* GraphQL */ `
+          mutation VerifyWeb3Address($address: String!, $hash: String!) {
+            verifyWeb3Address(address: $address, hash: $hash) {
+              _id
+              web3Addresses {
+                address
+                verified
+                nonce
+              }
+            }
+          }
+        `,
+        variables: {
+          address,
+          hash: signature,
+        },
+      });
+
+      assert.ok(verifyWeb3Address);
+      const verifiedAddress = verifyWeb3Address.web3Addresses.find(
+        (a) => a.address.toLowerCase() === address.toLowerCase(),
+      );
+      assert.ok(verifiedAddress);
+      assert.strictEqual(verifiedAddress.verified, true, 'Address should be verified');
     });
   });
 });
