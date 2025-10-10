e3cf5bc52b5ef9437283c676a33142117327d4e2
Pascal Kaufmann <pascal@unchained.shop>
2025-10-10 11:22:30 +0200

    Revert buildObfuscatedFieldsFilter

--- packages/utils/src/build-obfuscated-fields-filter.ts
+++ packages/utils/src/build-obfuscated-fields-filter.ts
@@ -4,13 +4,10 @@ const defaultObfuscatedFields = [
   'oldPassword',
   'authorization',
   'secret',
-  'accessKey',
-  'accessToken',
+  'accesskey',
+  'accesstoken',
   'token',
 ];
-
-// Dangerous properties that should never be modified to prevent prototype pollution
-const DANGEROUS_PROPERTIES = ['__proto__', 'constructor', 'prototype'];
 
 const buildObfuscatedFieldsFilter = (blacklistedVariables: string[] = []) => {
   const sensitiveFields = blacklistedVariables || defaultObfuscatedFields;
@@ -23,27 +20,16 @@ const buildObfuscatedFieldsFilter = (blacklistedVariables: string[] = []) => {
     }
 
     if (typeof data === 'object') {
-      // Create a new object to avoid mutating the original
-      const result = {};
-
-      // Use Object.keys to only iterate over own enumerable properties
-      Object.keys(data).forEach((key) => {
-        // Skip dangerous properties to prevent prototype pollution
-        if (DANGEROUS_PROPERTIES.includes(key)) {
-          return;
-        }
-
-        // Use hasOwnProperty for additional safety
-        if (Object.prototype.hasOwnProperty.call(data, key)) {
-          if (sensitiveFields.includes(key)) {
-            result[key] = '******';
-          } else {
-            result[key] = obfuscateSensitiveFields(data[key]);
-          }
+      const temp = data;
+      Object.keys(temp).forEach((key) => {
+        if (sensitiveFields.includes(key)) {
+          temp[key] = '******';
+        } else {
+          temp[key] = obfuscateSensitiveFields(temp[key]);
         }
       });
 
-      return result;
+      return temp; // Return the modified copy
     }
 
     return data; // Return unchanged data for non-objects
