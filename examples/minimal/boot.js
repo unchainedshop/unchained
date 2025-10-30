493fad4b3986579756d19fbc3051dbc2e06f0837
Pascal Kaufmann <pascal@unchained.shop>
2025-10-30 13:32:56 +0100

    Add readme to minimal

--- examples/minimal/boot.js
+++ examples/minimal/boot.js
@@ -1,15 +1,14 @@
 import Fastify from 'fastify';
 import { startPlatform } from '@unchainedshop/platform';
+import { connect } from '@unchainedshop/api/fastify';
 import baseModules from '@unchainedshop/plugins/presets/base.js';
 import initPluginMiddlewares from '@unchainedshop/plugins/presets/base-fastify.js';
-import { connect } from '@unchainedshop/api/fastify';
 
 const fastify = Fastify();
 
 const platform = await startPlatform({
   modules: baseModules,
 });
-
 connect(fastify, platform, {
   allowRemoteToLocalhostSecureCookies: true,
   initPluginMiddlewares,
---
+++ examples/minimal/readme.md
@@ -0,0 +1,15 @@
+# Minimal Example
+
+Minimal Headless E-Commerce Backend in < 20 lines of ESM code.
+
+Batteries included:
+- Fastify
+- Unchained Engine E-Commerce Framework
+- Unchained Admin UI
+- GraphiQL
+
+```
+npx degit unchainedshop/unchained/examples/minimal my-minimal-app
+npm install
+node --env-file .env.defaults boot.js
+```
