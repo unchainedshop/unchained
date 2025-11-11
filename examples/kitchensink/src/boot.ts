f82a68ea284a3c3bea55bef40bfafc1b78ddb9c3
Mikael Araya <mikaelaraya12@gmail.com>
2025-11-11 15:42:31 +0300

    Add raw pdf template for orderPdfRender

--- examples/kitchensink/src/boot.ts
+++ examples/kitchensink/src/boot.ts
@@ -11,8 +11,6 @@ import { useErrorHandler } from '@envelop/core';
 import '@unchainedshop/plugins/pricing/discount-half-price-manual.js';
 import '@unchainedshop/plugins/pricing/discount-100-off.js';
 import { registerProductDiscoverabilityFilter } from '@unchainedshop/core';
-import setupTicketing, { TicketingAPI } from "@unchainedshop/ticketing";
-import rest from "@unchainedshop/ticketing/src/fastify.js";
 
 const fastify = Fastify({
   loggerInstance: unchainedLogger('fastify'),
@@ -49,12 +47,6 @@ try {
     modules: defaultModules,
   });
 
-
-  setupTicketing(platform.unchainedAPI as TicketingAPI, {
-    createAppleWalletPass: null,
-    createGoogleWalletPass: null
-
-  });
   connect(fastify, platform, {
     allowRemoteToLocalhostSecureCookies: process.env.NODE_ENV !== 'production',
     adminUI: true,
