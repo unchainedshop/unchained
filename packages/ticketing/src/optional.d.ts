7f5fcc2625e57a9a2d07c6e764b19cd72f5871da
Mikael Araya <mikaelaraya12@gmail.com>
2025-11-12 18:23:05 +0300

    Optional react-pdf based order ticket renderer

---
+++ packages/ticketing/src/optional.d.ts
@@ -0,0 +1,10 @@
+// src/types/optional.d.ts
+declare module '@react-pdf/renderer' {
+  const content: any;
+  export = content;
+}
+
+declare module 'qrcode' {
+  const content: any;
+  export = content;
+}
