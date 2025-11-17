040967218bb7983b8e2d5e6d6c7dcf4a52f05926
Mikael Araya <mikaelaraya12@gmail.com>
2025-11-17 13:46:40 +0300

    Adjust level catalog pricing to use minQuantity

--- packages/api/src/schema/types/product/product.ts
+++ packages/api/src/schema/types/product/product.ts
@@ -139,7 +139,7 @@ export default [
       country: Country!
       currency: Currency!
       amount: Int!
-      maxQuantity: Int
+      minQuantity: Int
     }
 
     union ConfigurableOrBundleProduct = BundleProduct | ConfigurableProduct
