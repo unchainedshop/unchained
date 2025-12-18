98acd4c89cf7b07debed0442f9e281520e277b2f
Pascal Kaufmann <pascal@unchained.shop>
2025-12-18 21:55:18 +0100

    Fix dead code in quotation status

--- packages/platform/src/templates/resolveQuotationStatusTemplate.ts
+++ packages/platform/src/templates/resolveQuotationStatusTemplate.ts
@@ -5,17 +5,6 @@ const { EMAIL_FROM, EMAIL_WEBSITE_NAME, EMAIL_WEBSITE_URL } = process.env;
 export const resolveQuotationStatusTemplate: TemplateResolver = async ({ quotationId }, { modules }) => {
   const quotation = await modules.quotations.findQuotation({ quotationId });
   const user = await modules.users.findUserById(quotation.userId);
-  const attachments = (
-    await modules.files.findFiles(
-      { 'meta.quotationId': quotation._id, 'meta.type': 'PROPOSAL' },
-      { sort: { 'meta.date': -1 } },
-    )
-  )
-    .filter(Boolean)
-    .map((file) => ({
-      filename: `${quotation.quotationNumber}_${file.name}`,
-      path: file.url,
-    }));
 
   const subject = `${EMAIL_WEBSITE_NAME}: Updated Quotation / ${quotation.quotationNumber}`;
   const url = `${EMAIL_WEBSITE_URL}/quotation?_id=${quotation._id}`;
@@ -38,7 +27,6 @@ export const resolveQuotationStatusTemplate: TemplateResolver = async ({ quotati
         to: modules.users.primaryEmail(user)?.address,
         subject,
         text,
-        attachments,
       },
     },
   ];
