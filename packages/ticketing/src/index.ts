11a28b1fcad9279fbed00c85d592d3743b09e86b
Mikael Araya <mikaelaraya12@gmail.com>
2025-12-23 15:20:51 +0300

    Add customizable pdf ticket renderer

--- packages/ticketing/src/index.ts
+++ packages/ticketing/src/index.ts
@@ -40,9 +40,9 @@ export default function setupTicketing(
     createGoogleWalletPass,
   }: {
     renderOrderPDF?: any;
-    createAppleWalletPass: any;
-    createGoogleWalletPass: any;
-  },
+    createAppleWalletPass?: any;
+    createGoogleWalletPass?: any;
+  } = {},
 ) {
   setupPDFTickets({
     renderOrderPDF,
