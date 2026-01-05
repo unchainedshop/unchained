ac46bf523fb6cbecdd6969e57a6483253050e259
Pascal Kaufmann <pascal@unchained.shop>
2026-01-05 10:01:59 +0100

    Format

--- examples/ticketing/tests/google-wallet.test.js
+++ examples/ticketing/tests/google-wallet.test.js
@@ -1,30 +1,33 @@
 import test from 'node:test';
 import assert from 'node:assert';
 
-
-const VALID_TOKEN1_HASH = '14e21852abe0b7609b6659ffed526ab9d261c8a43c72481043a6141f782e3df0'
-test.describe('googleWallet', async () => {   
-  
-  test('Return PASS_GENERATION_ERROR after success because no default renderer is registered', async () => {        
-    const result = await fetch(`http://localhost:4010/rest/google-wallet/download/test-token-1?hash=${VALID_TOKEN1_HASH}`);    
+const VALID_TOKEN1_HASH = '14e21852abe0b7609b6659ffed526ab9d261c8a43c72481043a6141f782e3df0';
+test.describe('googleWallet', async () => {
+  test('Return PASS_GENERATION_ERROR after success because no default renderer is registered', async () => {
+    const result = await fetch(
+      `http://localhost:4010/rest/google-wallet/download/test-token-1?hash=${VALID_TOKEN1_HASH}`,
+    );
     assert.strictEqual(result.status, 500);
     const errorsMessage = await result.json();
     assert.strictEqual(errorsMessage.success, false);
     assert.strictEqual(errorsMessage.message, 'Error generating pass');
-    assert.strictEqual(errorsMessage.name, 'PASS_GENERATION_ERROR');       
+    assert.strictEqual(errorsMessage.name, 'PASS_GENERATION_ERROR');
   });
 
-  test('Return HASH_MISMATCH Error when invalid hash is provided', async () => {        
-    const result = await fetch(`http://localhost:4010/rest/google-wallet/download/test-token-1?hash=14e21852abe0b7609b6659ffed526ab9d261c8a43c72481043a6141f782e3`);    
+  test('Return HASH_MISMATCH Error when invalid hash is provided', async () => {
+    const result = await fetch(
+      `http://localhost:4010/rest/google-wallet/download/test-token-1?hash=14e21852abe0b7609b6659ffed526ab9d261c8a43c72481043a6141f782e3`,
+    );
     assert.strictEqual(result.status, 403);
     const errorsMessage = await result.json();
     assert.strictEqual(errorsMessage.success, false);
     assert.strictEqual(errorsMessage.message, 'Token hash invalid for current owner');
-    assert.strictEqual(errorsMessage.name, 'HASH_MISMATCH');       
+    assert.strictEqual(errorsMessage.name, 'HASH_MISMATCH');
   });
-  test('Return Not found / 4040 when non existing token ID is provided', async () => {        
-    const result = await fetch(`http://localhost:4010/rest/google-wallet/download/invalid-id?hash=${VALID_TOKEN1_HASH}`);    
+  test('Return Not found / 4040 when non existing token ID is provided', async () => {
+    const result = await fetch(
+      `http://localhost:4010/rest/google-wallet/download/invalid-id?hash=${VALID_TOKEN1_HASH}`,
+    );
     assert.strictEqual(result.status, 404);
-    
   });
 });
