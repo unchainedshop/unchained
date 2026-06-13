a5f87b435ada9817b2f0f880e6fee2f36b3a3baa
Mikael Araya <mikaelaraya12@gmail.com>
2026-06-13 09:32:24 +0300

    Optimize enrollment periods

--- packages/core-enrollments/src/module/configureEnrollmentsModule.ts
+++ packages/core-enrollments/src/module/configureEnrollmentsModule.ts
@@ -363,7 +363,7 @@ export const configureEnrollmentsModule = async ({
           $pull: {
             periods: {
               start: { $gte: afterDate },
-              orderId: { $in: [null, undefined] },
+              orderId: { $in: [null] },
             },
           } as any,
           $set: { updated: new Date() },
