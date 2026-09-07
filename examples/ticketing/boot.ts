27db1e8d87110920537f772a49cefeac23f5a77d
Mikael Araya <mikaelaraya12@gmail.com>
2026-09-07 23:49:38 +0300

    Export composable ticketing admin plugin config with definePlugin

--- examples/ticketing/boot.ts
+++ examples/ticketing/boot.ts
@@ -12,14 +12,7 @@ import setupTicketing, {
 } from '@unchainedshop/ticketing';
 import connectTicketingToFastify from '@unchainedshop/ticketing/lib/fastify.js';
 import ticketingServices from '@unchainedshop/ticketing/lib/services.js';
-import { definePlugin } from '@unchainedshop/admin-ui/plugins';
-import {
-  ticketingBundlePath,
-  ticketingEntities,
-  ticketingPages,
-  ticketingLinks,
-  ticketingNavigation,
-} from '@unchainedshop/ticketing/admin-plugin';
+import { ticketingAdminPlugin } from '@unchainedshop/ticketing/admin-plugin';
 import seed from './seed.ts';
 
 const fastify = Fastify({
@@ -55,19 +48,7 @@ try {
   connect(fastify, platform, {
     allowRemoteToLocalhostSecureCookies: process.env.NODE_ENV !== 'production',
     adminUI: {
-      plugins: [
-        definePlugin({
-          name: 'ticketing',
-          version: '1.0.0',
-          bundlePath: ticketingBundlePath,
-          navigation: ticketingNavigation,
-          slots: {
-            entities: ticketingEntities,
-            pages: ticketingPages,
-            links: ticketingLinks,
-          },
-        }),
-      ],
+      plugins: [ticketingAdminPlugin()],
     },
   });
 
